5c9aba65d1edbcb705e23b496547920191277ec8
bergstrj@iro.umontreal.ca <bergstrj@iro.umontreal.ca>
2008-02-01 13:52:16 -0500

    removed print statement, whitespace

--- opt.py
+++ opt.py
@@ -9,14 +9,12 @@ This variable is used in compile.prog as the optimizer for all programs built
 using either compile.single, compile.to_func, and compile.prog.
 
 """
-        
+
 def optimizer(lst):
     begin = gof.SeqOptimizer([])
     end   = gof.SeqOptimizer([gof.DummyRemover])
     seq_opt = gof.SeqOptimizer(begin + lst + end)
-    print 'seq_opt', seq_opt
     return gof.PythonOpt(gof.MergeOptMerge(seq_opt))
-    
 
 if 0:
     optimizer_begin = gof.SeqOptimizer([opt for name, opt in [
@@ -29,7 +27,7 @@ if 0:
              ['mulxx_to_sqr',                pattern_opt((mul_elemwise, 'x', 'x'), (sqr, 'x'))],
 
              ['sqr_to_isqr',                 op_sub(sqr, isqr)],
-            
+
              ['add_to_iadd',                 op_sub(add_elemwise, iadd_elemwise)],
 
              ['add_to_iadd_reverse',         pattern_opt((add_elemwise, 'x', 'y'),
