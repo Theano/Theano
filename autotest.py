db400460e9d1de0803e96732cab5449969321dce
bergstrj@iro.umontreal.ca <bergstrj@iro.umontreal.ca>
2008-02-04 19:38:55 -0500

    better autotest driver

--- autotest.py
+++ autotest.py
@@ -1,13 +1,17 @@
 import unittest, os, sys
 
-for filename in os.listdir('.'):
-    if filename == __file__: continue
-    #continue
-    if filename[-3:] == '.py':
-        modname = filename[:-3]
-        suite = unittest.TestLoader().loadTestsFromModule(__import__(modname))
-        #suite.addTests(unittest.TestLoader().loadTestsFromModule(__import__(modname)))
-        if suite.countTestCases() > 0:
-            print >>sys.stderr, 'Testing', modname, '(%s)'% (filename),
-            unittest.TextTestRunner(verbosity=1).run(suite)
+if __name__ == '__main__':
+    suite = None
+    for filename in os.listdir('.'):
+        if filename[-3:] == '.py':
+            modname = filename[:-3]
+            tests = unittest.TestLoader().loadTestsFromModule(__import__(modname))
+            if tests.countTestCases() > 0:
+                print >>sys.stderr, 'Testing', modname
+                if suite is None:
+                    suite = tests
+                else:
+                    suite.addTests(tests)
+
+    unittest.TextTestRunner(verbosity=1).run(suite)
 
