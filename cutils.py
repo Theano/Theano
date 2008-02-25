5d410e230ad88feafb1d1c695740e69de4c679e8
Joseph Turian <turian@gmail.com>
2008-02-25 15:44:14 -0500

    Fixed cutils.py for gcc 3.4 -james

--- cutils.py
+++ cutils.py
@@ -12,7 +12,13 @@ except ImportError:
                             "Argument to run_cthunk must be a PyCObject returned by the c_thunk method of an omega_op.");
             return NULL;
         }
-        int (*fn)(void*) = reinterpret_cast<int (*)(void*)>(PyCObject_AsVoidPtr(py_cthunk));
+        void * ptr_addr = PyCObject_AsVoidPtr(py_cthunk);
+        int (*fn)(void*) = reinterpret_cast<int (*)(void*)>(ptr_addr);
+        //int (*fn)(void*) = static_cast<int (*)(void*)>(PyCObject_AsVoidPtr(py_cthunk));
+        //int (*fn)(void*) = NULL;
+        //fn += PyCObject_AsVoidPtr(py_cthunk);
+        //int (*fn)(void*) = 
+
         void* it = PyCObject_GetDesc(py_cthunk);
         int failure = fn(it);
         if (failure) {
@@ -24,7 +30,9 @@ except ImportError:
     
     cthunk = object()
     mod = weave.ext_tools.ext_module('cutils_ext')
-    mod.add_function(weave.ext_tools.ext_function('run_cthunk', single_runner, ['cthunk']))
+    fun =weave.ext_tools.ext_function('run_cthunk', single_runner, ['cthunk'])
+    fun.customize.add_extra_compile_arg('--permissive')
+    mod.add_function(fun)
     mod.compile()
 
     from cutils_ext import *
