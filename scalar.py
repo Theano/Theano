476c9f6604eb761f31dcbbc0f1501d1bf2ebf4f8
bergstrj@iro.umontreal.ca <bergstrj@iro.umontreal.ca>
2008-04-15 16:56:41 -0400

    added type checks to scalar.Abs.c_impl

--- scalar.py
+++ scalar.py
@@ -308,7 +308,12 @@ class Abs(UnaryScalarOp):
     def grad(self, (x, ), (gz, )):
         return gz * sgn(x),
     def c_code(self, (x, ), (z, ), sub):
-        return "%(z)s = abs(%(x)s);" % locals()
+        dtype = self.inputs[0].dtype
+        if dtype in ('int32', 'int64'):
+            return "%(z)s = abs(%(x)s);" % locals()
+        if dtype in ('float32', 'float64'):
+            return "%(z)s = fabs(%(x)s);" % locals()
+        raise NotImplementedError('Abs not implemented for dtype', dtype)
 
 class Sgn(UnaryScalarOp):
     def impl(self, x):
