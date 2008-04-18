cb823769c78dffa3ea8c13392f4b55e4ac87ff2a
Olivier Breuleux <breuleuo@iro.umontreal.ca>
2008-04-18 14:59:33 -0400

    merged base_tensor with tensor

--- __init__.py
+++ __init__.py
@@ -1,13 +1,11 @@
 
 import gof
-import base_tensor
 import tensor
 import sparse
 import compile
 import gradient
 import opt
 
-from base_tensor import *
 from tensor import *
 from compile import *
 from opt import *
