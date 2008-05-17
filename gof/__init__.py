82d158a2c88c6c641ede9a23164735702861989a
Olivier Breuleux <breuleuo@iro.umontreal.ca>
2008-05-17 16:27:54 -0400

    clone_with_equiv, changes to Apply.clone_with_new_inputs

--- gof/__init__.py
+++ gof/__init__.py
@@ -4,7 +4,7 @@ from env import InconsistencyError, Env
 from ext import DestroyHandler, view_roots
 from graph import Apply, Result, Constant, Value
 from link import Linker, LocalLinker, PerformLinker, MetaLinker, Profiler
-from op import Op
+from op import Op, Macro
 from opt import Optimizer, DummyOpt, SeqOptimizer, LocalOptimizer, OpSpecificOptimizer, OpSubOptimizer, OpRemover, PatternOptimizer, MergeOptimizer, MergeOptMerge
 from toolbox import Bookkeeper, History, Validator, ReplaceValidate, NodeFinder, PrintListener
 from type import Type, Generic, generic
