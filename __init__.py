f2903b1ed79ea684c3bde0a4011f1b2153b626c8
James Bergstra <bergstrj@iro.umontreal.ca>
2008-06-10 13:00:17 -0400

    added src_version cache

--- __init__.py
+++ __init__.py
@@ -52,33 +52,36 @@ def __src_version__():
     #
 
 
-    #print 'name:', __name__
-    location = _imp.find_module(__name__)[1]
-    #print 'location:', location
+    if not hasattr(__src_version__, 'rval'):
+        #print 'name:', __name__
+        location = _imp.find_module(__name__)[1]
+        #print 'location:', location
 
-    status = _subprocess.Popen(('hg','st'),cwd=location,stdout=_subprocess.PIPE).communicate()[0]
-    #status_codes = [line[0] for line in  if line and line[0] != '?']
-    for line in status.split('\n'):
-        if not line: continue
-        if line[0] != '?':
-            raise Exception('Uncommitted modification to "%s" in %s (%s)'
-                    %(line[2:], __name__,location))
-        if line[0] == '?' and line[-3:] == '.py':
-            raise Exception('Untracked file "%s" in %s (%s)'
-                    %(line[2:], __name__, location))
+        status = _subprocess.Popen(('hg','st'),cwd=location,stdout=_subprocess.PIPE).communicate()[0]
+        #status_codes = [line[0] for line in  if line and line[0] != '?']
+        for line in status.split('\n'):
+            if not line: continue
+            if line[0] != '?':
+                raise Exception('Uncommitted modification to "%s" in %s (%s)'
+                        %(line[2:], __name__,location))
+            if line[0] == '?' and line[-3:] == '.py':
+                raise Exception('Untracked file "%s" in %s (%s)'
+                        %(line[2:], __name__, location))
 
-    hg_id = _subprocess.Popen(('hg','id'),cwd=location,stdout=_subprocess.PIPE).communicate()[0]
+        hg_id = _subprocess.Popen(('hg','id'),cwd=location,stdout=_subprocess.PIPE).communicate()[0]
 
-    #This asserts my understanding of hg id return values
-    # There is mention in the doc that it might return two parent hash codes
-    # but I've never seen it, and I dont' know what it means or how it is
-    # formatted.
-    tokens = hg_id.split(' ')
-    assert len(tokens) <= 2
-    assert len(tokens) >= 1
-    assert tokens[0][-1] != '+' # the trailing + indicates uncommitted changes
-    if len(tokens) == 2:
-        assert tokens[1] == 'tip\n'
+        #This asserts my understanding of hg id return values
+        # There is mention in the doc that it might return two parent hash codes
+        # but I've never seen it, and I dont' know what it means or how it is
+        # formatted.
+        tokens = hg_id.split(' ')
+        assert len(tokens) <= 2
+        assert len(tokens) >= 1
+        assert tokens[0][-1] != '+' # the trailing + indicates uncommitted changes
+        if len(tokens) == 2:
+            assert tokens[1] == 'tip\n'
 
-    return tokens[0]
+        __src_version__.rval = tokens[0]
+
+    return __src_version__.rval
 
