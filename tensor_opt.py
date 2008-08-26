7cf4d637a9fc258a43fdb599ed0b3c8293f9ea9d
Olivier Breuleux <breuleuo@iro.umontreal.ca>
2008-08-26 17:35:37 -0400

    bugfix on bugfix on canonizer

--- tensor_opt.py
+++ tensor_opt.py
@@ -378,10 +378,10 @@ class Canonizer(gof.LocalOptimizer):
             return self.reciprocal(self.merge_num_denum(denum, []))
         if not ld:
             if ln == 1:
-                if isinstance(num[0], N.ndarray):
-                    return T.as_tensor(num[0])
-                else:
+                if isinstance(num[0], gof.Result):
                     return num[0]
+                else:
+                    return T.as_tensor(num[0])
             else:
                 return self.main(*num)
         return self.inverse(self.merge_num_denum(num, []),
