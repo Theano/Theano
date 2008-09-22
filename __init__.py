e1b16ba291f0898774c8dc1647d11dfc5b303564
james@crane <james@crane>
2008-09-21 20:55:51 -0400

    whitespace

--- __init__.py
+++ __init__.py
@@ -1,12 +1,7 @@
 """
-
 WRITEME: Some module-level documentation.
 
-Here is how to make a link into the wiki: check out the 
-:wiki:`DefineGraph()`
-
-:wiki:`Connection.cursor()`
- page.
+Here is how to make a link into the wiki: check out the :wiki:`DefineGraph()`.
 
 """
 
