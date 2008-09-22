8d89c6b004906b6181824c7e97eb9c1567e85cd8
James Bergstra <bergstrj@iro.umontreal.ca>
2008-09-22 15:22:55 -0400

    debugging new epydoc system

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ WRITEME: Some module-level documentation.
 Here is how to make a link into the wiki: check out the :wiki:`DefineGraph`.
 
 
-Testing... some more. and more. AAABCDEFGH
+Testing... some more. and more. AAABCDEFGHI
 """
 
 __docformat__ = "restructuredtext en"
