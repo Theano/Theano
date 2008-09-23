bf8df8dc0b3c2507e0b02a405fc35a4d43125813
bergstrj@lgcm <bergstrj@lgcm>
2008-09-23 18:07:06 -0400

    renamed gen_oplist, added autogen of oplist.txt to local.build_html

--- local.build_html.sh
+++ local.build_html.sh
@@ -11,6 +11,8 @@ if [ " $1" != " rst" ]; then
 epydoc --config local.epydoc
 fi
 
+python gen_op_list.py > doc/oplist.txt
+
 if [ " $1" != " epydoc" ]; then
 for RST in graph oplist ;  do
     $APIRST2HTML $EPYDOC_ARGS doc/$RST.txt html/doc/$RST.html
