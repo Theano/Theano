b69016e2cb66ef3f8182cb38412914791b05001f
bergstrj@lgcm <bergstrj@lgcm>
2008-09-23 18:08:53 -0400

    typo in  local.build_html

--- local.build_html.sh
+++ local.build_html.sh
@@ -4,14 +4,13 @@ APIRST2HTML=doc/apirst2html.py
 EPYDOC_ARGS='--external-api=api --external-api-file=api:html/api/api-objects.txt --external-api-root=api:../api/'
 
 
-mkdir -p html/api 
-mkdir -p html/doc
+mkdir -p html/api && mkdir -p html/doc
 
 if [ " $1" != " rst" ]; then
 epydoc --config local.epydoc
 fi
 
-python gen_op_list.py > doc/oplist.txt
+python gen_oplist.py > doc/oplist.txt
 
 if [ " $1" != " epydoc" ]; then
 for RST in graph oplist ;  do
