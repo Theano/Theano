cd10efccda4cb9cf375eeeab70e641b09c6e00e4
projects@lgcm <projects@lgcm>
2008-09-24 13:23:01 -0400

    typo

--- local.build_html.sh
+++ local.build_html.sh
@@ -6,8 +6,8 @@ EPYDOC_ARGS='--external-api=api --external-api-file=api:html/api/api-objects.txt
 
 mkdir -p html/api && mkdir -p html/doc
 
-#this builds some stuff or something... basically makes the rest work properly
-for a reason I don't understand.  -JB 20080924
+# this builds some stuff or something... basically makes the rest work properly
+# for a reason I don't understand.  -JB 20080924
 python __init__.py
 
 if [ " $1" != " rst" ]; then
