8e7bd58ad9abd1981e5b62a4789e025b778609f4
projects@lgcm <projects@lgcm>
2008-09-24 13:17:04 -0400

    potential autobuild fix

--- local.build_html.sh
+++ local.build_html.sh
@@ -6,14 +6,18 @@ EPYDOC_ARGS='--external-api=api --external-api-file=api:html/api/api-objects.txt
 
 mkdir -p html/api && mkdir -p html/doc
 
+#this builds some stuff or something... basically makes the rest work properly
+for a reason I don't understand.  -JB 20080924
+python __init__.py
+
 if [ " $1" != " rst" ]; then
 epydoc --config local.epydoc
 fi
 
-python gen_oplist.py > doc/oplist.txt
-
 if [ " $1" != " epydoc" ]; then
+python gen_oplist.py > doc/oplist.txt
 for RST in graph oplist ;  do
+
     $APIRST2HTML $EPYDOC_ARGS doc/$RST.txt html/doc/$RST.html
 done
 fi
