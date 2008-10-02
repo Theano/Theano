444ea1e4f0581df4d0256c269c4cb7669a9cab99
Olivier Breuleux <breuleuo@iro.umontreal.ca>
2008-10-02 15:39:41 -0400

    forgot to add tensor/__init__ to repo

---
+++ tensor/__init__.py
@@ -0,0 +1,19 @@
+
+from basic import *
+
+import opt
+
+import raw_random
+from raw_random import \
+    RandomKit
+
+random = RandomKit('random')
+
+from elemwise import \
+    DimShuffle, Elemwise, CAReduce
+
+
+
+
+
+
