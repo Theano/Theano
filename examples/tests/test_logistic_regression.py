172d2351cfe9d5d572884e6516c8016501234a3f
Frederic Bastien <bastienf@iro.umontreal.ca>
2008-11-25 14:24:06 -0500

    compile the module with more different compile option and compare them.

--- examples/tests/test_logistic_regression.py
+++ examples/tests/test_logistic_regression.py
@@ -37,6 +37,7 @@ class test_logistic_regression_example(unittest.TestCase):
         pprint.assign(nnet.crossentropy_softmax_argmax_1hot_with_bias, printing.FunctionPrinter('nll', 'softmax', 'argmax'))
 
         lrc = logistic_regression.LogisticRegressionN()
+        lr0 = lrc.make(10, 2, seed=1827)
         lr1 = lrc.make(10, 2, mode=theano.Mode('c|py', 'fast_run'), seed=1827)
         lr2 = lrc.make(10, 2, mode=theano.Mode('py', 'fast_run'), seed=1827)
         lr3 = lrc.make(10, 2, mode=theano.Mode('py', 'merge'), seed=1827) #'FAST_RUN')
@@ -49,14 +50,17 @@ class test_logistic_regression_example(unittest.TestCase):
             for i in xrange(1000):
                 lr.lr = 0.02
                 xe = lr.update(data_x, data_y) 
+
+        train(lr0)
         train(lr1)
         train(lr2)
         train(lr3)
         train(lr4)
 
-        assert lr1==lr2
-        assert lr1==lr3
-        assert lr1==lr4
+        assert lr0==lr1
+        assert lr0==lr2
+        assert lr0==lr3
+        assert lr0==lr4
 
     def test_example_module2(self): 
         """Test that the LogisticRegression2 module execute the same with different mode"""
@@ -65,11 +69,11 @@ class test_logistic_regression_example(unittest.TestCase):
         import logistic_regression
         lrc = logistic_regression.LogisticRegression2() #TODO: test 2==N
         lr0 = lrc.make(10,1827)
-#        lr1 = lrc.make(10, mode=theano.Mode('c|py', 'fast_run'), seed=1827)
-#        lr2 = lrc.make(10, mode=theano.Mode('py', 'fast_run'), seed=1827)
-#        lr3 = lrc.make(10, mode=theano.Mode('py', 'merge'), seed=1827) #'FAST_RU
-#        lr4 = lrc.make(10, mode=compile.FAST_RUN.excluding('fast_run'), seed=1827)
-#         #FAST_RUN, FAST_COMPILE, 
+        lr1 = lrc.make(10, mode=theano.Mode('c|py', 'fast_run'), seed=1827)
+        lr2 = lrc.make(10, mode=theano.Mode('py', 'fast_run'), seed=1827)
+        lr3 = lrc.make(10, mode=theano.Mode('py', 'merge'), seed=1827) #'FAST_RU
+        lr4 = lrc.make(10, mode=compile.FAST_RUN.excluding('fast_run'), seed=1827)
+         #FAST_RUN, FAST_COMPILE, 
         data_x = N.random.randn(5, 10)
         data_y = (N.random.randn(5) > 0)
         data_y = data_y.reshape((data_y.shape[0],1))#need to be a column
@@ -79,11 +83,17 @@ class test_logistic_regression_example(unittest.TestCase):
             for i in xrange(1000):
                 lr.lr = 0.02
                 xe = lr.update(data_x, data_y) 
+
         train(lr0)
-#        train(lr1)
-#        train(lr2)
-#        train(lr3)
- #       train(lr4)
+        train(lr1)
+        train(lr2)
+        train(lr3)
+        train(lr4)
+
+        assert lr0==lr1
+        assert lr0==lr2
+        assert lr0==lr3
+        assert lr0==lr4
 
 #        self.fail("NotImplementedError")
 
