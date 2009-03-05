5786d6bf230b152f7d76c17abf0aed26bee15869
Olivier Breuleux <breuleuo@iro.umontreal.ca>
2009-03-04 19:34:30 -0500

    I accidentally broke the pdf, should be fixed now

--- doc/conf.py
+++ doc/conf.py
@@ -164,7 +164,7 @@ htmlhelp_basename = 'theanodoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('index', 'theano.tex', 'theano Documentation',
+  ('contents', 'theano.tex', 'theano Documentation',
    'LISA lab', 'manual'),
 ]
 
