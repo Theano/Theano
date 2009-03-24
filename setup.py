0be9929c3b99f7f3afd5f61e4fe62c5071d64ebb
Joseph Turian <turian@iro.umontreal.ca>
2009-03-24 18:25:08 -0400

    Rewrite introduction slightly

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ from setuptools import setup, find_packages
 
 setup(name="Theano",
       version="0.1",
-      description="Theano",
-      long_description="""Machine learning toolkit""",
-      author="LISA",
+      description="Machine learning toolkit",
+      author="LISA laboratory, Université de Montréal",
       author_email="theano-dev@googlegroups.com",
       packages=find_packages(exclude='tests'),
       url="http://lgcm.iro.umontreal.ca/theano",
