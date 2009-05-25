a3d94ccd092529df7ac166aa93420c04a622b6ce
James Bergstra <bergstrj@iro.umontreal.ca>
2009-05-25 16:19:52 -0400

    added xlogy0 op

--- theano/tensor/tests/test_xlogx.py
+++ theano/tensor/tests/test_xlogx.py
@@ -1,4 +1,4 @@
-from theano.tensor.xlogx import xlogx
+from theano.tensor.xlogx import xlogx, xlogy0
 
 import unittest
 
@@ -24,6 +24,19 @@ class T_XlogX(unittest.TestCase):
 #            def make_node(self, a):
 #                return [xlogx(a)[:,2]]
         utt.verify_grad(xlogx, [numpy.random.rand(3,4)])
+
+class T_XlogY0(unittest.TestCase):
+    def setUp(self):
+        utt.seed_rng()
+    def test2(self):
+        utt.verify_grad(xlogy0, [numpy.random.rand(3,4), numpy.random.rand(3,4)])
+
+    def test3(self):
+        x = as_tensor_variable([1, 0])
+        y = as_tensor_variable([1, 0])
+        z = xlogy0(x, y)
+        f = theano.function([], z)
+        self.failUnless(numpy.all(f() == numpy.asarray([0, 0.])))
 
 
 if __name__ == '__main__':
--- theano/tensor/xlogx.py
+++ theano/tensor/xlogx.py
@@ -26,3 +26,27 @@ class XlogX(scalar.UnaryScalarOp):
 scalar_xlogx  = XlogX(scalar.upgrade_to_float, name='scalar_xlogx')
 xlogx = tensor.Elemwise(scalar_xlogx, name='xlogx')
 
+
+class XlogY0(scalar.BinaryScalarOp):
+    """
+    Compute X * log(Y), with special case 0 log(0) = 0.
+    """
+    @staticmethod
+    def st_impl(x, y):
+        if x == 0.0:
+            return 0.0
+        return x * numpy.log(y)
+    def impl(self, x, y):
+        return XlogY0.st_impl(x, y)
+    def grad(self, (x, y), (gz,)):
+        return [gz * scalar.log(y), gz * x / y]
+    def c_code(self, node, name, (x, y), (z,), sub):
+        if node.inputs[0].type in [scalar.float32, scalar.float64]:
+            return """%(z)s =
+                %(x)s == 0.0
+                ? 0.0
+                : %(x)s * log(%(y)s);""" % locals()
+        raise NotImplementedError('only floatingpoint is implemented')
+scalar_xlogy0  = XlogY0(scalar.upgrade_to_float, name='scalar_xlogy0')
+xlogy0 = tensor.Elemwise(scalar_xlogy0, name='xlogy0')
+
