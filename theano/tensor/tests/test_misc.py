def02f4d79b95ff621f1d5d897c40c612a6ff179
Olivier Delalleau <delallea@iro>
2009-06-10 13:06:22 -0400

    Partial merge - I messed up a bit with mercurial

--- theano/tensor/tests/test_misc.py
+++ theano/tensor/tests/test_misc.py
@@ -1,12 +1,15 @@
-# This bug is detailed in ticket #387. Please close this ticket once the test
-# passes (http://pylearn.org/theano/trac/ticket/387).
-
+import sys
 import numpy, theano
 from theano import tensor
 
 def test_bug_2009_06_02_trac_387():
 
-    y = tensor.lvector()
-    f = theano.function([y], tensor.stack(y[0] / 2))
+    y = tensor.lvector('y')
+    #f = theano.function([y], tensor.stack(y[0] / 2))
+    #f = theano.function([y], tensor.join(0,tensor.shape_padleft(y[0] / 2,1)))
+    f = theano.function([y], tensor.int_div(tensor.DimShuffle(y[0].broadcastable, ['x'])(y[0]), 2))
+    sys.stdout.flush()
+    print f(numpy.ones(1) * 3)
+    #z = tensor.lscalar('z')
+    #f = theano.function([z], tensor.DimShuffle([], ['x'])(z) / 2)
 
-    print f(numpy.ones(1) * 2)
