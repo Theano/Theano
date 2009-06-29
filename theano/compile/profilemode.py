f763476c30b518b2e10a85d58d147f251fe2d60f
Frederic Bastien <bastienf@iro.umontreal.ca>
2009-06-29 13:55:40 -0400

    bugfix: if linker is a string in ProfileMode(linker). It was not working.

--- theano/compile/profilemode.py
+++ theano/compile/profilemode.py
@@ -2,7 +2,7 @@ import time
 
 from ..gof.link import WrapLinkerMany
 from ..gof.cutils import run_cthunk
-from ..compile.mode import Mode
+from ..compile.mode import Mode, predefined_linkers
 
 class ProfileMode(Mode):
     def __init__(self, linker, optimizer=None):
@@ -30,6 +30,9 @@ class ProfileMode(Mode):
         self.apply_time = apply_time
         self.op_time = op_time
         self.op_cimpl = op_cimpl
+
+        if isinstance(linker, str):
+            linker = predefined_linkers[linker]
 
         wrap_linker = WrapLinkerMany([linker], [blah])
         if optimizer:
