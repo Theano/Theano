5332d39d26b5ddf6769eee71571842971215e922
James Bergstra <bergstrj@iro.umontreal.ca>
2009-09-11 15:25:03 -0400

    typo in Reshape.__hash__

--- theano/tensor/basic.py
+++ theano/tensor/basic.py
@@ -2257,7 +2257,7 @@ class Reshape(Op):
         return (type(other) is type(self)) and (other.ndim == self.ndim)
     def __hash__(self):
         # .name does not participate because it doesn't affect computations
-        return hash(type(other)) ^ hash(self.ndim)
+        return hash(type(self)) ^ hash(self.ndim)
     def __str__(self):
         return '%s{%s}' %(self.__class__.__name__, self.ndim)
     def make_node(self, x, shp):
