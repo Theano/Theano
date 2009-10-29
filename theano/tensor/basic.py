680c25f2420165f19a73456fa6b7700528e781d7
James Bergstra <bergstrj@iro.umontreal.ca>
2009-10-29 13:31:36 -0400

    Backing out of the idea to add 'shape' attribute to TensorType.  This is a bigger job.  See TRAC ticket on subject.

--- theano/tensor/basic.py
+++ theano/tensor/basic.py
@@ -253,6 +253,11 @@ class TensorType(Type):
     When this is True, strict filtering rejects data containing NaN or Inf entries. (Used in `DebugMode`)
     """
 
+    use_shape = False
+    """
+    This should be removed (hardcoded to be False) after AISTATS09
+    """
+
     def __init__(self, dtype, broadcastable, name = None, shape=None):
         """Initialize self.dtype and self.broadcastable.
 
@@ -305,10 +310,11 @@ class TensorType(Type):
             if self.filter_checks_isfinite and (not numpy.all(numpy.isfinite(data))):
                 raise TypeError("non-finite elements not allowed")
 
-            for si, di in zip(self.shape, data.shape):
-                if not (si is None or si == di):
-                    raise TypeError('%s requires ndarray with shape matching %s (got %s)'%(
-                        self, self.shape, data.shape))
+            if TensorType.use_shape:
+                for si, di in zip(self.shape, data.shape):
+                    if not (si is None or si == di):
+                        raise TypeError('%s requires ndarray with shape matching %s (got %s)'%(
+                            self, self.shape, data.shape))
             return data
         else:
             data = numpy.asarray(data, dtype = self.dtype)
@@ -347,9 +353,13 @@ class TensorType(Type):
 
     def __eq__(self, other):
         """Compare True iff other is the same kind of TensorType"""
-        return type(self) == type(other) and other.dtype == self.dtype \
-                and other.broadcastable == self.broadcastable \
-                and other.shape == self.shape
+        if TensorType.use_shape:
+            return type(self) == type(other) and other.dtype == self.dtype \
+                    and other.broadcastable == self.broadcastable \
+                    and other.shape == self.shape
+        else:
+            return type(self) == type(other) and other.dtype == self.dtype \
+                    and other.broadcastable == self.broadcastable
 
     @staticmethod
     def values_eq(a, b):
@@ -420,7 +430,10 @@ class TensorType(Type):
 
     def __hash__(self):
         """Hash equal for same kinds of TensorType"""
-        return hashtype(self) ^ hash(self.dtype) ^ hash(self.broadcastable) ^ hash(self.shape)
+        if TensorType.use_shape:
+            return hashtype(self) ^ hash(self.dtype) ^ hash(self.broadcastable) ^ hash(self.shape)
+        else:
+            return hashtype(self) ^ hash(self.dtype) ^ hash(self.broadcastable)
 
     ndim = property(lambda self: len(self.broadcastable), doc = "number of dimensions")
     """Number of dimensions
