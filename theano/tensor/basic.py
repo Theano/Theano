0b59bab30f9c41c0a11a853012d5752003b2c9c5
James Bergstra <bergstrj@iro.umontreal.ca>
2010-01-08 21:02:50 -0500

    Added comments to the function bodies of add, sub, mul, etc. in tensor.basic

--- theano/tensor/basic.py
+++ theano/tensor/basic.py
@@ -1748,36 +1748,44 @@ def div_proxy(x, y):
         return true_div(x, y)
 
 @_scal_elemwise
-def add(a, b):
+def add(a, *other_terms):
     """elementwise addition"""
+    # see decorator for function body
 
 @_scal_elemwise
 def sub(a, b):
     """elementwise subtraction"""
+    # see decorator for function body
 
 @_scal_elemwise
-def mul(a, b):
+def mul(a, *other_terms):
     """elementwise multiplication"""
+    # see decorator for function body
 
 @_scal_elemwise
 def true_div(a, b):
     """elementwise [true] division (inverse of multiplication)"""
+    # see decorator for function body
 
 @_scal_elemwise
 def int_div(a, b):
     """elementwise integer-division"""
+    # see decorator for function body
 
 @_scal_elemwise
 def mod(a, b):
     """elementwise modulo"""
+    # see decorator for function body
 
 @_scal_elemwise
 def pow(a, b):
     """elementwise power"""
+    # see decorator for function body
 
 @_scal_elemwise
 def clip(x, min, max):
     """clip x to be between min and max"""
+    # see decorator for function body
 
 pprint.assign(add, printing.OperatorPrinter('+', -2, 'either'))
 pprint.assign(mul, printing.OperatorPrinter('*', -1, 'either'))
