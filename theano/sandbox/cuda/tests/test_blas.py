7d3e5d6788d558ca6fe34283d4139013856df609
Frederic Bastien <nouiz@nouiz.org>
2010-01-29 15:33:07 -0500

    fix cuda test bug by making the difference between the presence of cuda and if cuda is available.
    -side effect, the cuda test are not enabled by default, will be on the build bot.
    -also, now the use fct don't have a default value.

--- theano/sandbox/cuda/tests/test_blas.py
+++ theano/sandbox/cuda/tests/test_blas.py
@@ -8,7 +8,7 @@ import numpy
 # Skip test if cuda_ndarray is not available.
 from nose.plugins.skip import SkipTest
 import theano.sandbox.cuda as cuda_ndarray
-if cuda_ndarray.enable_cuda == False:
+if cuda_ndarray.cuda_enabled == False:
     raise SkipTest('Optional package cuda disabled')
 
 import theano.sandbox.cuda as tcn
