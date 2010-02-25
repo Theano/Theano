6e61aa5e1d4c60b63af1e974c2026d7dcedf380b
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2010-02-25 11:42:44 -0500

    Typo fix.

--- theano/tensor/nnet/nnet.py
+++ theano/tensor/nnet/nnet.py
@@ -1063,7 +1063,7 @@ def local_advanced_indexing_crossentropy_onehot_grad(node):
 
             # set out_grad according to the numerator, it may be divided later
             # num should be a vector or a scalar
-            if numpy.ndim==1 or numpy.all(num.broadcastable):
+            if num.ndim==1 or numpy.all(num.broadcastable):
                 out_grad *= -num
             else:
                 return
