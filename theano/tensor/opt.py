8267bcc54863fccb3aa2e2694d9aa0938385efd9
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2010-02-26 12:47:42 -0500

    Slightly more detailed error

--- theano/tensor/opt.py
+++ theano/tensor/opt.py
@@ -382,7 +382,7 @@ class ShapeFeature(object):
         elif s_i.type == T.lscalar:
             return s_i
         else:
-            raise TypeError('Unsupported shape element', s_i)
+            raise TypeError('Unsupported shape element', s_i, s_i.type)
 
     def set_shape(self, r, s):
         assert r not in self.shape_of
@@ -503,7 +503,6 @@ def local_shape_i_lift(node):
     inode = input.owner
     if inode and isinstance(inode.op, Elemwise) and inode.op.scalar_op.nin==1:
         return node.env.shape_feature.shape_of[input]
-
 
 register_canonicalize(local_shape_i_lift)
 register_specialize(local_shape_i_lift)
