c0794e99c4d89c495fd2fb708df57d3d0594484d
Frederic Bastien <nouiz@nouiz.org>
2010-03-09 13:58:09 -0500

    make the GpuConv op differ by its verbose option

--- theano/sandbox/cuda/blas.py
+++ theano/sandbox/cuda/blas.py
@@ -162,16 +162,19 @@ class GpuConv(Op):
             and self.logical_img_hw == other.logical_img_hw \
             and self.logical_kern_hw == other.logical_kern_hw \
             and self.logical_kern_align_top == other.logical_kern_align_top \
-            and self.version == other.version
+            and self.version == other.version \
+            and self.verbose == other.verbose
 
     def __hash__(self):
+        # don't use hash(self.version) as hash(-1)==-2 and hash(-2)==-2 in python! 
         return hash(type(self)) \
             ^ hash(self.border_mode) \
             ^ hash(self.subsample) \
             ^ hash(self.logical_img_hw) \
             ^ hash(self.logical_kern_hw) \
             ^ hash(self.logical_kern_align_top) \
-            ^ self.version# don't use hash as hash(-1)==-2 and hash(-2)==-2 in python!
+            ^ self.version \
+            ^ self.verbose
     
     def __str__(self):
         return '%s{%s, %s, %s, %s, %s}' %(self.__class__.__name__,
