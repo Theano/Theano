09df3d2d180b895e65cf6a081e93eeada4d82bff
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2010-03-14 22:38:59 -0400

    Apply dimshuffle list during specialize too.

--- theano/tensor/opt.py
+++ theano/tensor/opt.py
@@ -218,6 +218,7 @@ def local_dimshuffle_lift(node):
             return DimShuffle(iinput.type.broadcastable, new_order, inplace).make_node(iinput).outputs
 
 register_canonicalize(local_dimshuffle_lift)
+register_specialize(local_dimshuffle_lift)
 
 
 
