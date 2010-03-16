e131aa3d7d6411053e7a182542dcccac5720baae
Frederic Bastien <nouiz@nouiz.org>
2010-03-16 13:39:46 -0400

    added a first version of a script to check that blas work and what version is used.

---
+++ theano/misc/check_blas.py
@@ -0,0 +1,24 @@
+import theano,numpy,time
+import theano.tensor as T
+
+shapes=(2000,2000)
+iters = 10
+
+
+a=T.matrix()
+b=T.matrix()
+
+c=theano.shared(numpy.ones(shapes))
+f=theano.function([a,b],updates={c:0.4*c+.8*T.dot(a,b)})
+print 'blas.ldflags=',theano.config.blas.ldflags
+print 'compiledir=',theano.config.compiledir
+print f.maker.env.toposort()
+
+av=numpy.ones(shapes)
+bv=numpy.ones(shapes)
+t0=time.time()
+for i in range(iters):
+    f(av,bv)
+
+print 'times=%.3fs'%(time.time()-t0)
+
