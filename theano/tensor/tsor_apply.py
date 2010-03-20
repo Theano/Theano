bcd2bfeae881e0156841f957e790b33dde554308
James Bergstra <bergstrj@iro.umontreal.ca>
2010-03-20 19:04:01 -0400

    adding tsor_apply file

---
+++ theano/tensor/tsor_apply.py
@@ -0,0 +1,35 @@
+"""Apply for use with Tensors that implements shape propagation via variable.tag.shape
+"""
+import sys
+from theano import gof
+
+def ishape(v):
+    try:
+        return (True, v.tag.shape)
+    except AttributeError:
+        return (False, (None,)*v.type.ndim)
+
+
+class Apply(gof.Apply):
+    def __init__(self, op, inputs, outputs):
+        super(Apply, self).__init__(op, inputs, outputs)
+        # if any input has any shape info, then propagate it
+        try:
+            provided, ishapes = zip(*[ishape(i) for i in inputs])
+        except AttributeError:
+            # i.type.ndim didn't make sense for some i
+            return
+        if provided == [False for i in inputs]:
+            # no input had a tag.shape
+            return
+        try:
+            infer_shape = op.infer_shape
+        except AttributeError:
+            # op has no infer_shape, that's fine
+            return
+
+        oshapes = infer_shape(self, ishapes)
+        for o, oshp in zip(outputs, oshapes):
+            o.tag.shape = oshp
+
+
