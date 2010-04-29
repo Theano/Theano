87abce94627a67b8e73a4090ef5e618765dfa078
Frederic Bastien <nouiz@nouiz.org>
2010-04-29 14:10:20 -0400

    allow to test with cpu and gpu.

--- theano/misc/check_blas.py
+++ theano/misc/check_blas.py
@@ -11,25 +11,28 @@ shapes=(2000,2000)
 iters = 10
 
 
-a=T.matrix()
-b=T.matrix()
+a=theano.shared(numpy.ones(shapes, dtype=theano.config.floatX))
+b=theano.shared(numpy.ones(shapes, dtype=theano.config.floatX))
+c=theano.shared(numpy.ones(shapes, dtype=theano.config.floatX))
 
-c=theano.shared(numpy.ones(shapes))
-f=theano.function([a,b],updates={c:0.4*c+.8*T.dot(a,b)})
+f=theano.function([],updates={c:0.4*c+.8*T.dot(a,b)})
 print 'blas.ldflags=',theano.config.blas.ldflags
 print 'compiledir=',theano.config.compiledir
+if any( [x.op.__class__.__name__=='Gemm' for x in f.maker.env.toposort()]):
+    print 'Used the cpu'
+elif any( [x.op.__class__.__name__=='GpuGemm' for x in f.maker.env.toposort()]):
+    print 'Used the gpu'
+
 print f.maker.env.toposort()
 
-av=numpy.ones(shapes)
-bv=numpy.ones(shapes)
 t0=time.time()
 for i in range(iters):
-    f(av,bv)
-
+    f()
+print
 print 'times=%.2fs'%(time.time()-t0)
 
 print """
-Some result that you can compare again:
+Some result that you can compare again on the cpu:
 
 Lib: goto
 computer: Intel(R) Xeon(R) CPU E5430  @ 2.66GHz
