8c6975e2892c933131138c3e6454e8789f3f3fb7
Frederic Bastien <nouiz@nouiz.org>
2010-06-21 09:48:22 -0400

    fix tests in FAST_COMPILE

--- theano/sandbox/test_neighbours.py
+++ theano/sandbox/test_neighbours.py
@@ -4,18 +4,22 @@ from theano import shared, function
 import theano.tensor as T
 from neighbours import images2neibs, neibs2images
 
+mode = theano.config.mode
+if mode=="FAST_COMPILE":
+    mode='FAST_RUN'
+
 def neibs_test():
     
     shape = (100,40,18,18)
     images = shared(arange(prod(shape), dtype='float32').reshape(shape))
     neib_shape = T.as_tensor_variable((2,2))#(array((2,2), dtype='float32'))
 
-    f = function([], images2neibs(images, neib_shape))
+    f = function([], images2neibs(images, neib_shape), mode=mode)
 
     #print images.value
     neibs = f()
     #print neibs
-    g = function([], neibs2images(neibs, neib_shape, images.shape))
+    g = function([], neibs2images(neibs, neib_shape, images.shape), mode=mode)
     
     #print g()
     assert allclose(images.value,g())
