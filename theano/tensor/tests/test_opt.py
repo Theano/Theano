3ce227ad23ed125ed485fa1f61a6acc255576380
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2010-08-03 21:03:07 -0400

    Use random values instead of fixed ones in test.

--- theano/tensor/tests/test_opt.py
+++ theano/tensor/tests/test_opt.py
@@ -1622,6 +1622,7 @@ class T_local_sum_dimshuffle(unittest.TestCase):
         a_val = rng.randn(2,2)
         b_val = rng.randn(2)
         c_val = rng.randn(2,2,2)
+        d_val = rng.randn()
 
         for i,s in enumerate(sums):
             print i
@@ -1630,7 +1631,7 @@ class T_local_sum_dimshuffle(unittest.TestCase):
             g = f.maker.env.toposort()
             #print 'g =', g
             assert isinstance(g[-1].op.scalar_op, theano.scalar.basic.TrueDiv)
-            f([[1,2],[3,4]],[5,6],[[[7,8],[9,10]],[[11,12],[13,14]]],15)
+            f(a_val, b_val, c_val, d_val)
 
     # TODO:
     # test_local_sum_prod_dimshuffle (a * b * c)
