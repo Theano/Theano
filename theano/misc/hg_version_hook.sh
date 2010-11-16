fd289d1aa21cc368d57acfd74b8979a386e6ca96
James Bergstra <bergstrj@iro.umontreal.ca>
2010-11-16 17:43:37 -0500

    tweak to hg_version_hook to do nothing when no HG_NODE is given

--- theano/misc/hg_version_hook.sh
+++ theano/misc/hg_version_hook.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # Script to update version.py in response to Mercurial hooks. This should
 # not appear in a release tarball.
-
-sed -e "s/^hg_revision.*/hg_revision = '`expr substr $HG_NODE 1 12`'/" theano/version.py >theano/version.py.out && mv theano/version.py.out theano/version.py
+if [ -z "$HG_NODE" ] ; then
+    echo No HG_NODE, skipping update of theano.__version__
+    exit 0 # this seems to be normal sometimes
+else
+    sed -e "s/^hg_revision.*/hg_revision = '`expr substr $HG_NODE 1 12`'/" theano/version.py >theano/version.py.out && mv theano/version.py.out theano/version.py
+fi
 
