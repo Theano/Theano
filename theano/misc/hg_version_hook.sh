6ac049d615b0e9f44558b94f282fe37a97b2b7ca
Josh Bleecher Snyder <josharian@gmail.com>
2010-11-18 09:18:15 -0500

    Make hook more portable (expr didn't support substr on OS X)

--- theano/misc/hg_version_hook.sh
+++ theano/misc/hg_version_hook.sh
@@ -5,6 +5,6 @@ if [ -z "$HG_NODE" ] ; then
     echo No HG_NODE, skipping update of theano.__version__
     exit 0 # this seems to be normal sometimes
 else
-    sed -e "s/^hg_revision.*/hg_revision = '`expr substr $HG_NODE 1 12`'/" theano/version.py >theano/version.py.out && mv theano/version.py.out theano/version.py
+    sed -e "s/^hg_revision.*/hg_revision = '`python -c \"print \\"$HG_NODE\\"[0:12]\"`'/" theano/version.py >theano/version.py.out && mv theano/version.py.out theano/version.py
 fi
 
