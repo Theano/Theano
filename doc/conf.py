95dc51ba1d820a7c079cc2ee4aa7921d59e5f934
David Warde-Farley <wardefar@iro.umontreal.ca>
2010-11-23 15:33:44 -0500

    version = 0.3.0 in conf.py, and 2008--2009 to 2008--2010 for copyright year.

--- doc/conf.py
+++ doc/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'Theano'
-copyright = '2008--2009, LISA lab'
+copyright = '2008--2010, LISA lab'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
@@ -53,7 +53,7 @@ copyright = '2008--2009, LISA lab'
 # The short X.Y version.
 version = '0.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.3'
+release = '0.3.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
