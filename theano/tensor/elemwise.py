1a308f4711f9a573a56271728037ad36267ab7df
fsavard <devnull@localhost>
2010-11-26 09:01:49 -0500

    Removed old code

--- theano/tensor/elemwise.py
+++ theano/tensor/elemwise.py
@@ -1178,23 +1178,6 @@ class Prod(CAReduce):
 
         return [Elemwise(scalar.true_div)(p_gz, x)]
 
-        # fill a matrix with the same shape as x by broadcasting
-        # values taken from gz, which has the same shape as the output
-        # of prod().
-        #gz_filled_x = Elemwise(scalar.second)(x, 
-        #                DimShuffle(gz.type.broadcastable, new_dims)(gz))
-
-        # do the same with the output of prod, by broadcasting along
-        # axises where the product was taken
-        #prod_out_filled_x = Elemwise(scalar.second)(x, 
-        #                DimShuffle(prod_out.type.broadcastable,
-        #                            new_dims)(prod_out))
-
-        #return [theano.tensor.mul(gz_filled_x,
-        #            theano.tensor.true_div(prod_out_filled_x, x))]
-        #else:
-        #    raise NotImplementedError('Will be implemented shortly')
-
     def __str__(self):
         if self.axis is None:
             return "Prod"
