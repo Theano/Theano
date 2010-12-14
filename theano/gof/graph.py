827e7c7938402fe5c132320cf7fc3ac86c03c577
Frederic Bastien <nouiz@nouiz.org>
2010-12-14 10:06:56 -0500

    make code fit in 80 caractere line.

--- theano/gof/graph.py
+++ theano/gof/graph.py
@@ -301,13 +301,17 @@ class Variable(utils.object2):
         return cp
 
     def __lt__(self,other):
-        raise NotImplementedError('Subclasses of Variable must implement __lt__',self.__class__.__name__)
+        raise NotImplementedError('Subclasses of Variable must provide __lt__',
+                                  self.__class__.__name__)
     def __le__(self,other):
-        raise NotImplementedError('Subclasses of Variable must implement __le__',self.__class__.__name__)
+        raise NotImplementedError('Subclasses of Variable must provide __le__',
+                                  self.__class__.__name__)
     def __gt__(self,other):
-        raise NotImplementedError('Subclasses of Variable must implement __gt__',self.__class__.__name__)
+        raise NotImplementedError('Subclasses of Variable must provide __gt__',
+                                  self.__class__.__name__)
     def __ge__(self,other):
-        raise NotImplementedError('Subclasses of Variable must implement __ge__',self.__class__.__name__)
+        raise NotImplementedError('Subclasses of Variable must provide __ge__',
+                                  self.__class__.__name__)
 
 class Value(Variable):
     """
