cf49d2bcc1aaa56634edb0ae11c5b0ad3e0da23e
Josh Bleecher Snyder <josharian@gmail.com>
2011-01-29 11:01:59 -0800

    Use warnings module to issue DeprecationWarning instead of printing to stderr

--- theano/compile/sandbox/__init__.py
+++ theano/compile/sandbox/__init__.py
@@ -1,4 +1,5 @@
-import sys
-print >> sys.stderr, "DEPRECATION: theano.compile.sandbox no longer provides shared, shared_constructor, and pfunc.  They have been moved to theano.compile."
+import warnings
+warnings.warn("theano.compile.sandbox no longer provides shared, shared_constructor, and pfunc.  They have been moved to theano.compile.", DeprecationWarning)
+
 from theano.compile.sharedvalue import shared, shared_constructor
 from theano.compile.pfunc import pfunc
