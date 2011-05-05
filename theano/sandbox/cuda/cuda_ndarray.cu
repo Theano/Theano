ed10bc1479995dac54cf7f314a3719ad4cdf591d
Frederic Bastien <nouiz@nouiz.org>
2011-05-05 09:29:00 -0400

    small compilation fix.

--- theano/sandbox/cuda/cuda_ndarray.cu
+++ theano/sandbox/cuda/cuda_ndarray.cu
@@ -636,9 +636,9 @@ PyObject * CudaNdarray_Reshape(CudaNdarray * self, PyObject * shape)
     }
     if (rval_size==0)
     {
-        CudaNdarray * rval = CudaNdarray_NewDims(rval_nd, rval_dims);
+        PyObject * rval = CudaNdarray_NewDims(rval_nd, rval_dims);
         free(rval_dims);
-        return (PyObject*) rval;
+        return rval;
     }
 
     if(CudaNdarray_is_c_contiguous(self))
