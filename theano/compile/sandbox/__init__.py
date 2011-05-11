8751b7da6c755acdc866bf46e3ab99fae7747212
Olivier Delalleau <delallea@iro>
2011-05-11 16:41:18 -0400

    Removed shared / shared_constructor / pfunc from compile.sandbox, please use theano.shared / theano.function instead

--- theano/compile/sandbox/__init__.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.warn("theano.compile.sandbox no longer provides shared, shared_constructor, and pfunc.  They have been moved to theano.compile.", DeprecationWarning)
-
-from theano.compile.sharedvalue import shared, shared_constructor
-from theano.compile.pfunc import pfunc
