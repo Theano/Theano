7f1f4d9e0e521d26ad7b59226a1b2b1ef2732e2e
Olivier Delalleau <delallea@iro>
2011-05-17 14:37:02 -0400

    Improved script running tests individually to allow batches of tests

--- theano/tests/run_individual_tests.py
+++ theano/tests/run_individual_tests.py
@@ -17,12 +17,17 @@ This script performs three tasks:
 One reason to use this script is if you are a Windows user, and see errors like
 "Not enough storage is available to process this command" when trying to simply
 run `nosetests` in your Theano installation directory.
-By using this script, nosetests is run on each test individually, which is
+By using this script, nosetests is run on each test individually, which is much
 slower but should at least let you run the test suite.
 
-Note that this script is experimental and might not actually be running the
-whole test suite (it is suspicious that the first step reports more tests than
-the amount of tests found in the resulting '.noseids' file).
+Note that this script is a work-in-progress and is not fully functional at this
+point: the way some tests are defined in the Theano test-suite seems to confuse
+nosetests' TestID module, probably leading to not running all tests, as well as
+to some unexpected test failures.
+
+You can also provide a single command-line argument, which should be an integer
+number N (default = 1), in order to run batches of N tests rather than run tests
+one at a time. It will be faster (but may fail under Windows if N is too large).
 """
 
 
@@ -45,11 +50,28 @@ def main():
     ids = data['ids']
     n_tests = len(ids)
     # Run tests.
-    for test_id in xrange(1, n_tests + 1):
-        subprocess.call(['nosetests', '-v', '--with-id', str(test_id)])
-    # Re-run only failed tests
-    subprocess.call(['nosetests', '-v', '--failed'])
-    return 0
+    n_batch = 1
+    if len(sys.argv) >= 2:
+        n_batch = int(sys.argv[1])
+    has_error = 0
+    for test_id in xrange(1, n_tests + 1, n_batch):
+        test_range = range(test_id, min(test_id + n_batch, n_tests + 1))
+        rval = subprocess.call(['nosetests', '-v', '--with-id'] +
+                               map(str, test_range))
+        has_error += rval
+    if has_error:
+        # Re-run only failed tests
+        print """\
+###########################
+# RE-RUNNING FAILED TESTS #
+###########################"""
+        subprocess.call(['nosetests', '-v', '--failed'])
+        return 0
+    else:
+        print """\
+####################
+# ALL TESTS PASSED #
+####################"""
 
 
 if __name__ == '__main__':
