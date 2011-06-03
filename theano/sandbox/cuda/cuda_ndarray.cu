6cbeca31b68527a8c0b804416cf343f6402aa77e
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2011-06-03 19:06:47 -0400

    Revert 14919017ced62de0e2a9c090f3451c0cb5c311e9
    There was no memory leak.

--- theano/sandbox/cuda/cuda_ndarray.cu
+++ theano/sandbox/cuda/cuda_ndarray.cu
@@ -328,7 +328,6 @@ PyObject * CudaNdarray_CreateArrayObj(CudaNdarray * self)
     if (CUBLAS_STATUS_SUCCESS != cublasGetError())
     {
         PyErr_SetString(PyExc_RuntimeError, "error copying data to host");
-        Py_DECREF(contiguous_self);
         Py_DECREF(rval);
         rval = NULL;
     }
