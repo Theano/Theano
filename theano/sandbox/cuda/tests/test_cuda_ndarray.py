770f9d2cd4f0b5910036d9c67f319524caa966a5
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2011-06-03 21:19:39 -0400

    Add test for new behaviour of the .base attribute of CudaNdarray

--- theano/sandbox/cuda/tests/test_cuda_ndarray.py
+++ theano/sandbox/cuda/tests/test_cuda_ndarray.py
@@ -816,6 +816,24 @@ def test_zeros_basic_vector():
     assert numpy.allclose(numpy.asarray(_a), numpy.zeros((300,)))
 
 
+def test_base():
+    # Test that the 'base' attribute of a CudaNdarray is the one
+    # built initially, not an intermediate one.
+    a = cuda_ndarray.CudaNdarray.zeros((3,4,5))
+    for i in xrange(5):
+        b = a[:]
+    assert b.base is a
+
+    c = a[0]
+    d = c[:,0]
+    print d.shape
+    assert c.base is a
+    assert d.base is a
+
+    e = b.reshape((5,2,2,3))
+    assert e.base is a
+
+
 if __name__ == '__main__':
     test_zeros_basic_3d_tensor()
     test_zeros_basic_vector()
