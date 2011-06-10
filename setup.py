ab5f6a7485829290060b87c9276a54c0124e320c
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2011-06-09 23:18:25 -0400

    modifications for 0.4.0rc3 release

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ PLATFORMS           = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"]
 MAJOR               = 0
 MINOR               = 4
 MICRO               = 0
-SUFFIX              = "rc2"  # Should be blank except for rc's, betas, etc.
+SUFFIX              = "rc3"  # Should be blank except for rc's, betas, etc.
 ISRELEASED          = False
 
 VERSION             = '%d.%d.%d%s' % (MAJOR, MINOR, MICRO, SUFFIX)
