ea89ea631a70431b3e4e8820188aa0ff056278cb
Olivier Delalleau <delallea@iro>
2011-06-13 16:46:30 -0400

    Always convert compiledir to real path to avoid relative paths and symlinks mismatch

--- theano/gof/compiledir.py
+++ theano/gof/compiledir.py
@@ -4,7 +4,7 @@ import os, sys
 import platform
 import re
 
-from theano.configparser import config, AddConfigVar, StrParam
+from theano.configparser import config, AddConfigVar, ConfigParam, StrParam
 
 def default_compiledirname():
     platform_id = '-'.join([
@@ -14,7 +14,15 @@ def default_compiledirname():
     platform_id = re.sub("[\(\)\s]+", "_", platform_id)
     return 'compiledir_' + platform_id
 
-def is_valid_compiledir(path):
+
+def filter_compiledir(path):
+    # Turn path into the 'real' path. This ensures that:
+    #   1. There is no relative path, which would fail e.g. when trying to
+    #      import modules from the compile dir.
+    #   2. The path is stable w.r.t. e.g. symlinks (which makes it easier
+    #      to re-use compiled modules).
+    path = os.path.realpath(path)
+    valid = True
     if not os.access(path, os.R_OK | os.W_OK):
         try:
             os.makedirs(path, 0770) #read-write-execute for this user only
@@ -22,17 +30,22 @@ def is_valid_compiledir(path):
             # Maybe another parallel execution of theano was trying to create
             # the same directory at the same time.
             if e.errno != errno.EEXIST:
-                return False
+                valid = False
 
-    try:
-        # PROBLEM: sometimes the first approach based on os.system('touch')
-        # returned -1 for an unknown reason; the alternate approach here worked
-        # in all cases... it was weird.
-        open(os.path.join(path, '__init__.py'), 'w').close()
-    except:
-        return False
+    if valid:
+        try:
+            # PROBLEM: sometimes the initial approach based on
+            # os.system('touch') returned -1 for an unknown reason; the
+            # alternate approach here worked in all cases... it was weird.
+            open(os.path.join(path, '__init__.py'), 'w').close()
+        except:
+            valid = False
 
-    return True
+    if not valid:
+        raise ValueError('Invalid value for compiledir: %s' % path)
+
+    return path
+
 
 AddConfigVar('base_compiledir',
         "arch-independent cache directory for compiled modules",
@@ -40,9 +53,9 @@ AddConfigVar('base_compiledir',
 
 AddConfigVar('compiledir',
         "arch-dependent cache directory for compiled modules",
-        StrParam(
+        ConfigParam(
             os.path.join(
                 os.path.expanduser(config.base_compiledir),
                 default_compiledirname()),
-            is_valid=is_valid_compiledir,
+            filter=filter_compiledir,
             allow_override=False))
