9fd1311488d1c2f29a94abeabb6ac8cb1f48e05f
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2011-06-27 19:12:50 -0400

    Revert 9024e481d2b5 now that numpy+floatX is not the default.

--- theano/sandbox/cuda/tests/test_opt.py
+++ theano/sandbox/cuda/tests/test_opt.py
@@ -35,28 +35,18 @@ def test_no_shared_var_graph():
     assert numpy.any(isinstance(x.op,cuda.HostFromGpu) for x in l)
 
 def test_int_pow():
-    # This is to ensure that '4' does not upcast to float64.
-    if config.cast_policy == 'numpy+floatX':
-        floatX_backup = config.floatX
-        config.floatX = 'float32'
+    a = CudaNdarrayType([False])()
 
-    try:
-        a = CudaNdarrayType([False])()
+    f = theano.function([a], (a*4).sum(), mode=mode_with_gpu)
 
-        f = theano.function([a], (a*4).sum(), mode=mode_with_gpu)
+    op_names = [n.op.__class__.__name__ for n in f.maker.env.toposort()]
+    assert op_names == ['GpuSum', 'GpuElemwise', 'HostFromGpu']
 
-        op_names = [n.op.__class__.__name__ for n in f.maker.env.toposort()]
-        assert op_names == ['GpuSum', 'GpuElemwise', 'HostFromGpu']
+    f = theano.function([a], tensor.pow(a,4).sum(), mode=mode_with_gpu)
+    op_names = [n.op.__class__.__name__ for n in f.maker.env.toposort()]
+    assert op_names == ['GpuElemwise', 'GpuSum', 'HostFromGpu']
 
-        f = theano.function([a], tensor.pow(a,4).sum(), mode=mode_with_gpu)
-        op_names = [n.op.__class__.__name__ for n in f.maker.env.toposort()]
-        assert op_names == ['GpuElemwise', 'GpuSum', 'HostFromGpu']
-
-        #theano.printing.debugprint(f)
-
-    finally:
-        if config.cast_policy == 'numpy+floatX':
-            config.floatX = floatX_backup
+    #theano.printing.debugprint(f)
 
 def test_gpualloc():
     '''
