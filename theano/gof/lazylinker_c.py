2f197df77845907941191e7df0689d855841784b
Olivier Delalleau <delallea@iro>
2011-07-06 16:53:48 -0400

    Reload the lazylinker lib instead of recompiling it on every import.
    This is especially important under Windows, where one cannot overwrite a
    library currently in use.

--- theano/gof/lazylinker_c.py
+++ theano/gof/lazylinker_c.py
@@ -1,20 +1,34 @@
-import os
+import os, sys
+
 import theano
 from theano import config
 from theano.gof.compilelock import get_lock, release_lock
 from theano.gof import cmodule
 
-get_lock()
-try:
-    dirname = 'lazylinker_ext'
-    cfile = os.path.join(theano.__path__[0], 'gof', 'lazylinker_c.c')
-    code = open(cfile).read()
-    loc = os.path.join(config.compiledir, dirname)
-    if not os.path.exists(loc):
-        os.mkdir(loc)
-    cmodule.gcc_module_compile_str(dirname, code, location=loc)
-    from lazylinker_ext.lazylinker_ext import *
-finally:
-    # Release lock on compilation directory.
-    release_lock()
+# Ensure the compiledir is in `sys.path` to be able to reload an existing
+# precompiled library.
+if config.compiledir not in sys.path:
+    sys.path.append(config.compiledir)
 
+try:
+    import lazylinker_ext
+except ImportError:
+    get_lock()
+    try:
+        # Maybe someone else already finished compiling it while we were
+        # waiting for the lock?
+        try:
+            import lazylinker_ext
+        except ImportError:
+            dirname = 'lazylinker_ext'
+            cfile = os.path.join(theano.__path__[0], 'gof', 'lazylinker_c.c')
+            code = open(cfile).read()
+            loc = os.path.join(config.compiledir, dirname)
+            if not os.path.exists(loc):
+                os.mkdir(loc)
+            cmodule.gcc_module_compile_str(dirname, code, location=loc)
+    finally:
+        # Release lock on compilation directory.
+        release_lock()
+
+from lazylinker_ext.lazylinker_ext import *
