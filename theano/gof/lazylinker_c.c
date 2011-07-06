45a0790ad3277421e7951efa620a0722f32115e5
James Bergstra <bergstrj@iro.umontreal.ca>
2011-07-06 17:14:20 -0400

    adding Py_ssize_t typedef for older pythons to lazylinker_c

--- theano/gof/lazylinker_c.c
+++ theano/gof/lazylinker_c.c
@@ -1,6 +1,15 @@
 #include <Python.h>
 #include "structmember.h"
 #include <sys/time.h>
+
+// Old Python compatibility from here:
+// http://www.python.org/dev/peps/pep-0353/
+#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
+typedef int Py_ssize_t;
+#define PY_SSIZE_T_MAX INT_MAX
+#define PY_SSIZE_T_MIN INT_MIN
+#endif
+
 /**
 
 TODO: 
