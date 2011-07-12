527466c6ca5e74e80eaddb29db17e55c87858d86
Frederic Bastien <nouiz@nouiz.org>
2011-07-12 09:48:43 -0400

    Added comment and coding style directive.

--- theano/sandbox/cuda/cuda_ndarray.cuh
+++ theano/sandbox/cuda/cuda_ndarray.cuh
@@ -79,6 +79,7 @@ struct CudaNdarray
 
 /*
  * Return a CudaNdarray whose 'nd' dimensions are all 0.
+ * if nd==-1, it is not initialized.
  */
 PyObject *
 CudaNdarray_New(int nd=-1);
@@ -550,3 +551,14 @@ void fprint_CudaNdarray(FILE * fd, const CudaNdarray *self)
 }
 
 #endif
+/*
+  Local Variables:
+  mode:c++
+  c-basic-offset:4
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0))
+  indent-tabs-mode:nil
+  fill-column:79
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:encoding=utf-8:textwidth=79 :
