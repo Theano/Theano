a7ab337e5fc5afd129c39b2017d8a68f5bddc2fd
Frederic Bastien <nouiz@nouiz.org>
2011-07-13 12:50:25 -0400

    Make code more clear following code review.

--- theano/sparse/__init__.py
+++ theano/sparse/__init__.py
@@ -4,7 +4,7 @@ import theano.scipy
 enable_sparse = False
 if theano.scipy.scipy_available:
     import scipy
-    if not scipy.__version__ < '0.7':
+    if scipy.__version__ >= '0.7':
         enable_sparse = True
     else:
         sys.stderr.write("WARNING: scipy version = %s. We request version >=0.7.0 for the sparse code as it has bugs fixed in the sparse matrix code.\n" % scipy.__version__)
