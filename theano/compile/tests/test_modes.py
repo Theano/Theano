b52f16ffcb403abfde629f2a522191d1f3790256
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2011-07-15 22:41:03 -0400

    Enumerate modes and linkers to test. Remove printing of profile infos.

--- theano/compile/tests/test_modes.py
+++ theano/compile/tests/test_modes.py
@@ -5,6 +5,7 @@ import unittest
 
 import theano
 import theano.tensor as T
+from theano.compile import Mode, ProfileMode
 
 
 class T_bunch_of_modes(unittest.TestCase):
@@ -13,15 +14,25 @@ class T_bunch_of_modes(unittest.TestCase):
         # this is a quick test after the LazyLinker branch merge
         # to check that all the current modes can still be used.
         linker_classes_involved = []
-        for modename in theano.config.__class__.__dict__['mode'].all:
+
+        predef_modes = ['FAST_COMPILE', 'FAST_RUN', 'DEBUG_MODE']
+        # Use a new instance of ProfileMode instead of 'PROFILE_MODE' to
+        # avoid printing a profile mode summary in nose output
+        predef_modes.append(ProfileMode())
+
+        # Linkers to use with regular Mode
+        linkers = ['c|py', 'c|py_nogc', 'vm', 'vm_nogc', 'cvm', 'cvm_nogc']
+        modes = predef_modes + [Mode(linker, 'fast_run') for linker in linkers]
+
+        for mode in modes:
 
             x = T.matrix()
             y = T.vector()
-            f = theano.function([x,y], x+y, mode=modename)
+            f = theano.function([x,y], x+y, mode=mode)
             # test that it runs something
             f([[1,2],[3,4]], [5, 6])
             linker_classes_involved.append(f.maker.mode.linker.__class__)
-            print 'MODE:', modename, f.maker.mode.linker, 'stop'
+            print 'MODE:', mode, f.maker.mode.linker, 'stop'
         # regression check:
         # there should be
         # - VM_Linker
