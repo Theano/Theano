32cb5be26d2261fce37134e091a6c1548b74cac0
Frederic Bastien <nouiz@nouiz.org>
2011-07-21 16:46:33 -0400

    Hide and check that the warning is printed.

--- theano/tests/test_printing.py
+++ theano/tests/test_printing.py
@@ -4,15 +4,31 @@ This is a REALLY PARTIAL TEST.
 I did them to help debug stuff.
 
 """
-
+import logging
+import StringIO
 
 import theano
 import theano.tensor as tensor
 
 
 def test_pydotprint_cond_highlight():
+    assert len(theano.theano_logger.handlers) == 1
+
     x = tensor.dvector()
     f = theano.function([x], x*2)
     f([1,2,3,4])
 
-    theano.printing.pydotprint(f, cond_highlight = True)
+    s = StringIO.StringIO()
+    new_handler = logging.StreamHandler(s)
+    new_handler.setLevel(logging.DEBUG)
+    orig_handler = theano.theano_logger.handlers[0]
+
+    theano.theano_logger.removeHandler(orig_handler)
+    theano.theano_logger.addHandler(new_handler)
+    try:
+        theano.printing.pydotprint(f, cond_highlight = True)
+    finally:
+        theano.theano_logger.addHandler(orig_handler)
+        theano.theano_logger.removeHandler(new_handler)
+
+    assert s.getvalue() == 'pydotprint: cond_highlight is set but there is no IfElse node in the graph\n'
