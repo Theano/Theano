57a53223bb61cce7b4cdd3fb572b4791dddac5fd
James Bergstra <james.bergstra@gmail.com>
2011-08-24 16:22:15 -0400

    removing overly strict type check in AdvancedSubtensor1.make_node

--- theano/tensor/basic.py
+++ theano/tensor/basic.py
@@ -4863,10 +4863,6 @@ class AdvancedSubtensor1(Op):
             raise TypeError('index must be vector')
         if x_.type.ndim == 0:
             raise TypeError('cannot index into a scalar')
-        if x_.type.broadcastable[0]:
-            # the caller should have made a copy of x len(ilist) times
-            raise TypeError('cannot index into a broadcastable dimension')
-
         return Apply(self, [x_, ilist_], [x_.type()])
 
     def perform(self, node, inp, out_):
