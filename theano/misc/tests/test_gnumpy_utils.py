0250e5eef8ff723ed90b73bcbf33153ceba91f12
Frederic <nouiz@nouiz.org>
2011-09-26 17:05:28 -0400

    Added the test in reverse order.

--- theano/misc/tests/test_gnumpy_utils.py
+++ theano/misc/tests/test_gnumpy_utils.py
@@ -1,3 +1,5 @@
+import numpy
+
 import theano
 from theano.misc.gnumpy_utils import gnumpy_available
 
@@ -20,8 +22,12 @@ Make sure that the gnumpy conversion is exact.
 
     A = gnumpy.rand(*shape)
     A_cnd = garray_to_cudandarray(A)
+    assert A_cnd.shape == A.shape
+#    assert A_cnd.dtype == A_gar.dtype # dtype always float32
+#    assert A_cnd._strides == A_gar.strides, garray don't have strides
     B_cnd = ii(A_cnd)
     B = cudandarray_to_garray(B_cnd)
+    assert A_cnd.shape == A.shape
     from numpy import array
     B2 = array(B_cnd)
 
@@ -30,3 +36,30 @@ Make sure that the gnumpy conversion is exact.
     w = B2
     assert abs(u-v).max() == 0
     assert abs(u-w).max() == 0
+
+def test2(shape=(3,4,5)):
+    """
+Make sure that the gnumpy conversion is exact.
+"""
+    gpu = theano.sandbox.cuda.basic_ops.gpu_from_host
+    U = gpu(theano.tensor.ftensor3('U'))
+    ii = theano.function([U], gpu(U+1))
+
+
+    A = numpy.random.rand(*shape).astype('float32')
+    A_cnd = theano.sandbox.cuda.CudaNdarray(A)
+    A_gar = cudandarray_to_garray(A_cnd)
+    assert A_cnd.shape == A_gar.shape
+#    assert A_cnd.dtype == A_gar.dtype # dtype always float32
+#    assert A_cnd._strides == A_gar.strides, garray don't have strides
+
+    B = garray_to_cudandarray(A_gar)
+    from numpy import array
+    B2 = array(B)
+
+    assert A_cnd.shape == B.shape
+#    assert A_cnd.dtype == B.dtype # dtype always float32
+    assert A_cnd._strides == B._strides
+    assert A_cnd.gpudata == B.gpudata
+    v = numpy.asarray(B)
+    assert abs(v-A).max() == 0
