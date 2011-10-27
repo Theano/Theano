407386ae396ea242f5853362bd3e2940da066b2c
Frederic <nouiz@nouiz.org>
2011-10-27 14:26:49 -0400

    small fix. We want "8", not "8:".

--- theano/misc/check_blas_many.sh
+++ theano/misc/check_blas_many.sh
@@ -11,7 +11,7 @@ TIME_PREFIX=time
 VAR=OMP_NUM_THREADS
 echo "numpy gemm take="
 THEANO_FLAGS=blas.ldflags= $TIME_PREFIX python misc/check_blas.py --quiet
-for i in 1 2 4 8:
+for i in 1 2 4 8
 do
   export $VAR=$i
   x=`$TIME_PREFIX python misc/check_blas.py --quiet`
