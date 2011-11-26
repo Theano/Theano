Theano/Theano

commit a6e1769d0e2970cf4b8171b44c51e3902ca09b29

Review spectral_radius_bound: added other exceptions for the exponent

theano/sandbox/linalg/ops.py
@@ -695,6 +695,10 @@ def spectral_radius_bound(X, log2_exponent):
     """
     if X.type.ndim != 2:
         raise TypeError('spectral_radius_bound requires a matrix argument', X)
+    if not(isintance(log2_exponent,int)):
+        raise TypeError('spectral_radius_bound requires a integer exponent', log2_exponent)
+    if log2_exponent<=0:
+        raise ValueError('spectral_radius_bound requires a strictly positive exponent', log2_exponent)
     XX = X
     for i in xrange(log2_exponent):
         XX = tensor.dot(XX, XX)

commit 6bdc6dd2f26446a133b5175cd9e5181503a3be31

Fixed test_linalg.py:test_spectral_radius_bound
Was broken by a6e1769.
This fix also includes some coding style fixes.

theano/sandbox/linalg/ops.py
@@ -695,9 +695,9 @@ def spectral_radius_bound(X, log2_exponent):
     """
     if X.type.ndim != 2:
         raise TypeError('spectral_radius_bound requires a matrix argument', X)
-    if not(isintance(log2_exponent,int)):
+    if not isinstance(log2_exponent, int):
         raise TypeError('spectral_radius_bound requires a integer exponent', log2_exponent)
-    if log2_exponent<=0:
+    if log2_exponent <= 0:
         raise ValueError('spectral_radius_bound requires a strictly positive exponent', log2_exponent)
     XX = X
     for i in xrange(log2_exponent):