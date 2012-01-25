2b1e54ed0c41d08d21d013e5dd2e41c0fb4d36b5
Olivier Delalleau <delallea@iro>
2012-01-25 11:25:25 -0500

    Do not fall into pdb while running the test suite

--- theano/sandbox/scan_module/tests/test_scan.py
+++ theano/sandbox/scan_module/tests/test_scan.py
@@ -300,12 +300,14 @@ class TestScan(unittest.TestCase):
                     try:
                         assert numpy.allclose(th_out, num_out)
                     except:
-                        import ipdb; ipdb.set_trace()
+                        #import ipdb; ipdb.set_trace()
+                        raise
                 for th_out, num_out in zip(shared_vars, numpy_shared):
                     try:
                         assert numpy.allclose(th_out.get_value(), num_out)
                     except:
-                        import ipdb; ipdb.set_trace()
+                        #import ipdb; ipdb.set_trace()
+                        raise
                 # Scenario 2 : Loose fit (sequences longer then required)
                 print >>sys.stderr, '       Scenario 2. Loose shapes'
                 input_values = []
