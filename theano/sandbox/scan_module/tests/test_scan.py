83cab8bfcc33bd74a109bd3b4eafd0a18c0f4dbb
Olivier Delalleau <delallea@iro>
2012-01-25 11:19:14 -0500

    Added short text description to KnownFailureTests

--- theano/sandbox/scan_module/tests/test_scan.py
+++ theano/sandbox/scan_module/tests/test_scan.py
@@ -470,7 +470,8 @@ class TestScan(unittest.TestCase):
         # place (even when told not to by DebugMode). As this op will change
         # soon, and it is in the sandbox and not for user consumption, the
         # error is marked as KnownFailure
-        raise KnownFailureTest
+        raise KnownFailureTest('Work-in-progress sandbox ScanOp is not fully '
+                               'functional yet')
         def f_pow2(x_tm1):
             return 2 * x_tm1
 
@@ -505,7 +506,8 @@ class TestScan(unittest.TestCase):
         # place (even when told not to by DebugMode). As this op will change
         # soon, and it is in the sandbox and not for user consumption, the
         # error is marked as KnownFailure
-        raise KnownFailureTest
+        raise KnownFailureTest('Work-in-progress sandbox ScanOp is not fully '
+                               'functional yet')
         def f_rnn(u_t, x_tm1, W_in, W):
             return u_t * W_in + x_tm1 * W
         u = theano.tensor.vector('u')
