1ffd3e3e900fc11ee7212a350ad6ab643cb98d7c
Frederic <nouiz@nouiz.org>
2012-02-07 10:49:14 -0500

    Add a function to hash ndarray.

---
+++ theano/tensor/utils.py
@@ -0,0 +1,20 @@
+import numpy
+
+from theano.gof.cc import hash_from_code
+
+
+def hash_from_ndarray(data):
+    # We need to hash the shapes and strides as hash_from_code only hash
+    # the data buffer. Otherwise, this will cause problem with shapes likes:
+    # (1, 0) and (2, 0) and problem with inplace transpose.
+    # We also need to add the dtype to make the distinction between
+    # uint32 and int32 of zeros with the same shape and strides.
+
+    # python hash are not strong, so I always use md5. To don't have a too long
+    # hash, I call it again on the contatenation of all part.
+    if not data.flags["C_CONTIGUOUS"] and not data.flags["F_CONTIGUOUS"]:
+        data = numpy.ascontiguousarray(data)
+    return (hash_from_code(hash_from_code(data) +
+                           hash_from_code(str(data.shape)) +
+                           hash_from_code(str(data.strides)) +
+                           hash_from_code(str(data.dtype))))
