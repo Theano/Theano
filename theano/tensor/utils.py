bb26960966e82292eac47e8c19e6864a37858190
Olivier Delalleau <delallea@iro>
2012-02-07 16:25:29 -0500

    Typo fix in comment and removed useless ()

--- theano/tensor/utils.py
+++ theano/tensor/utils.py
@@ -4,7 +4,7 @@ from theano.gof.cc import hash_from_code
 
 
 def hash_from_ndarray(data):
-    # We need to hash the shapes and strides as hash_from_code only hash
+    # We need to hash the shapes and strides as hash_from_code only hashes
     # the data buffer. Otherwise, this will cause problem with shapes like:
     # (1, 0) and (2, 0) and problem with inplace transpose.
     # We also need to add the dtype to make the distinction between
@@ -14,7 +14,7 @@ def hash_from_ndarray(data):
     # too long hash, I call it again on the concatenation of all parts.
     if not data.flags["C_CONTIGUOUS"] and not data.flags["F_CONTIGUOUS"]:
         data = numpy.ascontiguousarray(data)
-    return (hash_from_code(hash_from_code(data) +
-                           hash_from_code(str(data.shape)) +
-                           hash_from_code(str(data.strides)) +
-                           hash_from_code(str(data.dtype))))
+    return hash_from_code(hash_from_code(data) +
+                          hash_from_code(str(data.shape)) +
+                          hash_from_code(str(data.strides)) +
+                          hash_from_code(str(data.dtype)))
