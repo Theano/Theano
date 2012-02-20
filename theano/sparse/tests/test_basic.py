92e0000d97468c0952a9350cf688e4ef3771b7a7
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2012-02-20 14:55:54 -0500

    Make test deterministic by using unittests.rseed

--- theano/sparse/tests/test_basic.py
+++ theano/sparse/tests/test_basic.py
@@ -833,14 +833,13 @@ class UsmmTests(unittest.TestCase):
         y_size = (100, 200)
         z_size = (x_size[0], y_size[1])
 
-        self.x = numpy.asarray(numpy.random.binomial(1, 0.5, x_size),
-                               dtype=theano.config.floatX)
-        self.y = numpy.asarray(numpy.random.uniform(-1, 1, y_size),
-                               dtype=theano.config.floatX)
-        self.z = numpy.asarray(numpy.random.uniform(-1, 1, z_size),
-                               dtype=theano.config.floatX)
-        utt.seed_rng()
         self.rng = numpy.random.RandomState(seed=utt.fetch_seed())
+        self.x = numpy.asarray(self.rng.binomial(1, 0.5, x_size),
+                               dtype=theano.config.floatX)
+        self.y = numpy.asarray(self.rng.uniform(-1, 1, y_size),
+                               dtype=theano.config.floatX)
+        self.z = numpy.asarray(self.rng.uniform(-1, 1, z_size),
+                               dtype=theano.config.floatX)
 
     def test(self):
         def mat(format, name, dtype):
