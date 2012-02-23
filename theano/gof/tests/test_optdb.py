232671a87ea2430215dc6404ee746d0adf1c2059
Frederic <nouiz@nouiz.org>
2012-02-23 16:31:23 -0500

    pep8

--- theano/gof/tests/test_optdb.py
+++ theano/gof/tests/test_optdb.py
@@ -1,11 +1,13 @@
-from theano.gof.optdb import *
 from unittest import TestCase
+
+from theano.gof.optdb import opt, DB
+
 
 class Test_DB(TestCase):
 
     def test_0(self):
 
-        class Opt(opt.Optimizer):  #inheritance buys __hash__
+        class Opt(opt.Optimizer):  # inheritance buys __hash__
             name = 'blah'
 
         db = DB()
@@ -16,7 +18,7 @@ class Test_DB(TestCase):
         db.register('c', Opt(), 'z', 'asdf')
 
         try:
-            db.register('c', Opt()) #name taken
+            db.register('c', Opt())  # name taken
             self.fail()
         except ValueError, e:
             if e[0].startswith("The name"):
@@ -27,7 +29,7 @@ class Test_DB(TestCase):
             self.fail()
 
         try:
-            db.register('z', Opt()) #name collides with tag
+            db.register('z', Opt())  # name collides with tag
             self.fail()
         except ValueError, e:
             if e[0].startswith("The name"):
@@ -38,7 +40,7 @@ class Test_DB(TestCase):
             self.fail()
 
         try:
-            db.register('u', Opt(), 'b') #name new but tag collides with name
+            db.register('u', Opt(), 'b')  # name new but tag collides with name
             self.fail()
         except ValueError, e:
             if e[0].startswith("The tag"):
@@ -47,5 +49,3 @@ class Test_DB(TestCase):
                 raise
         except Exception:
             self.fail()
-
-
