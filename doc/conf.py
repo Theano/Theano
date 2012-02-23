1ea24b6418c54a139fcbb6d5cf938caf54246a5b
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2012-02-23 16:50:42 -0500

    Modifications for 0.5 release

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ copyright = '2008--2012, LISA lab'
 # The short X.Y version.
 version = '0.5'
 # The full version, including alpha/beta/rc tags.
-release = '0.5rc2'
+release = '0.5'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
