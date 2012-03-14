2b5c33f059b6e9b66c1521a8112c7ed1e766e3a5
Razvan Pascanu <r.pascanu@gmail.com>
2012-03-14 09:08:21 -0400

    another test including sequences

--- theano/sandbox/test_scan.py
+++ theano/sandbox/test_scan.py
@@ -32,8 +32,29 @@ def test_002():
     assert numpy.all(fn(val_x0)[0] == val_x0)
 
 
+def test_003():
+    x0 = theano.tensor.fvector('x0')
+    sq = theano.tensor.fvector('sq')
+    state = theano.tensor.alloc(
+        theano.tensor.constant(numpy.float32(0)),
+        6,
+        x0.shape[0])
+    state = theano.tensor.set_subtensor(state[0], x0)
+
+    out, _ = scan.scan(lambda s, x:x+s,
+                           sequences=sq,
+                           states = state,
+                           n_steps = 5)
+    fn = theano.function([sq, x0], out)
+    val_x0 = numpy.float32([1,2,3])
+    val_sq = numpy.float32([1,2,3,4,5])
+    assert numpy.all(fn(val_sq, val_x0)[-1] == val_x0 +15)
+    assert numpy.all(fn(val_sq, val_x0)[0] == val_x0)
+
+
 if __name__=='__main__':
     test_001()
     test_002()
+    test_003()
 
 
