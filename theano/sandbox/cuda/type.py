Theano/Theano

commit ccb013559e979cfb199016ec9c8435667a638416

Add value_zeros method to {Tensor,CudaNdarray}Type
That way, we can more easily allocate an array full of zeros
of the right type.

theano/sandbox/cuda/type.py
@@ -54,6 +54,11 @@ class CudaNdarrayType(Type):
     A cyclic dependency is avoided by not hardcoding this class.
     """
 
+    value_zeros = staticmethod(cuda.CudaNdarray.zeros)
+    """
+    Create an CudaNdarray full of 0 values
+    """
+
     def __init__(self, broadcastable, name=None, dtype=None):
         if dtype != None and dtype != 'float32':
             raise TypeError('%s only supports dtype float32 for now. Tried '

theano/compile/debugmode.py
@@ -1089,15 +1089,9 @@ def _get_preallocated_maps(node, thunk, prealloc_modes, def_val,
         # Initial allocation
         init_strided = {}
         for r in node.outputs:
-            if isinstance(r.type, TensorType):
+            if isinstance(r.type, (TensorType, CudaNdarrayType)):
                 # Create a buffer twice as large in every dimension
-                new_buf = numpy.zeros(
-                        shape=[(s * 2) for s in r_vals[r].shape],
-                        dtype=r_vals[r].dtype)
-                init_strided[r] = new_buf
-
-            elif isinstance(r.type, CudaNdarrayType):
-                new_buf = CudaNdarray.zeros(
+                new_buf = r.type.value_zeros(
                         [(s * 2) for s in r_vals[r].shape])
                 init_strided[r] = new_buf
 

theano/tensor/basic.py
@@ -1024,6 +1024,13 @@ class TensorType(Type):
         else:
             return ()
 
+    def value_zeros(self, shape):
+        """
+        Create an numpy ndarray full of 0 values.
+        """
+        return numpy.zeros(shape, dtype=self.dtype)
+
+
 # Register CudaNdarrayType to the OutputGuard list of known types
 # to have OutputGuard generate C code for this type.
 theano.compile.mode.register_OutputGuard_c_code(TensorType)

commit 3e58a15d1e8dad980db3332f7044e25847579a1c

Fixed crash when cuda is not available
This crash was introduced recently by ccb0135

theano/sandbox/cuda/type.py
@@ -18,7 +18,8 @@ try:
     from theano.sandbox.cuda.nvcc_compiler import NVCC_compiler
     import cuda_ndarray
 except ImportError:
-    pass
+    # Used to know that `cuda` could not be properly imported.
+    cuda = None
 
 
 class CudaNdarrayType(Type):
@@ -54,7 +55,8 @@ class CudaNdarrayType(Type):
     A cyclic dependency is avoided by not hardcoding this class.
     """
 
-    value_zeros = staticmethod(cuda.CudaNdarray.zeros)
+    if cuda is not None:
+        value_zeros = staticmethod(cuda.CudaNdarray.zeros)
     """
     Create an CudaNdarray full of 0 values
     """
@@ -433,9 +435,7 @@ copy_reg.constructor(CudaNdarray_unpickler)
 def CudaNdarray_pickler(cnda):
     return (CudaNdarray_unpickler, (numpy.asarray(cnda),))
 
-try:
-    # In case cuda is not imported.
+# In case cuda is not imported.
+if cuda is not None:
     copy_reg.pickle(cuda.CudaNdarray, CudaNdarray_pickler,
                     CudaNdarray_unpickler)
-except NameError:
-    pass