cfb66e1d9c2072647140d7a68ddbd5c356d12cb0
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2012-03-28 15:25:52 -0400

    Compare size, not just value, in test

--- theano/tensor/tests/test_elemwise.py
+++ theano/tensor/tests/test_elemwise.py
@@ -323,7 +323,9 @@ class test_CAReduce(unittest_tools.InferShapeTester):
                                                                        zv),
                                     (f(xv), zv))
                 else:
-                    self.assertTrue(numpy.allclose(f(xv), zv), (f(xv), zv))
+                    f_xv = f(xv)
+                    self.assertTrue((f_xv.shape == zv.shape), (f_xv, zv))
+                    self.assertTrue(numpy.allclose(f_xv, zv), (f_xv, zv))
 
             #test CAReduce.infer_shape
             #the Shape op don't implement c_code!
