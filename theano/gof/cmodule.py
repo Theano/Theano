Theano/Theano

commit 8ac5ab987dfa8eca18501ede9a1d13aacbecb4b5

Yet more dangerous default arguments.

theano/gof/cmodule.py
@@ -1411,7 +1411,8 @@ class GCC_compiler(object):
 
     @staticmethod
     def compile_str(module_name, src_code, location=None,
-                    include_dirs=[], lib_dirs=[], libs=[], preargs=[]):
+                    include_dirs=None, lib_dirs=None, libs=None,
+                    preargs=None):
         """
         :param module_name: string (this has been embedded in the src_code
 
@@ -1435,6 +1436,12 @@ class GCC_compiler(object):
         """
         #TODO: Do not do the dlimport in this function
 
+        if include_dirs is None:
+            preargs = []
+        if lib_dirs is None:
+            lib_dirs = []
+        if libs is None:
+            libs = []
         if preargs is None:
             preargs = []
         else:

theano/compile/debugmode.py
@@ -504,9 +504,9 @@ def char_from_number(number):
 
 
 def debugprint(r, prefix='', depth=-1, done=None, print_type=False,
-               file=sys.stdout, print_destroy_map=False, print_view_map=False,
-               order=[], ids='CHAR', stop_on_name=False,
-               prefix_child=None):
+               file=sys.stdout, print_destroy_map=False,
+               print_view_map=False, order=None, ids='CHAR',
+               stop_on_name=False, prefix_child=None):
     """Print the graph leading to `r` to given depth.
 
     :param r: Variable instance
@@ -530,6 +530,9 @@ def debugprint(r, prefix='', depth=-1, done=None, print_type=False,
     """
     if depth == 0:
         return
+
+    if order is None:
+        order = []
 
     if done is None:
         done = dict()

theano/misc/pycuda_example.py
@@ -54,7 +54,9 @@ def theano_parse_c_arg(c_arg):
 """
 class TheanoElementwiseKernel(pycuda.elementwise.ElementwiseKernel):
     def __init__(self, arguments, operation,
-                 name="kernel", keep=False, options=[], **kwargs):
+                 name="kernel", keep=False, options=None, **kwargs):
+        if options is None:
+            options = []
         if isinstance(arguments, basestring):
             arguments = [theano_parse_c_arg(arg)
                          for arg in arguments.split(",")]

theano/sandbox/debug.py
@@ -10,12 +10,16 @@ class DebugLinker(gof.WrapLinker):
 
     def __init__(self,
                  linkers,
-                 debug_pre = [],
-                 debug_post = [],
-                 copy_originals = False,
-                 check_types = True,
-                 compare_variables = True,
-                 compare_fn = (lambda x, y: x == y)):
+                 debug_pre=None,
+                 debug_post=None,
+                 copy_originals=False,
+                 check_types=True,
+                 compare_variables=True,
+                 compare_fn=(lambda x, y: x == y)):
+        if debug_pre is None:
+            debug_pre = []
+        if debug_post is None:
+            debug_post = []
         gof.WrapLinker.__init__(self,
                                 linkers = linkers,
                                 wrapper = self.wrapper)

theano/tensor/basic.py
@@ -4089,7 +4089,9 @@ class IncSubtensor(Op):
     """
 
     def __init__(self, idx_list, inplace=False, set_instead_of_inc=False,
-            destroyhandler_tolerate_aliased=[]):
+            destroyhandler_tolerate_aliased=None):
+        if destroyhandler_tolerate_aliased is None:
+            destroyhandler_tolerate_aliased = []
         self.idx_list = map(Subtensor.convert, idx_list)
         self.inplace = inplace
         if inplace:

commit 7877e99f26c93e1225f419e99ff74fb30974c233

Fixed bug introduced in 8ac5ab987dfa8eca18501ed

theano/gof/cmodule.py
@@ -1440,7 +1440,7 @@ class GCC_compiler(object):
         #TODO: Do not do the dlimport in this function
 
         if include_dirs is None:
-            preargs = []
+            include_dirs = []
         if lib_dirs is None:
             lib_dirs = []
         if libs is None: