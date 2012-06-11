bc347c6d2f751fdeba2dd7325e11c333e20f4653
Nicolas Bouchard <nicolas.bouchard.1@gmail.com>
2012-06-11 14:10:00 -0400

    fix bug to cast a sparse

--- theano/sparse/tests/test_sp2.py
+++ theano/sparse/tests/test_sp2.py
@@ -2,7 +2,7 @@ import time
 import unittest
 
 from nose.plugins.skip import SkipTest
-import numpy
+import numpy as np
 try:
     import scipy.sparse as sp
     import scipy.sparse
@@ -19,6 +19,7 @@ if not S.enable_sparse:
 from theano.sparse.sandbox import sp2 as S2
 
 from theano.tests import unittest_tools as utt
+from theano.sparse.basic import verify_grad_sparse
 
 def as_sparse_format(data, format):
     if format == 'csc':
@@ -38,8 +39,8 @@ def random_lil(shape, dtype, nnz):
     huge = 2 ** 30
     for k in range(nnz):
         # set non-zeros in random locations (row x, col y)
-        idx = numpy.random.random_integers(huge, size=len(shape)) % shape
-        value = numpy.random.rand()
+        idx = np.random.random_integers(huge, size=len(shape)) % shape
+        value = np.random.rand()
         #if dtype *int*, value will always be zeros!
         if "int" in dtype:
             value = int(value * 100)
@@ -47,6 +48,75 @@ def random_lil(shape, dtype, nnz):
                 idx,
                 value)
     return rval
+
+
+class TestCast(utt.InferShapeTester):
+    compatible_types = T.int_dtypes + T.continuous_dtypes
+    x_csc = [S.csc_matrix(dtype=t) for t in compatible_types]
+    x_csr = [S.csr_matrix(dtype=t) for t in compatible_types]
+
+    indptr = np.array([0, 2, 3, 6])
+    indices = np.array([0, 2, 2, 0, 1, 2])
+    data = np.array([1, 2, 3, 4, 5, 6])
+    properties = (data, indices, indptr)
+
+    def setUp(self):
+        super(TestCast, self).setUp()
+        self.op_class = S2.Cast
+
+    def test_cast(self):
+        cast_csc = dict([
+            (x, [theano.function([x], S2.astype(x, t))
+                 for t in self.compatible_types])
+            for x in self.x_csc])
+
+        cast_csr = dict([
+            (x, [theano.function([x], S2.astype(x, t))
+                 for t in self.compatible_types])
+            for x in self.x_csr])
+
+        for x in self.x_csc:
+            for f, t in zip(cast_csc[x], self.compatible_types):
+                a = sp.csc_matrix(self.properties, dtype=x.dtype)
+                assert f(a).dtype == t
+
+        for x in self.x_csr:
+            for f, t in zip(cast_csr[x], self.compatible_types):
+                a = sp.csr_matrix(self.properties, dtype=x.dtype)
+                assert f(a).dtype == t
+
+    def test_infer_shape(self):
+        for x in self.x_csc:
+            for t in self.compatible_types:
+                a = sp.csc_matrix(self.properties, dtype=x.dtype)
+                self._compile_and_check([x],
+                                        [S2.astype(x, t)],
+                                        [a],
+                                        self.op_class)
+
+        for x in self.x_csr:
+            for t in self.compatible_types:
+                a = sp.csr_matrix(self.properties, dtype=x.dtype)
+                self._compile_and_check([x],
+                                        [S2.astype(x, t)],
+                                        [a],
+                                        self.op_class)
+
+    def test_grad(self):
+        x_csc = [S.csc_matrix(dtype=t) for t in T.float_dtypes]
+        x_csr = [S.csr_matrix(dtype=t) for t in T.float_dtypes]
+
+        # There is a problem with the grad
+        # TODO Find the problem
+        # for x in x_csc:
+        #     for t in T.float_dtypes:
+        #         a = sp.csc_matrix(self.properties, dtype=x.dtype)
+        #         verify_grad_sparse(S2.Cast(t), [a])
+
+        # for x in x_csr:
+        #     for t in T.float_dtypes:
+        #         a = sp.csr_matrix(self.properties, dtype=x.dtype)
+        #         verify_grad_sparse(S2.Cast(t), [a])
 
 
 class test_structured_add_s_v(unittest.TestCase):
@@ -60,7 +130,7 @@ class test_structured_add_s_v(unittest.TestCase):
         for format in ['csr', 'csc']:
             for dtype in ['float32', 'float64']:
                 spmat = sp_types[format](random_lil((4, 3), dtype, 3))
-                mat = numpy.asarray(numpy.random.rand(3), dtype=dtype)
+                mat = np.asarray(np.random.rand(3), dtype=dtype)
                 
                 S.verify_grad_sparse(S2.structured_add_s_v,
                     [spmat, mat], structured=True)
@@ -77,12 +147,12 @@ class test_structured_add_s_v(unittest.TestCase):
                 
                 spmat = sp_types[format](random_lil((4, 3), dtype, 3))
                 spones = spmat.copy()
-                spones.data = numpy.ones_like(spones.data)
-                mat = numpy.asarray(numpy.random.rand(3), dtype=dtype)
+                spones.data = np.ones_like(spones.data)
+                mat = np.asarray(np.random.rand(3), dtype=dtype)
                 
                 out = f(spmat, mat)
                 
-                assert numpy.allclose(out.toarray(), spones.multiply(spmat + mat))
+                assert np.allclose(out.toarray(), spones.multiply(spmat + mat))
 
 
 class test_mul_s_v(unittest.TestCase):
@@ -96,7 +166,7 @@ class test_mul_s_v(unittest.TestCase):
         for format in ['csr', 'csc']:
             for dtype in ['float32', 'float64']:
                 spmat = sp_types[format](random_lil((4, 3), dtype, 3))
-                mat = numpy.asarray(numpy.random.rand(3), dtype=dtype)
+                mat = np.asarray(np.random.rand(3), dtype=dtype)
                 
                 S.verify_grad_sparse(S2.mul_s_v,
                     [spmat, mat], structured=True)
@@ -112,11 +182,11 @@ class test_mul_s_v(unittest.TestCase):
                 f = theano.function([x, y], S2.mul_s_v(x, y))
                 
                 spmat = sp_types[format](random_lil((4, 3), dtype, 3))
-                mat = numpy.asarray(numpy.random.rand(3), dtype=dtype)
+                mat = np.asarray(np.random.rand(3), dtype=dtype)
                 
                 out = f(spmat, mat)
                 
-                assert numpy.allclose(out.toarray(), spmat.toarray() * mat)
+                assert np.allclose(out.toarray(), spmat.toarray() * mat)
 
 if __name__ == '__main__':
     unittest.main()
