5c11aadae1412b469a00ae21b1fca6fe943bb418
Frederic <nouiz@nouiz.org>
2012-07-03 17:01:49 -0400

    Move the compilation code to a separate function to better see the control flow.

--- theano/gof/cutils.py
+++ theano/gof/cutils.py
@@ -7,29 +7,10 @@ from theano import config
 if os.path.exists(os.path.join(config.compiledir,'cutils_ext.so')):
     os.remove(os.path.join(config.compiledir,'cutils_ext.so'))
 
-# Ensure no-one else is currently modifying the content of the compilation
-# directory. This is important to prevent multiple processes from trying to
-# compile the cutils_ext module simultaneously.
-try:
-    # If we load a previously-compiled version, config.compiledir should
-    # be in sys.path.
-    if config.compiledir not in sys.path:
-        sys.path.append(config.compiledir)
-    from cutils_ext.cutils_ext import *
-except ImportError:
-    import cmodule
 
-    get_lock()
-    try:
-        try:
-            # We must retry to import it as some other processs could
-            # have been compiling it between the first failed import
-            # and when we receive the lock
-            from cutils_ext.cutils_ext import *
-        except ImportError:
-            import cmodule
-
-            code = """
+def compile_cutils():
+    """Do just the compilation of cutils_ext"""
+    code = """
         #include <Python.h>
         extern "C"{
         static PyObject *
@@ -66,13 +47,38 @@ except ImportError:
         }
         """
 
-            loc = os.path.join(config.compiledir, 'cutils_ext')
-            if not os.path.exists(loc):
-                os.mkdir(loc)
+    loc = os.path.join(config.compiledir, 'cutils_ext')
+    if not os.path.exists(loc):
+        os.mkdir(loc)
 
-            args = cmodule.GCC_compiler.compile_args()
-            cmodule.GCC_compiler.compile_str('cutils_ext', code, location=loc,
-                                             preargs=args)
+    args = cmodule.GCC_compiler.compile_args()
+    cmodule.GCC_compiler.compile_str('cutils_ext', code, location=loc,
+                                     preargs=args)
+
+
+try:
+    # If we load a previously-compiled version, config.compiledir should
+    # be in sys.path.
+    if config.compiledir not in sys.path:
+        sys.path.insert(0, config.compiledir)
+    from cutils_ext.cutils_ext import *
+except ImportError:
+    import cmodule
+
+    get_lock()
+# Ensure no-one else is currently modifying the content of the compilation
+# directory. This is important to prevent multiple processes from trying to
+# compile the cutils_ext module simultaneously.
+    try:
+        try:
+            # We must retry to import it as some other processs could
+            # have been compiling it between the first failed import
+            # and when we receive the lock
+            from cutils_ext.cutils_ext import *
+        except ImportError:
+            import cmodule
+
+            compile_cutils()
             from cutils_ext.cutils_ext import *
 
     finally:
