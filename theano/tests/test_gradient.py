3fc423907efa46b9429901e99179a8e76d4d26e5
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-08-23 16:56:03 -0400

    wrote tests that tensor.grad and function reject uncomputable inputs

--- theano/tests/test_gradient.py
+++ theano/tests/test_gradient.py
@@ -333,6 +333,40 @@ def test_undefined_grad_grad():
     except theano.gradient.GradUndefinedError:
         pass
 
+
+def test_unimplemented_grad_cost():
+    #tests that an unimplemented grad is not differentiable
+
+    a = theano.tensor.vector()
+    b = theano.gradient.grad_not_implemented(theano.tensor.add, 0, a)
+    cost = theano.tensor.dot(b,a)
+
+    try:
+        g = theano.tensor.grad(cost,a)
+        raise AssertionError(
+            "Returned an expression for the derivative of an "
+            "uncomputable expression")
+    except TypeError:
+        pass
+
+
+def test_unimplemented_grad_wrt():
+    #tests that the gradient with respect to an uncomputable expression
+    #is undefined
+
+    a = theano.tensor.vector()
+    b = theano.gradient.grad_not_implemented(theano.tensor.add, 0, a)
+    cost = a.sum()
+
+    try:
+        g = theano.tensor.grad(cost,b)
+        raise AssertionError(
+            "Returned an expression for the derivative wrt an"
+            "uncomputable expression")
+    except TypeError:
+        pass
+
+
 def test_grad_name():
     A = theano.tensor.matrix('A')
     x = theano.tensor.vector('x')
