086f8cbb7ff0d7104618b19660aa6b832beb8329
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-08-27 14:58:56 -0400

    made FunctionGraph check for NaNs

--- theano/gof/fg.py
+++ theano/gof/fg.py
@@ -11,6 +11,7 @@ import toolbox
 from python25 import all
 from theano import config
 import warnings
+NaNType = None
 
 
 class InconsistencyError(Exception):
@@ -211,6 +212,9 @@ class FunctionGraph(utils.object2):
     ### import ###
 
     def __import_r__(self, variables):
+        global NaNType
+        if NaNType is None:
+            from nan_type import NaNType
         # Imports the owners of the variables
         r_owner_done = set(self.nodes)
         for node in [r.owner for r in variables if r.owner is not None]:
@@ -219,6 +223,8 @@ class FunctionGraph(utils.object2):
                 self.__import__(node)
         for r in variables:
             if r.owner is None and not isinstance(r, graph.Constant) and r not in self.inputs:
+                if isinstance(r.type,NaNType):
+                    raise TypeError("Computation graph contains a NaN. "+r.type.why_nan)
                 raise MissingInputError("Undeclared input", r)
             if not getattr(r, 'fgraph', None) is self:
                 self.__setup_r__(r)
