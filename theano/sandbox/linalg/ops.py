e96d94fbe7b3fcb39b877d0ee6f228e8bf4767d1
Matthew Rocklin <mrocklin@gmail.com>
2012-08-27 21:33:28 -0500

    Solve.infer_shape now handles 1d array case

--- theano/sandbox/linalg/ops.py
+++ theano/sandbox/linalg/ops.py
@@ -655,9 +655,15 @@ class Solve(Op):
         #TODO: use the A_structure to go faster
         output_storage[0][0] = scipy.linalg.solve(A, b)
 
+    # computes shape of x where x = inv(A) * b
     def infer_shape(self, node, shapes):
-        (Ar, Ac), (Br, Bc) = shapes
-        return [(Ac, Bc)]
+        Ashape, Bshape = shapes
+        rows = Ashape[1]
+        if len(Bshape) == 1: # b is a Vector
+            return [(rows,)]
+        else:
+            cols = Bshape[1] # b is a Matrix
+            return [(rows, cols)]
 
 solve = Solve()  # general solve
 
