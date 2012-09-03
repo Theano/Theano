b5659130bf46ecfdd84e74680556a6fdc1df875f
Olivier Delalleau <delallea@iro>
2012-09-03 18:49:20 -0400

    Typo fixes

--- theano/tensor/tests/test_basic.py
+++ theano/tensor/tests/test_basic.py
@@ -1399,7 +1399,7 @@ _good_broadcast_unary_gammaln = dict(
     normal=(rand_ranged(-1 + 1e-2, 10, (2, 3)),),
     empty=(numpy.asarray([]),),)
 _grad_broadcast_unary_gammaln = dict(
-    # smaller range as our grad method don't estimate it good enough.
+    # smaller range as our grad method does not estimate it well enough.
     normal=(rand_ranged(1e-8, 8, (2, 3)),),)
 
 GammaTester = makeBroadcastTester(
