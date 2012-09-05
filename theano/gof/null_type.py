1f5af445015db1c973f06d1c761c86e584325a37
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-09-05 09:24:32 -0400

    pep8 on null_type.py

--- theano/gof/null_type.py
+++ theano/gof/null_type.py
@@ -1,6 +1,7 @@
 from type import Type
 from graph import Variable
 
+
 class NullType(Type):
     """
 
@@ -11,7 +12,7 @@ class NullType(Type):
 
     """
 
-    def __init__(self, why_null = '(no explanation given)'):
+    def __init__(self, why_null='(no explanation given)'):
         """
             why_null: A string explaining why this variable
                     can't take on any values
@@ -19,14 +20,16 @@ class NullType(Type):
         self.why_null = why_null
 
     def filter(self, data, strict=False, allow_downcast=None):
-       raise ValueError("No values may be assigned to a NullType")
-    def filter_variable(self, other):
-       raise ValueError("No values may be assigned to a NullType")
-    def may_share_memory(a, b):
-       return False
-    def values_eq(a, b, force_same_dtype=True):
-       raise ValueError("NullType has no values to compare")
+        raise ValueError("No values may be assigned to a NullType")
 
+    def filter_variable(self, other):
+        raise ValueError("No values may be assigned to a NullType")
+
+    def may_share_memory(a, b):
+        return False
+
+    def values_eq(a, b, force_same_dtype=True):
+        raise ValueError("NullType has no values to compare")
 
 
 class NullVariable(Variable):
