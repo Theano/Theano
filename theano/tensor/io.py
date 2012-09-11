c6f424f87fae44a3226b7b8e53361dd3b483cee5
Matthew Rocklin <mrocklin@gmail.com>
2012-09-10 21:22:19 -0500

    fix infer_shapes for mpi ops

--- theano/tensor/io.py
+++ theano/tensor/io.py
@@ -130,8 +130,8 @@ class MPIRecv(Op):
     def __str__(self):
         return "MPIRecv{source: %d, tag: %d, shape: %s, dtype: %s}"%self._info
 
-    #def infer_shape(self, node, shapes):
-    #    return [self.shape]
+    def infer_shape(self, node, shapes):
+        return [None, self.shape]
 
 class MPIRecvWait(Op):
     """
@@ -168,8 +168,8 @@ class MPIRecvWait(Op):
     def __str__(self):
         return "MPIRecvWait"
 
-#    def infer_shape(self, node, shapes):
-#        return shapes
+    def infer_shape(self, node, shapes):
+        return [shapes[1]]
 
 class MPISend(Op):
     """
