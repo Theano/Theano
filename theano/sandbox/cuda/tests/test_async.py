1ef7afd0f4ee5afa101b65cfd9aac77a775484aa
Matthew Rocklin <mrocklin@gmail.com>
2012-09-12 12:13:27 -0500

    add basic compilation test

--- theano/sandbox/cuda/tests/test_async.py
+++ theano/sandbox/cuda/tests/test_async.py
@@ -9,3 +9,10 @@ def test_async_to_gpu():
 
     assert (gx.dtype, type(gx)) == (gx2.dtype, type(gx2))
     assert isinstance(gx2.owner.op, GpuFromHostWait)
+
+def test_transfer():
+    x = theano.tensor.matrix('x')
+    gx = theano.sandbox.cuda.gpu_from_host(x)
+    gx2 = local_async_gpu.transform(gx.owner)
+
+    theano.function([x], gx2, mode=theano.Mode(optimizer=None, linker='c|py'))
