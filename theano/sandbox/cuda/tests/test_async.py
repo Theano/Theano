bda0bd97bbf8ead26c1cf58a26a2ddb678ded5b9
Frederic <nouiz@nouiz.org>
2012-09-12 13:59:03 -0400

    small fix

--- theano/sandbox/cuda/tests/test_async.py
+++ theano/sandbox/cuda/tests/test_async.py
@@ -7,7 +7,8 @@ def test_async_to_gpu():
     x = theano.tensor.matrix('x')
     gx = theano.sandbox.cuda.gpu_from_host(x)
     gx2 = local_async_gpu.transform(gx.owner)
-
+    assert len(gx2) == 1
+    gx2 = gx2[0]
     assert (gx.dtype, type(gx)) == (gx2.dtype, type(gx2))
     assert isinstance(gx2.owner.op, GpuFromHostWait)
 
@@ -16,6 +17,8 @@ def test_async_to_host():
     gx = theano.sandbox.cuda.gpu_from_host(x)
     y = theano.sandbox.cuda.host_from_gpu(gx)
     y2 = local_async_gpu.transform(y.owner)
+    assert len(y2) == 1
+    y2 = y2[0]
 
     assert (y.dtype, type(y)) == (y2.dtype, type(y2))
     assert isinstance(y2.owner.op, HostFromGpuWait)
