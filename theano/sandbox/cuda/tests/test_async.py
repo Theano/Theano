1711e1ad66c89b2bff35f9888ce66eef20a5a8fe
Matthew Rocklin <mrocklin@gmail.com>
2012-09-12 12:04:14 -0500

    moved async code to separate file
    renamed gpu_async to local_gpu_async

--- theano/sandbox/cuda/tests/test_async.py
+++ theano/sandbox/cuda/tests/test_async.py
@@ -1,10 +1,11 @@
-from theano.sandbox.cuda.basic_ops import async_gpu, GpuFromHostWait
+from theano.sandbox.cuda.async import local_async_gpu, GpuFromHostWait
+from theano.sandbox.cuda.basic_ops import gpu_from_host
 import theano
 
 def test_async_to_gpu():
     x = theano.tensor.matrix('x')
     gx = theano.sandbox.cuda.gpu_from_host(x)
-    gx2 = async_gpu.transform(gx.owner)
+    gx2 = local_async_gpu.transform(gx.owner)
 
     assert (gx.dtype, type(gx)) == (gx2.dtype, type(gx2))
     assert isinstance(gx2.owner.op, GpuFromHostWait)
