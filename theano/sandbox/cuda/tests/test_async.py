fd1a1b32d17fd1dac9113cc5ee557f631b22116a
Matthew Rocklin <mrocklin@gmail.com>
2012-09-12 12:53:45 -0500

    add execution test - seg faults

--- theano/sandbox/cuda/tests/test_async.py
+++ theano/sandbox/cuda/tests/test_async.py
@@ -1,6 +1,7 @@
 from theano.sandbox.cuda.async import local_async_gpu, GpuFromHostWait, HostFromGpuWait
 from theano.sandbox.cuda.basic_ops import gpu_from_host, host_from_gpu
 import theano
+import numpy as np
 
 def test_async_to_gpu():
     x = theano.tensor.matrix('x')
@@ -19,9 +20,19 @@ def test_async_to_host():
     assert (y.dtype, type(y)) == (y2.dtype, type(y2))
     assert isinstance(y2.owner.op, HostFromGpuWait)
 
-def test_transfer():
+def test_compile():
     x = theano.tensor.matrix('x')
     gx = theano.sandbox.cuda.gpu_from_host(x)
     gx2 = local_async_gpu.transform(gx.owner)
 
     theano.function([x], gx2, mode=theano.Mode(optimizer=None, linker='c|py'))
+
+def test_execute():
+    x = theano.tensor.matrix('x')
+    gx = theano.sandbox.cuda.gpu_from_host(x)
+    gx2 = local_async_gpu.transform(gx.owner)
+
+    f = theano.function([x], gx2, mode=theano.Mode(optimizer=None, linker='c|py'))
+
+    xx = np.ones((5,5), dtype=x.dtype)
+    f(xx)
