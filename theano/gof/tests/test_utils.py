970ecbbc5c43267a66a479f66b066b142e2776a4
Matthew Rocklin <mrocklin@gmail.com>
2012-09-14 12:31:02 -0500

    removed set notation from utils test

--- theano/gof/tests/test_utils.py
+++ theano/gof/tests/test_utils.py
@@ -5,7 +5,7 @@ def test_give_variables_names():
     x = theano.tensor.matrix('x')
     y = x + 1
     z = theano.tensor.dot(x, y)
-    variables = {x,y,z}
+    variables = (x, y, z)
     give_variables_names(variables)
     assert all(var.name for var in variables)
     assert unique([var.name for var in variables])
