48f84b7f2fb75d1befc09e9e8a453521d7d01801
Matthew Rocklin <mrocklin@gmail.com>
2012-09-14 11:41:13 -0500

    bad_var now fails on single None

--- theano/gof/tests/test_utils.py
+++ theano/gof/tests/test_utils.py
@@ -1,7 +1,7 @@
 import theano
 from theano.gof.utils import give_variables_names, unique
 
-def test_variables_with_names():
+def test_give_variables_names():
     x = theano.tensor.matrix('x')
     y = x + 1
     z = theano.tensor.dot(x, y)
@@ -9,3 +9,12 @@ def test_variables_with_names():
     give_variables_names(variables)
     assert all(var.name for var in variables)
     assert unique([var.name for var in variables])
+
+def test_give_variables_names_small():
+    x = theano.tensor.matrix('x')
+    y = theano.tensor.dot(x, x)
+    fgraph = theano.FunctionGraph((x,), (y,))
+    give_variables_names(fgraph.variables)
+    assert all(var.name for var in fgraph.variables)
+    assert unique([var.name for var in fgraph.variables])
+
