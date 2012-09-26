f0a3f06c8f9f6458fb0317d1d04923c268e12255
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2012-09-26 14:49:35 -0400

    Expand ~ in base_compiledir. clean-up after tests.

--- theano/tensor/tests/test_io.py
+++ theano/tensor/tests/test_io.py
@@ -4,21 +4,32 @@ from theano import tensor, function, Variable, Generic
 import numpy
 import os
 
+
 class T_load_tensor(unittest.TestCase):
     def test0(self):
         data = numpy.arange(5, dtype=numpy.int32)
-        filename = os.path.join(theano.config.base_compiledir, "_test.npy")
+        filename = os.path.join(
+            os.path.expanduser(theano.config.base_compiledir),
+            "_test.npy")
         numpy.save(filename, data)
         path = Variable(Generic())
         x = tensor.load(path, 'int32', (False,))
         y = x*2
         fn = function([path], y)
         assert (fn(filename) == data*2).all()
+
     def test_memmap(self):
         data = numpy.arange(5, dtype=numpy.int32)
-        filename = os.path.join(theano.config.base_compiledir, "_test.npy")
+        filename = os.path.join(
+            os.path.expanduser(theano.config.base_compiledir),
+            "_test.npy")
         numpy.save(filename, data)
         path = Variable(Generic())
         x = tensor.load(path, 'int32', (False,), mmap_mode='r+')
         fn = function([path], x)
         assert type(fn(filename)) == numpy.core.memmap
+
+    def tearDown(self):
+        os.remove(os.path.join(
+            os.path.expanduser(theano.config.base_compiledir),
+            "_test.npy"))
