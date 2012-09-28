7a6a2712e782646c6d5e84ae8268fd243f451cec
Matthew Rocklin <mrocklin@gmail.com>
2012-09-28 09:10:51 -0500

    clean up _test_mpi_roundtrip
    mpi_cmp -> mpi_cmps
    
    fix incorrect test `y = x + x` -> `y = x + 1` to match expected output
    
    remove old comments

--- theano/tensor/tests/_test_mpi_roundtrip.py
+++ theano/tensor/tests/_test_mpi_roundtrip.py
@@ -1,11 +1,10 @@
 # Run using
 # mpiexec -np 2 python _test_mpi_roundtrip.py
 
-
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 import theano
-from theano.tensor.io import send, recv, mpi_cmp
+from theano.tensor.io import send, recv, mpi_cmps
 from theano.gof.sched import sort_schedule_fn
 import numpy as np
 from sys import stdout
@@ -16,15 +15,14 @@ size = comm.Get_size()
 shape = (2, 2)
 dtype = 'float32'
 
-scheduler = sort_schedule_fn(mpi_cmp)
+scheduler = sort_schedule_fn(*mpi_cmps)
 mode = theano.Mode(optimizer=None,
                    linker=theano.OpWiseCLinker(schedule=scheduler))
 
 if rank == 0:
     x = theano.tensor.matrix('x', dtype=dtype)
-    y = x + x
+    y = x + 1
     send_request = send(y, 1, 11)
-    # send_request = send(x, 1, 11)
 
     z = recv(shape, dtype, 1, 12)
 
@@ -32,7 +30,6 @@ if rank == 0:
 
     xx = np.random.rand(*shape).astype(dtype)
     expected = (xx + 1) * 2
-    # expected = xx * 2
 
     _, zz = f(xx)
 
