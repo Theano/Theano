e7a68bd9b39d722fef933425e0593cdb8998e141
Frederic <nouiz@nouiz.org>
2012-10-01 14:45:24 -0400

    Modifications for 0.6rc1 release

--- doc/conf.py
+++ doc/conf.py
@@ -51,9 +51,9 @@ copyright = '2008--2012, LISA lab'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.5'
+version = '0.6'
 # The full version, including alpha/beta/rc tags.
-release = '0.5'
+release = '0.6rc1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
