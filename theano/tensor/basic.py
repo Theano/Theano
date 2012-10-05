3347655076d38338ed6ec3fb3b18acb803f5b999
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-10-05 10:20:31 -0400

    change assert to ValueError

--- theano/tensor/basic.py
+++ theano/tensor/basic.py
@@ -4441,7 +4441,10 @@ class IncSubtensor(Op):
 
     def make_node(self, x, y, *inputs):
         x, y = map(as_tensor_variable, [x, y])
-        assert y.ndim <= x.ndim
+        if y.ndim > x.ndim:
+            raise ValueError(("Trying to increment a %d-dimensional "
+                "subtensor with a %d-dimensional value.") % (x.ndim,
+                    y.ndim))
         inputs = tuple(map(Subtensor.my_as_scalar, inputs))
 
         idx_list = list(self.idx_list)
