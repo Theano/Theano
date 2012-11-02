91a17e2b5930f71bf88f5ea6c7507803a287b356
Razvan Pascanu <pascanur@iro>
2012-11-02 10:34:03 -0400

    corrected order of arguments

--- theano/scan_module/tests/test_scan.py
+++ theano/scan_module/tests/test_scan.py
@@ -1493,7 +1493,7 @@ class T_Scan(unittest.TestCase):
                                          go_backwards=False)
         vparams = [v_u1, v_u2, v_x0, v_y0, vW_in1]
         # y0 is actually not used in the computation of the cost
-        params = [u1, u2, y0, x0, W_in1]
+        params = [u1, u2, x0, y0, W_in1]
         gparams = theano.grad(cost, params,
                                      disconnected_inputs='ignore')
 
