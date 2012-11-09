e11dceb0084ffa23980926b5c35eaed6f9c0e34d
Frederic <nouiz@nouiz.org>
2012-11-08 20:55:47 -0500

    pep8

--- theano/sandbox/cuda/tests/test_async.py
+++ theano/sandbox/cuda/tests/test_async.py
@@ -6,7 +6,7 @@ if cuda_ndarray.cuda_available == False:
 
 import numpy as np
 from theano.sandbox.cuda.async import (local_async_gpu, async_optimizer,
-        GpuFromHostWait, HostFromGpuWait,GpuFromHostSend, HostFromGpuSend,
+        GpuFromHostWait, HostFromGpuWait, GpuFromHostSend, HostFromGpuSend,
         gpu_cmps, send_wait, gpu_ops_first, send_in_order, tiebreaker)
 from theano.sandbox.cuda.basic_ops import (gpu_from_host, host_from_gpu,
         GpuFromHost)
@@ -16,8 +16,9 @@ import theano.sandbox.linalg as linalg
 
 gpu_scheduler = sort_schedule_fn(*gpu_cmps)
 gpu_linker = theano.OpWiseCLinker(schedule=gpu_scheduler)
-gpu_mode = theano.Mode(linker = gpu_linker, optimizer = async_optimizer)
+gpu_mode = theano.Mode(linker=gpu_linker, optimizer=async_optimizer)
 full_opt = theano.compile.optdb.query("+fast_run", "+gpu_async")
+
 
 def test_async_to_gpu():
     x = theano.tensor.fmatrix('x')
@@ -70,7 +71,7 @@ def test_optimizer():
     ops = set([node.op for node in fgraph.apply_nodes])
     assert len(filter(lambda op: isinstance(op, GpuFromHostWait), ops)) == 1
     assert len(filter(lambda op: isinstance(op, GpuFromHostSend), ops)) == 1
-    assert len(filter(lambda op: isinstance(op, GpuFromHost), ops))     == 0
+    assert len(filter(lambda op: isinstance(op, GpuFromHost), ops)) == 0
 
 
 def test_optimizer2():
@@ -81,6 +82,7 @@ def test_optimizer2():
     f = theano.function([x], gx, mode=mode)
     xx = np.ones((5, 5), dtype=x.dtype)
     f(xx)
+
 
 def test_send_wait():
     send_wait_cmp = key_to_cmp(send_wait)
@@ -93,9 +95,10 @@ def test_send_wait():
     sendnode = gx2.owner.inputs[0].owner
     waitnode = gx2.owner
     addnode = y.owner
-    assert send_wait_cmp(sendnode, waitnode) < 0 # send happens first
-    assert send_wait_cmp(sendnode, addnode) < 0 # send happens first
-    assert send_wait_cmp(waitnode, addnode) > 0 # wait happens last
+    assert send_wait_cmp(sendnode, waitnode) < 0  # send happens first
+    assert send_wait_cmp(sendnode, addnode) < 0  # send happens first
+    assert send_wait_cmp(waitnode, addnode) > 0  # wait happens last
+
 
 # This shouldn't necessarily happen.
 def _test_gpu_ops_first():
@@ -105,18 +108,20 @@ def _test_gpu_ops_first():
     d = linalg.solve(a, b)
 
     mode = theano.Mode(linker=gpu_linker, optimizer=full_opt)
-    f = theano.function((a,b), (c, d), mode=mode)
+    f = theano.function((a, b), (c, d), mode=mode)
     nodes = f.maker.linker.make_all()[-1]
 
     index_of_solve = nodes.index(
            filter(lambda n: isinstance(n.op, linalg.ops.Solve),
                    nodes)[0])
 
-    index_of_gemm  = nodes.index(
-           filter(lambda n: isinstance(n.op, theano.sandbox.cuda.blas.GpuDot22),
+    index_of_gemm = nodes.index(
+           filter(lambda n: isinstance(n.op,
+                                       theano.sandbox.cuda.blas.GpuDot22),
                    nodes)[0])
 
     assert index_of_solve > index_of_gemm
+
 
 def test_send_order():
     a = theano.tensor.fmatrix('a')
@@ -126,13 +131,14 @@ def test_send_order():
     e = theano.tensor.dot(d, c)
 
     mode = theano.Mode(linker=gpu_linker, optimizer=full_opt)
-    f = theano.function((a,b,c), e, mode=mode)
+    f = theano.function((a, b, c), e, mode=mode)
     nodes = f.maker.linker.make_all()[-1]
 
     assert set(node.inputs[0].name for node in nodes[:2]) == \
            set((a.name, b.name))
     # C is the last needed. Ensure that it happens last
     assert nodes[2].inputs[0].name == c.name
+
 
 def test_tiebreaker():
     a = theano.tensor.fmatrix('a')
@@ -142,11 +148,12 @@ def test_tiebreaker():
     e = b+c
     assert tiebreaker(d.owner) < tiebreaker(e.owner)
 
+
 def test_gpu_schedule():
     x = theano.tensor.fmatrix('x')
     gx = theano.sandbox.cuda.gpu_from_host(x)
     y = x + 1
-    f = theano.function((x,), (gx, y), mode = gpu_mode)
+    f = theano.function((x,), (gx, y), mode=gpu_mode)
 
     nodes = f.maker.linker.make_all()[-1]
     assert isinstance(nodes[0].op, GpuFromHostSend)
