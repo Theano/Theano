0517a8f35c971f54acd3d5b29bf73b2664fa9d6b
John Salvatier <jsalvatier@gmail.com>
2012-11-09 18:20:43 -0800

    gave binary and unary bit ops proper gradients instead of none (caused failure)

--- theano/tensor/elemwise.py
+++ theano/tensor/elemwise.py
@@ -743,8 +743,6 @@ class Elemwise(Op):
             scalar_inputs = map(as_scalar, inputs)
             scalar_ograds = map(as_scalar, ograds)
             scalar_igrads = self.scalar_op.grad(scalar_inputs, scalar_ograds)
-            for igrad in scalar_igrads:
-                assert igrad is not None
 
         finally:
 
