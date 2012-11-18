41f1dbff8cef28b9fae53dc7f4dff401dcfa70d2
abalkin <abalkin@enlnt.com>
2012-11-18 15:15:21 -0500

    More linalg ops.
    Exposed pinv in linalg, implemented default case of norm(), added
    placeholders for some other linalg functions to be implemented.

--- theano/sandbox/linalg/__init__.py
+++ theano/sandbox/linalg/__init__.py
@@ -1,7 +1,8 @@
+import theano.tensor as _T
 from theano.tensor import as_tensor_variable as _as_tensor_variable
 from ops import (cholesky, matrix_inverse, solve,
         diag, extract_diag, alloc_diag,
-        det, psd, eig, eigh,
+        det, psd, eig, eigh, pinv,
         trace, spectral_radius_bound)
 
 
@@ -16,3 +17,26 @@ def eigvalsh(a, UPLO='L'):
     a = _as_tensor_variable(a)
     assert a.ndim == 2    
     return eigh(a, UPLO)[0]
+
+def svd(a, full_matrices=1, compute_uv=1):
+    "Singular Value Decomposition."
+    raise ValueError("Not implemented. Work in progress.")
+
+def norm(x, ord=None):
+    "Matrix or vector norm."
+    x = _as_tensor_variable(x)
+    if ord is None: # check the default case first and handle it immediately
+        return _T.sqrt(_T.real((_T.conj(x) * x).sum()))
+    raise ValueError("Not implemented. Work in progress.")
+
+def lstsq(a, b, rcond=-1):
+    "Return the least-squares solution to a linear matrix equation."
+    raise ValueError("Not implemented. Work in progress.")
+
+def tensorsolve(a, b, axes=None):
+    "Solve the tensor equation ``a x = b`` for x."
+    raise ValueError("Not implemented. Work in progress.")
+
+def tensorinv(a, ind=2):
+    "Compute the 'inverse' of an N-dimensional array."
+    raise ValueError("Not implemented. Work in progress.")
