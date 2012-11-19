e4e620c9ac9a59e0223ff923f2dd9b5d9728ef12
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-11-19 14:12:02 -0500

    fixed some assertRaises(TypeError)s

--- theano/tensor/opt_uncanonicalize.py
+++ theano/tensor/opt_uncanonicalize.py
@@ -41,7 +41,7 @@ from theano.gof.python25 import any, all
 from theano.gof.opt import Optimizer
 from theano.gof import InconsistencyError, toolbox
 
-from basic import get_scalar_constant_value
+from basic import get_scalar_constant_value, NotScalarConstantError
 from theano.tensor.opt import register_uncanonicalize
 from theano import scalar as scal
 
@@ -65,7 +65,7 @@ class MaxAndArgmaxOptimizer(Optimizer):
                     if len(node.outputs[1].clients)==0:
                         try:
                             axis=get_scalar_constant_value(node.inputs[1])
-                        except (ValueError, TypeError), e:
+                        except NotScalarConstantError:
                             return False
 
                         new = CAReduce(scal.maximum,axis)(node.inputs[0])
