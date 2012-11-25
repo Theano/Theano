6447ce7ee72203062e2ec99f8d39dc47f4626d2d
Ian Goodfellow <goodfellow.ian@gmail.com>
2012-11-25 12:12:28 -0500

    fixed determinism issues in scan

--- theano/updates.py
+++ theano/updates.py
@@ -8,23 +8,25 @@ __contact__ = "theano-dev <theano-dev@googlegroups.com>"
 
 __docformat__ = "restructuredtext en"
 
+from theano.gof.python25 import OrderedDict
+
 from theano.compile.sharedvalue import SharedVariable
 import logging
 logger = logging.getLogger('theano.updates')
 
-
-class Updates(dict):
+# Must be an OrderedDict or updates will be applied in a non-deterministic order
+class OrderedUpdates(OrderedDict):
     """
     Dict-like mapping from SharedVariable keys to their new values.
 
     This mapping supports the use of the "+" operator for the union of updates.
     """
     def __init__(self, *key, **kwargs):
-        ret = super(Updates, self).__init__(*key, **kwargs)
+        ret = super(OrderedUpdates, self).__init__(*key, **kwargs)
         for key in self:
             if not isinstance(key, SharedVariable):
                 raise TypeError(
-                    'Updates keys must inherit from SharedVariable',
+                    'OrderedUpdates keys must inherit from SharedVariable',
                     key)
         return ret
 
@@ -38,9 +40,9 @@ class Updates(dict):
             # value. Should it be cast to a GPU value right away?  Should
             # literals be transformed into constants immediately?
 
-            return super(Updates, self).__setitem__(key, value)
+            return super(OrderedUpdates, self).__setitem__(key, value)
         else:
-            raise TypeError('Updates keys must inherit from SharedVariable',
+            raise TypeError('OrderedUpdates keys must inherit from SharedVariable',
                     key)
 
     def update(self, other):
@@ -52,13 +54,13 @@ class Updates(dict):
             self[key] = val  # __setitem__ does type-checking
 
     def __add__(self, other):
-        rval = Updates()
+        rval = OrderedUpdates()
         rval.update(self)
         rval.update(other)
         return rval
 
     def __radd__(other, self):
-        rval = Updates()
+        rval = OrderedUpdates()
         rval.update(other)
         rval.update(self)
         return rval
