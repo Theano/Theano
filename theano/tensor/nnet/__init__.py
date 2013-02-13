7fa448b8ebf2b7c0662443fc0877d1bc6861e79a
abalkin <abalkin@enlnt.com>
2013-02-13 09:52:53 -0500

    Converted relative imports to absolute.

--- theano/tensor/nnet/__init__.py
+++ theano/tensor/nnet/__init__.py
@@ -1,6 +1,6 @@
-from nnet import *
-from conv import conv2d, ConvOp
-from Conv3D import *
-from ConvGrad3D import *
-from ConvTransp3D import *
-from sigm import softplus, sigmoid, sigmoid_inplace, scalar_sigmoid
+from theano.tensor.nnet import *
+from theano.tensor.nnet.conv import conv2d, ConvOp
+from theano.tensor.nnet.Conv3D import *
+from theano.tensor.nnet.ConvGrad3D import *
+from theano.tensor.nnet.ConvTransp3D import *
+from theano.tensor.nnet.sigm import softplus, sigmoid, sigmoid_inplace, scalar_sigmoid
