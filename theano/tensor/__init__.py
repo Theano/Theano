aee5674a1dff85adf7c5d42df53fb8b42435c6cd
abalkin <abalkin@enlnt.com>
2013-02-14 18:48:50 -0500

    Convert relative imports to absolute

--- theano/tensor/__init__.py
+++ theano/tensor/__init__.py
@@ -3,34 +3,34 @@ __docformat__ = "restructuredtext en"
 
 import warnings
 
-from basic import *
+from theano.tensor.basic import *
 
-import opt
-import opt_uncanonicalize
-import blas
-import blas_scipy
-import blas_c
-import xlogx
+import theano.tensor.opt
+import theano.tensor.opt_uncanonicalize
+import theano.tensor.blas
+import theano.tensor.blas_scipy
+import theano.tensor.blas_c
+import theano.tensor.xlogx
 
-import raw_random
-import randomstreams
-import shared_randomstreams
-from randomstreams import \
+import theano.tensor.raw_random
+import theano.tensor.randomstreams
+import theano.tensor.shared_randomstreams
+from theano.tensor.randomstreams import \
     RandomStreams
 
 random = RandomStreams(seed=0xBAD5EED, no_warn = True)
 """Imitate the numpy.random symbol with a tensor.random one"""
 
-from elemwise import \
+from theano.tensor.elemwise import \
     DimShuffle, Elemwise, CAReduce
 
-import sharedvar  # adds shared-variable constructors
+import theano.tensor.sharedvar  # adds shared-variable constructors
 
 # We import as `_shared` instead of `shared` to avoid confusion between
 # `theano.shared` and `tensor._shared`.
-from sharedvar import tensor_constructor as _shared
+from theano.tensor.sharedvar import tensor_constructor as _shared
 
-from io import *
+from theano.tensor.io import *
 
 def shared(*args, **kw):
     """
@@ -49,11 +49,11 @@ def shared(*args, **kw):
     return _shared(*args, **kw)
 
 
-import nnet  # used for softmax, sigmoid, etc.
+import theano.tensor.nnet  # used for softmax, sigmoid, etc.
 
 from theano.gradient import Rop, Lop, grad, numeric_grad, verify_grad, \
     jacobian, hessian
 
 from theano.tensor.sort import sort, argsort
-from extra_ops import (DiffOp, bincount, squeeze,
+from theano.tensor.extra_ops import (DiffOp, bincount, squeeze,
                        repeat, bartlett, fill_diagonal)
