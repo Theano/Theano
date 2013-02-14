c55cb65c146ebea89b93dd7a6e587c155bdd3045
Frederic <nouiz@nouiz.org>
2013-02-14 14:02:59 -0500

    Modifications for 0.6rc3 release

--- theano/tests/record.py
+++ theano/tests/record.py
@@ -1,5 +1,4 @@
 __authors__ = "Ian Goodfellow"
-__copyright__ = "Copyright 2010-2012, Universite de Montreal"
 __credits__ = ["Ian Goodfellow"]
 __license__ = "3-clause BSD"
 __maintainer__ = "Ian Goodfellow"
