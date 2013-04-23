588dcf8f808c2d9e91d535212e402cf867258413
Razvan Pascanu <pascanur@iro>
2013-04-23 17:03:29 -0400

    re-add change

--- theano/scan_module/scan_op.py
+++ theano/scan_module/scan_op.py
@@ -248,6 +248,12 @@ class Scan(PureOp):
                 rval = rval.astype(as_var.type.dtype)
             if rval.ndim == as_var.ndim:
                 rval = as_var.type.filter_variable(rval)
+            else:
+                tmp = as_var.type.__class__(
+                    broadcastable=tuple(var.broadcastable[:1])+\
+                                  tuple(as_var.broadcastable),
+                    dtype=as_var.dtype)
+                rval = tmp.filter_variable(rval)
             return rval
 
         # Check if input sequences and variables representing a slice of
