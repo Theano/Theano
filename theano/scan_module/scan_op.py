01e67c7f170508ec49629743d8875a314d8659f6
Olivier Delalleau <delallea@iro>
2013-04-27 10:37:54 -0400

    Fixed a number of typos in doc reported by eneftci
    Also made sample code better PEP8-compliant, and fixed a minor typo in
    an error message.
    
    Fixes gh-1353

--- theano/scan_module/scan_op.py
+++ theano/scan_module/scan_op.py
@@ -228,7 +228,7 @@ class Scan(PureOp):
                    )
         err_msg2 = ('When compiling the inner function of scan the '
                     'following error has been encountered: The '
-                    'initial state (outputs_info in scan nomenclature)'
+                    'initial state (outputs_info in scan nomenclature) '
                     'of variable %s (argument number %d)'
                     ' has dtype %s and %d dimension(s), while the result '
                     'of the inner function for this output has dtype %s '
