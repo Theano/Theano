3298dcf504352837c560ee0d9aaa2ea58f38c3af
Olivier Delalleau <delallea@iro>
2013-05-30 20:25:24 -0400

    Typos / clarifications

--- theano/tensor/basic.py
+++ theano/tensor/basic.py
@@ -1533,15 +1533,15 @@ class _tensor_py_operators:
             return True
         else:
             raise TypeError(
-                "Variable does not support boolean operations. This"
-                "can happen if you do logical operator (<, <=, >, <=,"
-                "==, !=) between numpy.ndarray and theano tensor"
-                "variable. Due NumPy implementation before NumPy 1.8,"
-                "we can't make the python syntax work when the ndarray"
-                "is on the left, and this end with this error. To work"
-                "around that, just call"
-                "theano.tensor.{lt,le,eq,ne,gt,ge}(ndarray, tensor) or"
-                "use the python syntax with the theano tensor on the"
+                "Variables do not support boolean operations. This "
+                "can happen if you do a logical operation (<, <=, >, <=, "
+                "==, !=) between a numpy.ndarray and a Theano tensor"
+                "variable. Due to NumPy implementation before NumPy 1.8, "
+                "we cannot make the Python syntax work when the ndarray "
+                "is on the left, and this results in this error. To work "
+                "around that, either call "
+                "theano.tensor.{lt,le,eq,ne,gt,ge}(ndarray, tensor), or "
+                "use the Python syntax with the Theano tensor on the "
                 "left. Or update to NumPy 1.8 or above."
             )
 
