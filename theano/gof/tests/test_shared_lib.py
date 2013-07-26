efffb78dec12cd37816fe1c4d599d81f20a884e3
Frederic <nouiz@nouiz.org>
2013-07-26 14:56:26 -0400

    Make real test of shared lib test file.

--- theano/gof/tests/test_shared_lib.py
+++ theano/gof/tests/test_shared_lib.py
@@ -4,32 +4,32 @@ import numpy as np
 
 import theano
 
-a = theano.tensor.vector()
-b = theano.tensor.vector()
-for inps, out in [([a], theano.tensor.exp(a)),  # 1 input/1 outputs
-                  ([a, b], a + b),  # 2 inputs
-                  ((a, b), [a, b]),  # 2 outputs, 2 deepcopy ops
-                  ((a, b), [a + b, a - b]),
-              ]:
-    f = theano.function(inps, out, theano.Mode(linker='c'),
-                        on_unused_input='ignore')
-    theano.printing.debugprint(f, print_type=True)
-    #filename = f.fn.thunks[0].filename  # with linker=vm
-    filename = f.fn.filename  # with linker=c
-    print filename
 
-#theano.shared_lib(f, name='libtheano_exp')
-#f(np.arange(10))
-    x = os.system(os.path.join(os.path.split(filename)[0], 'exec'))
-    if x != 0:
-        exit(x)
+def test_basic():
+    a = theano.tensor.vector()
+    b = theano.tensor.vector()
+    for inps, out in [([a], theano.tensor.exp(a)),  # 1 input/1 outputs
+                      ([a, b], a + b),  # 2 inputs
+                      ((a, b), [a, b]),  # 2 outputs, 2 deepcopy ops
+                      ((a, b), [a + b, a - b]),
+                  ]:
+        f = theano.function(inps, out, theano.Mode(linker='c'),
+                            on_unused_input='ignore')
+        theano.printing.debugprint(f, print_type=True)
+        #filename = f.fn.thunks[0].filename  # with linker=vm
+        filename = f.fn.filename  # with linker=c
+        print filename
 
+        #theano.shared_lib(f, name='libtheano_exp')
+        #f(np.arange(10))
+        x = os.system(os.path.join(os.path.split(filename)[0], 'exec'))
+        assert x == 0, "The executable crashed!"
 
-# Test raise error if no c code
-try:
-    theano.function((a,), [theano.tensor.argmax(a)],
-                    theano.Mode(linker='c'),
-                    on_unused_input='ignore')
-    assert False, "Expected an error"
-except NotImplementedError:
-    pass
+        # Test raise error if no c code
+        try:
+            theano.function((a,), [theano.tensor.argmax(a)],
+                            theano.Mode(linker='c'),
+                            on_unused_input='ignore')
+            assert False, "Expected an error"
+        except NotImplementedError:
+            pass
