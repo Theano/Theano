1e9fdf1d16d8fb2b2d4216cddf04e733a60ee822
Frederic <nouiz@nouiz.org>
2013-09-23 13:56:14 -0400

    Make the new GpuSubtensor tests run with it!

--- theano/sandbox/gpuarray/tests/test_subtensor.py
+++ theano/sandbox/gpuarray/tests/test_subtensor.py
@@ -7,6 +7,8 @@ from theano.sandbox.gpuarray.type import gpuarray_shared_constructor
 
 from theano.sandbox.gpuarray.tests.test_basic_ops import mode_with_gpu
 
+from theano import tensor
+
 class G_subtensor(T_subtensor):
     def shortDescription(self):
         return None
@@ -17,7 +19,19 @@ class G_subtensor(T_subtensor):
     dtype = 'float32' # avoid errors on gpus which do not support float64
     ignore_topo = (HostFromGpu, GpuFromHost)
     fast_compile = False
-    ops = (GpuSubtensor,)
+    ops = (GpuSubtensor,
+           tensor.IncSubtensor, tensor.AdvancedSubtensor1,
+           tensor.AdvancedIncSubtensor1)
+
+    #inc_sub = cuda.GpuIncSubtensor
+    #adv_sub1 = cuda.GpuAdvancedSubtensor1
+    #adv_incsub1 = cuda.GpuAdvancedIncSubtensor1
+    inc_sub = tensor.IncSubtensor
+    adv_sub1 = tensor.AdvancedSubtensor1
+    adv_incsub1 = tensor.AdvancedIncSubtensor1
 
     def __init__(self, name):
-        T_subtensor.__init__(self, name)
+        # We must call T_subtensor parten __init__, otherwise, sub is overrieded
+        ret = super(T_subtensor, self).__init__(name)
+        assert self.sub == GpuSubtensor
+        return ret
