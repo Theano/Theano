2528f038c8dbbe00870226fdd9bc05db77ed4af4
Arnaud Bergeron <abergeron@gmail.com>
2013-09-25 16:29:53 -0400

    Use the proper way to init T_subtensor.

--- theano/sandbox/gpuarray/tests/test_subtensor.py
+++ theano/sandbox/gpuarray/tests/test_subtensor.py
@@ -7,31 +7,20 @@ from theano.sandbox.gpuarray.type import gpuarray_shared_constructor
 
 from theano.sandbox.gpuarray.tests.test_basic_ops import mode_with_gpu
 
+from theano.compile import DeepCopyOp
+
 from theano import tensor
 
 class G_subtensor(T_subtensor):
     def shortDescription(self):
         return None
 
-    shared = staticmethod(gpuarray_shared_constructor)
-    sub = GpuSubtensor
-    mode = mode_with_gpu
-    dtype = 'float32' # avoid errors on gpus which do not support float64
-    ignore_topo = (HostFromGpu, GpuFromHost)
-    fast_compile = False
-    ops = (GpuSubtensor,
-           tensor.IncSubtensor, tensor.AdvancedSubtensor1,
-           tensor.AdvancedIncSubtensor1)
-
-    #inc_sub = cuda.GpuIncSubtensor
-    #adv_sub1 = cuda.GpuAdvancedSubtensor1
-    #adv_incsub1 = cuda.GpuAdvancedIncSubtensor1
-    inc_sub = tensor.IncSubtensor
-    adv_sub1 = tensor.AdvancedSubtensor1
-    adv_incsub1 = tensor.AdvancedIncSubtensor1
-
     def __init__(self, name):
-        # We must call T_subtensor parten __init__, otherwise, sub is overrieded
-        ret = super(T_subtensor, self).__init__(name)
+        T_subtensor.__init__(self, name,
+                             shared=gpuarray_shared_constructor,
+                             sub=GpuSubtensor,
+                             mode=mode_with_gpu,
+                             # avoid errors with limited devices
+                             dtype='float32',
+                             ignore_topo=(HostFromGpu,GpuFromHost,DeepCopyOp))
         assert self.sub == GpuSubtensor
-        return ret
