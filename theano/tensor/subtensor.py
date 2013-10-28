b7880c5ffa27b7f3b2db019ed9660804f97f60db
Frederic <nouiz@nouiz.org>
2013-10-28 09:17:36 -0400

    Use the same attribute checking as done in NumPy.

--- theano/tensor/subtensor.py
+++ theano/tensor/subtensor.py
@@ -862,7 +862,7 @@ class Subtensor(Op):
         finish_view = """
         if (%(z)s) Py_DECREF(%(z)s);
         Py_INCREF(py_%(x)s);
-#if NPY_VERSION < 0x01000009
+#if NPY_API_VERSION < 0x00000007
         PyArray_BASE(xview) = py_%(x)s;
 #else
         PyArray_SetBaseObject(xview, py_%(x)s);
