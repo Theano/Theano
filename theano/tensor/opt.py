1cf4a13c6b72f792fe93134d2633ddf726e1928b
Frederic <nouiz@nouiz.org>
2014-01-29 12:07:55 -0500

    local_mul_to_neg now only apply when it won't add node to the graph

--- theano/tensor/opt.py
+++ theano/tensor/opt.py
@@ -3387,12 +3387,21 @@ def local_sum_alloc(node):
 
 @gof.local_optimizer([T.mul])
 def local_mul_to_neg(node):
+    """
+    mul(-1, x) -> neg(x)
+
+    This is not done if we would add more nodes in the graph, like with:
+
+    mul(-1, x, y) -/-> neg(mul(x, y))
+    """
     if (node.op == T.mul and
         local_mul_canonizer.get_constant(node.inputs[0]) == -1.0):
         num = node.inputs[1:]
         if len(num) == 1:
             other_prod = num[0]
         else:
+            # This would add extra node in the graph
+            return False
             other_prod = local_mul_canonizer.main(*num)
         if other_prod.type == node.outputs[0].type:
             return [-other_prod]
