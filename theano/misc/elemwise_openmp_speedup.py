cd0bde8856d534b2fc64db5ec87b37129cad0497
Frederic <nouiz@nouiz.org>
2014-02-07 16:34:04 -0500

    Add -N param to the second script.

--- theano/misc/elemwise_openmp_speedup.py
+++ theano/misc/elemwise_openmp_speedup.py
@@ -1,12 +1,23 @@
 import os
 import subprocess
 import sys
+from optparse import OptionParser
+
+import theano
+
+parser = OptionParser(usage='%prog <options>\n Compute time for'
+                      ' fast and slow elemwise operations')
+parser.add_option('-N', '--N', action='store', dest='N',
+                  default=theano.config.openmp_elemwise_minsize, type="int",
+                  help="Number of vector element")
 
 
-def runScript():
+def runScript(N):
     script = 'elemwise_time_test.py'
     dir = os.path.dirname(os.path.abspath(__file__))
-    proc = subprocess.Popen(['python', script, '--script'], stdout=subprocess.PIPE, stderr = subprocess.PIPE, cwd = dir)
+    proc = subprocess.Popen(['python', script, '--script', '-N', str(N)],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            cwd=dir)
     (out, err) = proc.communicate()
     if err:
         print err
@@ -14,10 +25,14 @@ def runScript():
     return map(float, out.split(" "))
 
 if __name__ == '__main__':
+    options, arguments = parser.parse_args(sys.argv)
+    if hasattr(options, "help"):
+        print options.help
+        sys.exit(0)
     os.environ['THEANO_FLAGS'] = 'openmp=false'
-    (cheapTime, costlyTime) = runScript()
+    (cheapTime, costlyTime) = runScript(N=options.N)
     os.environ['THEANO_FLAGS'] = 'openmp=true'
-    (cheapTimeOpenmp, costlyTimeOpenmp) = runScript()
+    (cheapTimeOpenmp, costlyTimeOpenmp) = runScript(N=options.N)
 
     if cheapTime > cheapTimeOpenmp:
         cheapSpeed = (cheapTime - cheapTimeOpenmp) / cheapTime
