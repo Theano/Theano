ddd11bd4052e2d257ddc6d5e4f70663a8d4c4640
rizar <dimabv@tut.by>
2014-02-16 17:28:26 +0100

    Fix Variable.eval() (issue #1724 on github)
    It is now possible to call Variable.eval() with different sets of input
    parameters.

--- theano/gof/graph.py
+++ theano/gof/graph.py
@@ -415,18 +415,22 @@ class Variable(Node):
         if inputs_to_values is None:
             inputs_to_values = {}
 
-        if not hasattr(self, '_fn'):
-            self._fn_inputs = inputs_to_values.keys()
-            self._fn = theano.function(self._fn_inputs, self)
-        args = [inputs_to_values[param] for param in self._fn_inputs]
+        if not hasattr(self, '_fn_cache'):
+            self._fn_cache = dict()
 
-        rval = self._fn(*args)
+        input_list = list(inputs_to_values.keys())
+        input_set = frozenset(input_list)
+        if not input_set in self._fn_cache:
+            self._fn_cache[input_set] = theano.function(input_list, self)
+        args = [inputs_to_values[param] for param in input_list]
+
+        rval = self._fn_cache[input_set](*args)
 
         return rval
 
     def __getstate__(self):
         d = self.__dict__.copy()
-        d.pop("_fn", None)
+        d.pop("_fn_cache", None)
         return d
     env = property(env_getter, env_setter, env_deleter)
 
