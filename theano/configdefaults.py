99eec4e9ad08e865e842f31b514133ca355e9172
Olivier Delalleau <delallea@iro>
2014-02-16 21:18:25 -0500

    Typo fixes in doc

--- theano/configdefaults.py
+++ theano/configdefaults.py
@@ -483,9 +483,9 @@ AddConfigVar('openmp',
          )
 
 AddConfigVar('openmp_elemwise_minsize',
-             "If OpenMP is enable, this is the minimum size of vector "
-             "for which  the openmp parallel for is enable."
-             "Used in element wise ops",
+             "If OpenMP is enabled, this is the minimum size of vectors "
+             "for which the openmp parallelization is enabled "
+             "in element wise ops.",
              IntParam(200000),
              in_c_key=False,
          )
