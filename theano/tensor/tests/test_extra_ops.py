5764be527e203c498133aa8505934f6f1964cb5a
Sander Dieleman <sanderdieleman@gmail.com>
2014-02-26 15:41:33 +0100

    added consider_constant op, which truncates the gradient of an expression to 0. Also added an optimization to remove it from the graph, so other optimizations can be applied properly.
    test whether the gradient is computed correctly, and test whether the consider_constant op is removed from the graph.

--- theano/tensor/tests/test_extra_ops.py
+++ theano/tensor/tests/test_extra_ops.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy
+import unittest
 
 import theano
 from theano.tests import unittest_tools as utt
@@ -7,7 +8,7 @@ from theano.tests import unittest_tools as utt
 from theano.tensor.extra_ops import (CumsumOp, cumsum, CumprodOp, cumprod,
                                      BinCountOp, bincount, DiffOp, diff,
                                      squeeze, RepeatOp, repeat, Bartlett, bartlett,
-                                     FillDiagonal, fill_diagonal)
+                                     FillDiagonal, fill_diagonal, consider_constant)
 from theano import tensor as T
 from theano import config, tensor, function
 
@@ -463,3 +464,41 @@ class TestFillDiagonal(utt.InferShapeTester):
                                  numpy.random.rand()],
                                 self.op_class,
                                 warn=False)
+
+
+class TestConsiderConstant(unittest.TestCase):
+
+    def setUp(self):
+        utt.seed_rng()
+        self.rng = np.random.RandomState(seed=utt.fetch_seed())
+
+    def test_op_removed(self):
+        x = T.matrix('x')
+        y = x * consider_constant(x)
+        f = theano.function([x], y)
+        # need to refer to T.extra_ops.consider_constant_ here,
+        # T.consider_constant is a wrapper function!
+        assert T.extra_ops.consider_constant_ not in \
+            [node.op for node in f.maker.fgraph.toposort()]
+        
+    def test_grad(self):
+        a = numpy.asarray(self.rng.randn(50, 50),
+            dtype=config.floatX)
+        
+        x = T.matrix('x')
+        y = x * consider_constant(x)
+
+        expressions_gradients = [
+            (x * consider_constant(x), x),
+            (x * consider_constant(T.exp(x)), T.exp(x)),
+            (consider_constant(x), T.constant(0.)),
+            (x**2 * consider_constant(x), 2 * x**2),
+        ]
+
+        for expr, expr_grad in expressions_gradients:
+            g = T.grad(expr.sum(), x)
+            f = theano.function([x], g, on_unused_input='ignore') # grad according to theano
+            f2 = theano.function([x], expr_grad, on_unused_input='ignore') # desired grad
+
+            assert np.allclose(f(a), f2(a))
+
