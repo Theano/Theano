f42f9dff399132415bf28428a3fddbc9e3a443c2
Hei <lamyuenhei@gmail.com>
2014-03-21 21:41:56 +0800

    Added platform-dependent C include files to the include directories
    pyconfig.h maybe located in the platform-dependent include directory

--- theano/gof/cmodule.py
+++ theano/gof/cmodule.py
@@ -1438,8 +1438,12 @@ def get_gcc_shared_library_arg():
 
 
 def std_include_dirs():
-    return (numpy.distutils.misc_util.get_numpy_include_dirs()
-            + [distutils.sysconfig.get_python_inc()])
+    numpy_inc_dirs = numpy.distutils.misc_util.get_numpy_include_dirs()
+    py_inc = distutils.sysconfig.get_python_inc()
+    py_plat_spec_inc = distutils.sysconfig.get_python_inc(plat_specific=True)
+    python_inc_dirs = ([py_inc] if py_inc == py_plat_spec_inc
+                       else [py_inc, py_plat_spec_inc])
+    return numpy_inc_dirs + python_inc_dirs
 
 
 def std_lib_dirs_and_libs():
