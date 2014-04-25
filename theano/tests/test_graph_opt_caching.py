ce96798e4a855202547105df1615f54256e325f6
Li <you@yourdomain.example.com>
2014-04-25 15:10:02 -0400

    not ready, work on it later

--- theano/tests/test_graph_opt_caching.py
+++ theano/tests/test_graph_opt_caching.py
@@ -2,16 +2,17 @@ import unittest
 import theano
 import theano.tensor as T
 from theano.gof.graph import is_same_graph
+from theano.gof import FunctionGraph
 from theano.scan_module.scan_utils import equal_computations
 
 def graphs_equal(x, y):
     # check if two graphs are equal
     # x: outputs list of graph A
     # y: outputs list of graph B
-    import ipdb; ipdb.set_trace()
-    while True:
-        pass
-        
+
+    pass
+
+
 def test_graph_equivalence():
     # Test if equivalent graphs are in fact equivalent
     # by using some functions in Theano
@@ -34,7 +35,11 @@ def test_graph_equivalence():
     assert is_same_graph(g1_y, g1_y)
     assert is_same_graph(g1_y, g1_yy)
     assert is_same_graph(g1_y, g3_y, givens={g1_a: g3_a, g1_b: g3_b})
-    FunctionGraph([], g1_y)
+    l1=theano.gof.graph.inputs(g1_y)
+    l2=theano.gof.graph.inputs(g3_y)
+    assert len(l1) == len(l2)
+    
+    #FunctionGraph([], g1_y)
     
     #assert graphs_equal(g1_y, g3_y) == True
     #assert graphs_equal(g1_y, g2_y) == False
