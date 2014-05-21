b0e830ff856279d2749ddb752a482eb9df5a581f
Arnaud Bergeron <abergeron@gmail.com>
2014-05-21 17:31:22 -0400

    Fix the test to actually work.

--- theano/sandbox/cuda/tests/test_fftconv.py
+++ theano/sandbox/cuda/tests/test_fftconv.py
@@ -1,6 +1,7 @@
+import unittest
+import numpy
+
 import theano
-from theano.tensor import ftensor4
-from theano.tensor.nnet.tests.test_conv import TestConv2D
 
 # Skip tests if cuda_ndarray is not available.
 from nose.plugins.skip import SkipTest
@@ -13,12 +14,30 @@ if theano.config.mode == 'FAST_COMPILE':
 else:
     mode_with_gpu = theano.compile.mode.get_default_mode().including('gpu')
 
-class GTestConv2D(TestConv2D):
-    mode = mode_with_gpu
-    dtype = 'float32'
-
+def TestConv2dFFT(unittest.TestCase):
     def setUp(self):
-        super(GTestConv2D, self).setUp()
-        self.input = ftensor4('input')
-        self.filters = ftensor4('filters')
+        self._prev = theano.confg.enable_conv2d_fft
+        theano.confg.enable_conv2d_fft = True
 
+    def tearDown(self):
+        theano.confg.enable_conv2d_fft = self._prev
+
+    def test_valid(self):
+        inputs_shape = (5, 3, 7, 6)
+        filters_shape = (2, 3, 3, 3)
+
+        inputs_val = numpy.random.random(inputs_shape).astype('float32')
+        filters_val = numpy.random.random(filters_shape).astype('float32')
+
+        inputs = shared(inputs_val)
+        filters = shared(filters_val)
+
+        conv = theano.tensor.nnet.conv.conv2d(inputs, filters)
+
+        f_ref = theano.function([], conv)
+        f_fft = theano.function([], conv, mode=mode_with_gpu)
+
+        res_ref = f_ref()
+        res_fft = f_fft()
+
+        numpy.testing.assert_allclose(res_ref, res_fft)
