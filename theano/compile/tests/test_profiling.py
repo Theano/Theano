f0bd1a199d9b204c332dfe099d88fb2a52a89658
Roy Xue <xljroy@gmail.com>
2014-05-29 23:23:32 +0800

    Add test of profiling.

---
+++ theano/compile/tests/test_profiling.py
@@ -0,0 +1,29 @@
+"""
+Test of memory profiling
+
+"""
+import theano
+import theano.tensor as T
+import StringIO
+
+def test_profiling():
+	
+	old1 = theano.config.profile 
+	old2 = theano.config.profile_memory
+
+	theano.config.profile = True
+	theano.config.profile_memory = True
+	
+	x = T.dvector("x")
+	y = T.dvector("y")
+	z = x + y
+	f = theano.function([x, y], z, profile=True, name="test_profiling")
+	output = f([1, 2, 3, 4],[1, 1, 1, 1])
+
+	buf = StringIO.StringIO()
+	f.profile.summary(buf)
+	
+	theano.config.profile = old1
+	theano.config.profile_memory = old2
+
+test_profiling()
