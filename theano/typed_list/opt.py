228915eab546e4bdb97f963a64100477abd92d82
Hengjean <hengjeanp@gmail.com>
2014-06-02 14:22:15 -0400

    Made all opt into one

--- theano/typed_list/opt.py
+++ theano/typed_list/opt.py
@@ -5,43 +5,17 @@ from theano.typed_list.basic import (Reverse,
                     Append, Extend, Insert)
 
 
-def generic_opt_creator(op):
+@gof.local_optimizer([Append, Extend, Insert, Reverse], inplace=True)
+def typed_list_inplace_opt(node):
+    if isinstance(node.op, (Append, Extend, Insert, Reverse)) \
+                                        and not node.op.inplace:
 
-    @gof.local_optimizer([op], inplace=True)
-    def generic_inplace_opt(node):
-            if isinstance(node.op, op) and not node.op.inplace:
-                new_op = node.op.__class__(
+        new_op = node.op.__class__(
                     inplace=True)
-                new_node = new_op(*node.inputs)
-                return [new_node]
-            return False
-
-    return generic_inplace_opt
-
-
-local_inplace_reverse = generic_opt_creator(Reverse)
-compile.optdb.register('local_inplace_reverse',
-                       TopoOptimizer(local_inplace_reverse,
+        new_node = new_op(*node.inputs)
+        return [new_node]
+    return False
+compile.optdb.register('typed_list_inplace_opt',
+                       TopoOptimizer(typed_list_inplace_opt,
     failure_callback=TopoOptimizer.warn_inplace), 60,
                        'fast_run', 'inplace')
-
-
-local_inplace_append = generic_opt_creator(Append)
-compile.optdb.register('local_inplace_append',
-                       TopoOptimizer(local_inplace_append,
-    failure_callback=TopoOptimizer.warn_inplace), 60,
-                       'fast_run', 'inplace')
-
-
-local_inplace_extend = generic_opt_creator(Extend)
-compile.optdb.register('local_inplace_extend',
-                       TopoOptimizer(local_inplace_extend,
-    failure_callback=TopoOptimizer.warn_inplace), 60,
-                       'fast_run', 'inplace')
-
-
-local_inplace_insert = generic_opt_creator(Insert)
-compile.optdb.register('local_inplace_insert',
-                       TopoOptimizer(local_inplace_insert,
-    failure_callback=TopoOptimizer.warn_inplace), 60,
-                       'fast_run', 'inplace')  # DEBUG
