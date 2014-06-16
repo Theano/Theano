75f0652fb964bdca5bf822bcc990d5d299541e7c
Frederic <nouiz@nouiz.org>
2014-06-16 09:50:03 -0400

    Reset the flags if the tests crash and fix the test in debug mode.

--- theano/compile/tests/test_profiling.py
+++ theano/compile/tests/test_profiling.py
@@ -11,22 +11,27 @@ def test_profiling():
 
     old1 = theano.config.profile
     old2 = theano.config.profile_memory
+    try:
+        theano.config.profile = True
+        theano.config.profile_memory = True
 
-    theano.config.profile = True
-    theano.config.profile_memory = True
+        x = T.dvector("x")
+        y = T.dvector("y")
+        z = x + y
+        p = theano.ProfileStats(False)
+        if theano.config.mode in ["DebugMode", "DEBUG_MODE"]:
+            m = "FAST_RUN"
+        else:
+            m = None
+        f = theano.function([x, y], z, profile=p, name="test_profiling",
+                            mode=m)
+        output = f([1, 2, 3, 4], [1, 1, 1, 1])
 
-    x = T.dvector("x")
-    y = T.dvector("y")
-    z = x + y
-    p = theano.ProfileStats(False)
-    f = theano.function([x, y], z, profile=p, name="test_profiling")
-    output = f([1, 2, 3, 4], [1, 1, 1, 1])
-
-    buf = StringIO.StringIO()
-    f.profile.summary(buf)
-
-    theano.config.profile = old1
-    theano.config.profile_memory = old2
+        buf = StringIO.StringIO()
+        f.profile.summary(buf)
+    finally:
+        theano.config.profile = old1
+        theano.config.profile_memory = old2
 
 
 if __name__ == '__main__':
