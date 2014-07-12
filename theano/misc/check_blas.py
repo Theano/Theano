f39049162f44914f7272bac900cbfe51ff70fd43
Frederic <nouiz@nouiz.org>
2014-07-11 22:32:14 -0400

    Add 780 timming.

--- theano/misc/check_blas.py
+++ theano/misc/check_blas.py
@@ -218,6 +218,7 @@ if __name__ == "__main__":
 
         GTX Titan Black   0.05s
         GTX Titan(D15U-50)       0.06s  0.06s  don't work
+        GTX 780                  0.06s
         GTX 680                  0.11s  0.12s  0.154s               0.218s
         GTX 580                  0.16s  0.16s  0.164s               0.203s
         GTX 480                  0.19s  0.19s  0.192s               0.237s 0.27s
