6ec728efdb1127f16ddad2cdbd6b58ef53b333a3
Tanjay94 <you@yourdomain.example.com>
2014-07-16 13:25:51 -0400

    Fixed deprecation warning in kron.py.

--- theano/sandbox/linalg/kron.py
+++ theano/sandbox/linalg/kron.py
@@ -1,12 +1,7 @@
 from theano import tensor
 from theano.tensor.slinalg import kron
 
-def deprecation_warning():
-    # Make sure the warning is displayed only once.
-    if deprecation_warning.already_displayed:
-        return
-
-    warnings.warn(
-            "theano modules are deprecated and will be removed in release 0.7",
-            stacklevel=3)
-    deprecation_warning.already_displayed = True
+warnings.warn(
+        "theano modules are deprecated and will be removed in release 0.7",
+        stacklevel=3)
+deprecation_warning.already_displayed = True
