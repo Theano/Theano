116f70fe908b5c84800e934b312f896543e56e7f
Frederic <nouiz@nouiz.org>
2014-07-16 10:55:19 -0400

    pep8

--- theano/sandbox/softsign.py
+++ theano/sandbox/softsign.py
@@ -1,14 +1,16 @@
 
 import theano
-import numpy
 import theano.tensor
+
 
 class ScalarSoftsign(theano.scalar.UnaryScalarOp):
     @staticmethod
     def static_impl(x):
         return x / (1.0 + abs(x))
+
     def impl(self, x):
         return ScalarSoftsign.static_impl(x)
+
     def grad(self, inp, grads):
         x, = inp
         gz, = grads
@@ -17,11 +19,15 @@ class ScalarSoftsign(theano.scalar.UnaryScalarOp):
             return [gz / (d * d)]
         else:
             return NotImplemented
+
     def c_code(self, node, name, inp, out, sub):
         x, = inp
         z, = out
-        if node.inputs[0].type in [theano.scalar.float32, theano.scalar.float64]:
+        if node.inputs[0].type in [theano.scalar.float32,
+                                   theano.scalar.float64]:
             return "%(z)s = %(x)s / (1.0+fabs(%(x)s));" % locals()
         raise NotImplementedError('only floating point x is implemented')
-scalar_softsign = ScalarSoftsign(theano.scalar.upgrade_to_float, name='scalar_softsign')
+
+scalar_softsign = ScalarSoftsign(theano.scalar.upgrade_to_float,
+                                 name='scalar_softsign')
 softsign = theano.tensor.Elemwise(scalar_softsign, name='softsign')
