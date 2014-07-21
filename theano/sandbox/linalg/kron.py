47e433435d98c28cffa136efe0be5d4caf7aec0d
Tanjay94 <you@yourdomain.example.com>
2014-07-21 10:01:50 -0400

    Linalg import error.

--- theano/sandbox/linalg/kron.py
+++ theano/sandbox/linalg/kron.py
@@ -1,9 +1,6 @@
-from theano import tensor
 from theano.tensor.slinalg import kron
-from theano.compile.modules import deprecation_warning
 import warnings
 
 warnings.warn(
         "theano modules are deprecated and will be removed in release 0.7",
         stacklevel=3)
-deprecation_warning.already_displayed = True
