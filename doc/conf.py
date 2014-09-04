e95d9b5a6c28b0cfba17987220d4051b8b68aec6
Arnaud Bergeron <abergeron@gmail.com>
2014-09-03 20:59:44 -0400

    Add a mode to docgen to run the code samples in the documentation.
    This alleviates copying them elsewhere to ensure they are not broken.

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.doctest']
 
 todo_include_todos = True
 
