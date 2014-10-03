4ee324daf6f67853caa0741253d697f1c89eadad
Li <you@yourdomain.example.com>
2014-10-03 16:21:55 -0400

    support: no need to re-optmize when unpickling theano function. added a new option reoptimize_unpickled_function=False

--- theano/tests/test_pickle.py
+++ theano/tests/test_pickle.py
@@ -8,7 +8,7 @@ floatX = 'float32'
 
 def test_pickle_unpickle():
     # Test if pick and unpickling a theano function with
-    # shared variables work
+    # shared variables should be pickled properly
     x1 = T.fmatrix('x1')
     x2 = T.fmatrix('x2')
     x3 = theano.shared(numpy.ones((10,10),dtype=floatX))
@@ -19,12 +19,12 @@ def test_pickle_unpickle():
     updates[x3] = x3 + 1
     updates[x4] = x4 + 1
     f = theano.function([x1,x2],y, updates=updates)
-
+    
     pkl_path = open('thean_fn.pkl','wb')
     cPickle.dump(f, pkl_path, -1)
     pkl_path = open('thean_fn.pkl','r')
     f_ = cPickle.load(pkl_path)
-
+    import ipdb; ipdb.set_trace()
     in1 = numpy.ones((10, 10), dtype=floatX)
     in2 = numpy.ones((10, 10), dtype=floatX)
         
