83d734a2802a4aa6dedac7c805edd60ca96764ef
Caglar <ca9lar@gmail.com>
2014-10-16 13:14:29 -0400

    Fixed the compiled function print.

--- theano/printing.py
+++ theano/printing.py
@@ -119,7 +119,7 @@ def debugprint(obj, depth=-1, print_type=False,
 
     for s in scan_ops:
         debugmode.debugprint(s, depth=depth, done=done, print_type=print_type,
-                             file=_file, order=order, ids=ids,
+                             file=_file, ids=ids,
                              scan_ops=scan_ops, stop_on_name=stop_on_name)
 
         if hasattr(s.owner, 'op') and isinstance(s.owner.op, theano.scan_module.scan_op.Scan):
@@ -128,7 +128,7 @@ def debugprint(obj, depth=-1, print_type=False,
                     if isinstance(i.owner.op, theano.scan_module.scan_op.Scan):
                         scan_ops.append(i)
                 debugmode.debugprint(r=i, prefix=new_prefix, depth=depth, done=done,
-                           print_type=print_type, file=file, order=order,
+                           print_type=print_type, file=file,
                            ids=ids, stop_on_name=stop_on_name,
                            prefix_child=new_prefix_child, scan_ops=scan_ops)
 
