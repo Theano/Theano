8b3d3934182976795192319c361d312bbf49a7b4
Frederic <nouiz@nouiz.org>
2014-10-21 11:16:19 -0400

    pep8

--- theano/tests/test_pickle_unpickle_theano_fn.py
+++ theano/tests/test_pickle_unpickle_theano_fn.py
@@ -19,45 +19,48 @@ from theano.compat.python2x import DictMixin, OrderedDict
 floatX = 'float32'
 import theano
 import theano.tensor as T
-    
+
+
 def test_pickle_unpickle_with_reoptimization():
     x1 = T.fmatrix('x1')
     x2 = T.fmatrix('x2')
-    x3 = theano.shared(numpy.ones((10,10),dtype=floatX))
-    x4 = theano.shared(numpy.ones((10,10),dtype=floatX))
-    y = T.sum(T.sum(T.sum(x1**2+x2) + x3) + x4)
+    x3 = theano.shared(numpy.ones((10, 10), dtype=floatX))
+    x4 = theano.shared(numpy.ones((10, 10), dtype=floatX))
+    y = T.sum(T.sum(T.sum(x1 ** 2 + x2) + x3) + x4)
 
     updates = OrderedDict()
     updates[x3] = x3 + 1
     updates[x4] = x4 + 1
-    f = theano.function([x1,x2],y, updates=updates)
+    f = theano.function([x1, x2], y, updates=updates)
 
     # now pickle the compiled theano fn
     string_pkl = cPickle.dumps(f, -1)
-    
+
     in1 = numpy.ones((10, 10), dtype=floatX)
     in2 = numpy.ones((10, 10), dtype=floatX)
-    
+
     # test unpickle with optimization
     default = theano.config.reoptimize_unpickled_function
     try:
-        theano.config.reoptimize_unpickled_function=True # the default is True
+        # the default is True
+        theano.config.reoptimize_unpickled_function = True
         f_ = cPickle.loads(string_pkl)
         assert f(in1, in2) == f_(in1, in2)
     finally:
         theano.config.reoptimize_unpickled_function = default
-    
+
+
 def test_pickle_unpickle_without_reoptimization():
     x1 = T.fmatrix('x1')
     x2 = T.fmatrix('x2')
-    x3 = theano.shared(numpy.ones((10,10),dtype=floatX))
-    x4 = theano.shared(numpy.ones((10,10),dtype=floatX))
-    y = T.sum(T.sum(T.sum(x1**2+x2) + x3) + x4)
+    x3 = theano.shared(numpy.ones((10, 10), dtype=floatX))
+    x4 = theano.shared(numpy.ones((10, 10), dtype=floatX))
+    y = T.sum(T.sum(T.sum(x1**2 + x2) + x3) + x4)
 
     updates = OrderedDict()
     updates[x3] = x3 + 1
     updates[x4] = x4 + 1
-    f = theano.function([x1,x2],y, updates=updates)
+    f = theano.function([x1, x2], y, updates=updates)
 
     # now pickle the compiled theano fn
     string_pkl = cPickle.dumps(f, -1)
@@ -65,16 +68,18 @@ def test_pickle_unpickle_without_reoptimization():
     # compute f value
     in1 = numpy.ones((10, 10), dtype=floatX)
     in2 = numpy.ones((10, 10), dtype=floatX)
-    
+
     # test unpickle without optimization
     default = theano.config.reoptimize_unpickled_function
     try:
-        theano.config.reoptimize_unpickled_function=False # the default is True
+        # the default is True
+        theano.config.reoptimize_unpickled_function = False
         f_ = cPickle.loads(string_pkl)
         assert f(in1, in2) == f_(in1, in2)
     finally:
         theano.config.reoptimize_unpickled_function = default
-    
+
+
 if __name__ == '__main__':
     test_pickle_unpickle_with_reoptimization()
     test_pickle_unpickle_without_reoptimization()
