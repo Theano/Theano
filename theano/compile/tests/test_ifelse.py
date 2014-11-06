938ccc8895137f71dc9e6caa4a5b28a65c946a56
Roy Xue <xljroy@gmail.com>
2014-11-07 00:28:22 +0800

    new test_ifelse() function

--- theano/compile/tests/test_ifelse.py
+++
@@ -1,3 +0,0 @@
-"""
-Tests of the ifelse crash 
-"""
--- theano/compile/tests/test_profiling.py
+++ theano/compile/tests/test_profiling.py
@@ -8,6 +8,7 @@ import numpy
 
 import theano
 import theano.tensor as T
+from theano.ifelse import ifelse
 
 
 def test_profiling():
@@ -55,5 +56,47 @@ def test_profiling():
         theano.config.profiling.min_peak_memory = config3
 
 
+def test_ifelse():
+    config1 = theano.config.profile
+    config2 = theano.config.profile_memory
+
+    try:
+        theano.config.profile = True
+        theano.config.profile_memory = True
+
+        a,b = T.scalars('a', 'b')
+        x,y = T.scalars('x', 'y')
+
+        z = ifelse(T.lt(a, b), x*2, y*2)
+
+        p = theano.ProfileStats(False)
+
+        if theano.config.mode in ["DebugMode", "DEBUG_MODE", "FAST_COMPILE"]:
+            m = "FAST_RUN"
+        else:
+            m = None
+
+        f_ifelse = theano.function([a, b, x, y], z, profile=p, name="test_ifelse",
+                                mode=m)
+
+        val1 = 0.
+        val2 = 1.
+        big_mat1 = 10
+        big_mat2 = 11
+
+        out = f_ifelse(val1, val2, big_mat1, big_mat2)
+
+        print out
+
+        buf = StringIO.StringIO()
+        f_ifelse.profile.summary(buf)
+
+    finally:
+        theano.config.profile = config1
+        theano.config.profile_memory = config2
+
+
+
 if __name__ == '__main__':
     test_profiling()
+    test_ifelse()
