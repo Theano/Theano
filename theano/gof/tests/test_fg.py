04522c4e356192b51044fe112282bb7b77bc64c4
Li <you@yourdomain.example.com>
2014-11-10 16:40:05 -0500

    fix

--- theano/gof/tests/test_fg.py
+++ theano/gof/tests/test_fg.py
@@ -24,8 +24,3 @@ class TFunctionGraph(unittest.TestCase):
 
         s = pickle.dumps(func)
         func2 = pickle.loads(s)
-
-        vec = numpy.random.uniform(size=100)
-        assert func[vec] == func2(vec)
-
-        
