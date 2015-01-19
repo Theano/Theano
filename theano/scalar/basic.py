0bde212bb226b7a1ccf0405607280f580fd42e60
Mohammad Pezeshki <mohammadpz@gmail.com>
2015-01-19 17:54:59 -0500

    mod added. tests are more complex.

--- theano/scalar/basic.py
+++ theano/scalar/basic.py
@@ -2175,6 +2175,7 @@ pprint.assign(neg, printing.OperatorPrinter('-', 0, 'either'))
 pprint.assign(true_div, printing.OperatorPrinter('/', -1, 'left'))
 pprint.assign(int_div, printing.OperatorPrinter('//', -1, 'left'))
 pprint.assign(pow, printing.OperatorPrinter('**', 1, 'right'))
+pprint.assign(mod, printing.OperatorPrinter('%', -1, 'lef'))
 
 class Inv(UnaryScalarOp):
     """ multiplicative inverse. Also called reciprocal"""
