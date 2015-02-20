2fcafab800d78f6ec369e49a2e19fc2f11022ed3
Frederic <nouiz@nouiz.org>
2015-02-20 11:00:14 -0500

    Fix a crash as unpickle break our singleton pattern

--- theano/tensor/tests/test_type_other.py
+++ theano/tensor/tests/test_type_other.py
@@ -1,6 +1,7 @@
 """ This file don't test everything. It only test one past crash error."""
 import theano
-from theano.tensor.type_other import MakeSlice, make_slice
+from theano.gof import Constant
+from theano.tensor.type_other import MakeSlice, make_slice, NoneTypeT, NoneConst
 
 
 def test_make_slice_merge():
@@ -12,3 +13,27 @@ def test_make_slice_merge():
     nodes = f.maker.fgraph.nodes
     assert len([n for n in nodes if isinstance(n.op, MakeSlice)]) == 1
     theano.printing.debugprint(f)
+
+
+def test_none_Constant():
+    """ Tests equals
+
+    We had an error in the past with unpickling
+    """
+    o1 = Constant(NoneTypeT(), None, name='NoneConst')
+    o2 = Constant(NoneTypeT(), None, name='NoneConst')
+    assert o1.equals(o2)
+    assert NoneConst.equals(o1)
+    assert o1.equals(NoneConst)
+    assert NoneConst.equals(o2)
+    assert o2.equals(NoneConst)
+
+    # This trigger equals that returned the wrong answer in the past.
+    import cPickle
+    import theano
+    from theano import tensor
+
+    x = tensor.vector('x')
+    y = tensor.argmax(x)
+    f = theano.function([x], [y])
+    cPickle.loads(cPickle.dumps(f))
