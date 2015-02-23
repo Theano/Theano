b66c5e7de3e04fc7f3fedfcaa4dfe49c0960e3ee
Yann N. Dauphin <dhaemon@gmail.com>
2015-02-23 16:16:32 -0500

    remove debugmode

--- theano/sandbox/cuda/tests/test_blas.py
+++ theano/sandbox/cuda/tests/test_blas.py
@@ -311,8 +311,11 @@ def test_downsample():
 
                 ggf = gradient.Lop(tensor.grad((ds_op(
                     tensor.as_tensor_variable(a))**2).sum(), a), a, a)
-                gg = pfunc([], ggf, mode=mode_with_gpu)
-                gg2 = pfunc([], ggf, mode=mode_without_gpu)
+                
+                ref_mode = copy.copy(theano.compile.get_default_mode())
+                ref_mode.check_py_code = False
+                gg = pfunc([], ggf, mode=ref_mode)
+                gg2 = pfunc([], ggf, mode=ref_mode)
                 
                 assert any([isinstance(node.op,
                                        tcn.blas.GpuDownsampleFactorMaxGradGrad)
