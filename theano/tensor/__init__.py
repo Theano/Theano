67485b30f5621b88d0591e6d2459b282dd5c2559
Arnaud Bergeron <abergeron@gmail.com>
2015-03-06 13:53:26 -0500

    This has been deprecated since sept 2011.  It's old enough.

--- theano/tensor/__init__.py
+++ theano/tensor/__init__.py
@@ -38,24 +38,6 @@ from theano.tensor.sharedvar import tensor_constructor as _shared
 
 from theano.tensor.io import *
 
-
-def shared(*args, **kw):
-    """
-    Backward-compatibility wrapper around `tensor._shared`.
-
-    Once the deprecation warning has been around for long enough, this function
-    can be deleted.
-    """
-    # Note that we do not use the DeprecationWarning class because it is
-    # ignored by default since python 2.7.
-    warnings.warn('`tensor.shared` is deprecated. You should probably be using'
-                  ' `theano.shared` instead (if you *really* intend to call '
-                  '`tensor.shared`, you can get rid of this warning by using '
-                  '`tensor._shared`).',
-                  stacklevel=2)
-    return _shared(*args, **kw)
-
-
 from theano.tensor import nnet  # used for softmax, sigmoid, etc.
 
 from theano.gradient import Rop, Lop, grad, numeric_grad, verify_grad, \
