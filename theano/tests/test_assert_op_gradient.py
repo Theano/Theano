ba70cb82f326abd7cad6579fa03577fdc9d3d7db
Ying Zhang <ying.zhlisa@gmail.com>
2015-04-07 21:19:46 -0400

    add basic test

---
+++ theano/tests/test_assert_op_gradient.py
@@ -0,0 +1,15 @@
+import numpy
+
+import theano
+import theano.tensor as T
+from theano.tensor.opt import Assert
+
+def test_assert_op_gradient():
+    x = T.vector('x')
+    assert_op = Assert()
+    cost = T.sum(assert_op(x, x.size<2))
+    grad = T.grad(cost, x)
+    func = theano.function([x], grad)
+
+    x_val = numpy.ones(shape=(1,))
+    assert func(x_val) == 1
