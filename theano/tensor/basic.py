15249c25462a7c3be1342f8952ad0a648e74cb48
Melanie Ducoffe <ducoffem@boltzmann.iro.umontreal.ca>
2015-04-11 10:13:28 -0400

    en cours

--- theano/tensor/basic.py
+++ theano/tensor/basic.py
@@ -5493,7 +5493,7 @@ class AllocEmpty(gof.Op):
             except tensor.NotScalarConstantError:
                 const_shp = None
             bcast.append(numpy.all(1 == const_shp))
-        otype = tensor.TensorType(dtype='float32', broadcastable=bcast)
+        otype = tensor.TensorType(dtype=self.dtype, broadcastable=bcast)
         output = otype()
         return sh, output
 
@@ -5507,7 +5507,7 @@ class AllocEmpty(gof.Op):
         sh = tuple([int(i) for i in inputs])
         if out[0] is None or out[0].shape != sh:
             # XXX: We could implement and call CudaNdarray.empty(sh) instead.
-            out[0] = numpy.zeros(sh)
+            out[0] = numpy.empty(sh)
 
     def do_merge(self, node):
         return False
