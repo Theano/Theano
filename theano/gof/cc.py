71b5bf1ef99fe9dc732dcd1932991fb99d352722
AlOa <orlandi.alberto@gmail.com>
2015-05-17 20:09:02 +0200

    Add the support for constants in the shared library's code generation

--- theano/gof/cc.py
+++ theano/gof/cc.py
@@ -621,7 +621,6 @@ class CLinker(link.Linker):
         This method caches its computations.
 
         """
-
         if getattr(self, 'struct_code', False):
             return self.struct_code, self.run_code
 
@@ -1175,6 +1174,7 @@ class CLinker(link.Linker):
           f()
           first_output = ostor[0].data
         """
+
         init_tasks, tasks = self.get_init_tasks()
         cthunk, in_storage, out_storage, error_storage, filename = self.__compile__(
             input_storage, output_storage, storage_map,
@@ -1640,7 +1640,7 @@ class CLinker(link.Linker):
                 mod.add_include(header)
             for init_code_block in self.init_code() + self.c_init_code_apply:
                 if self.c_callable:
-                    mod.add_support_code(self.cinit_code(len(self.args)))
+                    mod.add_support_code(self.cinit_code())
                     mod.add_header_code("""
                                         DllExport %(struct_name)s* cinit();
                                         """ % dict(struct_name=self.struct_name))
@@ -1826,20 +1826,32 @@ PyErr_Print();
                    **locals())
         return main
 
-    def cinit_code(self, n_args):
+    def cinit_code(self):
         code = StringIO()
+        n_args = len(self.args)
         struct_name = self.struct_name
-        param = ','.join('PyObject * arg_%i' % n for n in xrange(n_args)), ');'
-
+        param = ','.join('PyObject * arg_%i' % n for n in range(n_args)), ');'
+        symbol2r = {v: k for k, v in self.r2symbol.items()}
         in_out_list = ""
         in_out_param = ["io%d_list" % idx for idx in range(n_args)]
-        for idx in range(n_args):
+        for idx, arg in enumerate(self.args):
             var = in_out_param[idx]
-            in_out_list += """
+            argType = symbol2r[arg.replace("storage_", "")]
+            if isinstance(argType, graph.Constant):
+                in_out_list += """
+                PyObject* %(var)s = PyList_New(1);
+                npy_intp dims[1]={1};
+                PyObject* const_%(var)s = PyArray_SimpleNew(1,dims,NPY_FLOAT64);
+                Py_XINCREF(const_%(var)s);
+                ((double *)(PyArray_DATA((PyArrayObject *)const_%(var)s)))[0]=0;
+                PyList_SetItem(%(var)s, 0,const_%(var)s);
+                """ % locals()
+            else:
+                in_out_list += """
                 PyObject* %(var)s = PyList_New(1);
                 Py_INCREF(Py_None);
                 PyList_SetItem(%(var)s, 0, Py_None);
-            """ % locals()
+                """ % locals()
         in_out_param = ', '.join(in_out_param)
         print >> code, """
 %(struct_name)s* cinit() {
