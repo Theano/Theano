e810b0b768767771ee063befee54bf518b767956
Arnaud Bergeron <abergeron@gmail.com>
2015-05-22 20:50:12 -0400

    Flake8 for gof/lazylinker_c.py

--- theano/tests/test_flake8.py
+++ theano/tests/test_flake8.py
@@ -269,7 +269,6 @@ whitelist_flake8 = [
     "sparse/sandbox/sp.py",
     "gof/destroyhandler.py",
     "gof/unify.py",
-    "gof/lazylinker_c.py",
     "gof/optdb.py",
     "gof/utils.py",
     "gof/graph.py",
