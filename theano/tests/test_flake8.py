9c605c00e8076ece660995287581d82037de6911
Arnaud Bergeron <abergeron@gmail.com>
2015-05-22 20:47:28 -0400

    Flake8 for gof/compiledir.py

--- theano/tests/test_flake8.py
+++ theano/tests/test_flake8.py
@@ -268,7 +268,6 @@ whitelist_flake8 = [
     "sparse/sandbox/truedot.py",
     "sparse/sandbox/sp.py",
     "gof/destroyhandler.py",
-    "gof/compiledir.py",
     "gof/unify.py",
     "gof/lazylinker_c.py",
     "gof/optdb.py",
