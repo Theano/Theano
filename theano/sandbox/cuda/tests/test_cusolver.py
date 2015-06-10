410f9cadd42f16c599050a2d20d67b6f72160248
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-06-10 15:07:45 -0400

    Test with Fortran order too

--- theano/sandbox/cuda/tests/test_cusolver.py
+++ theano/sandbox/cuda/tests/test_cusolver.py
@@ -4,9 +4,8 @@ from numpy.testing import assert_allclose
 from scipy.linalg.lapack import dgeqrf
 
 import theano
-from theano import tensor
 from theano.sandbox.cuda.cusolver import geqrf, cusolver_available
-from theano.sandbox.cuda import cuda_available
+from theano.sandbox.cuda import cuda_available, CudaNdarray
 
 if not cuda_available:
     raise SkipTest('CUDA not available')
@@ -19,12 +18,18 @@ def test_geqrf():
 
     def assert_geqrf_allclose(m, n):
         A = rng.rand(m, n).astype(theano.config.floatX)
-        A_tensor = tensor.fmatrix('A')
-        dgeqrf_ = theano.function([A_tensor], geqrf(A_tensor))
-        qr_, tau_, work_, info_ = dgeqrf_(A)
-        qr, tau, work, info = dgeqrf(A)
+        A_ = theano.shared(CudaNdarray(A), borrow=True)
+        A_T = theano.shared(CudaNdarray(A.T), borrow=True)
+        theano.config.optimizer = 'None'
+        dgeqrf_ = theano.function([], geqrf(A_))
+        dgeqrf_T = theano.function([], geqrf(A_T.T))
+        qr_, tau_, work_, info_ = dgeqrf_()
+        qr_T, tau_T, work_T, info_T = dgeqrf_T()
+        qr, tau, work, info = dgeqrf(numpy.array(A))
         assert_allclose(qr, qr_.eval(), 1e-3)
+        assert_allclose(qr, qr_T.eval(), 1e-3)
         assert_allclose(tau, tau_, 1e-3)
+        assert_allclose(tau, tau_T, 1e-3)
         assert not info and not numpy.array(info_)
 
     yield assert_geqrf_allclose, 10, 10
