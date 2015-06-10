a0f9969bd23bfa73c219d1bc29f688695896d179
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-06-10 17:20:01 -0400

    Implemented ormqr

--- theano/sandbox/cuda/cusolver.py
+++ theano/sandbox/cuda/cusolver.py
@@ -6,6 +6,7 @@ from theano.tensor import reshape
 try:
     from scikits.cuda.cusolver import (_libcusolver, cusolverDnCreate,
                                        cusolverCheckStatus)
+    from scikits.cuda.cublas import _CUBLAS_SIDE_MODE, _CUBLAS_OP
     cusolver_available = True
 except (ImportError, OSError):
     cusolver_available = False
@@ -28,14 +29,13 @@ class GpuGeqrf(CuSolverOp):
     Notes
     -----
     Wrapping of the methods is done as in `scikits.cuda.cusolver`. The
-    interface is made to match that of `scipy.linalg.lapac. The interface
-    is intendend to mimick `scipy.linalg.lapack`.
+    interface is intendend to mimick `scipy.linalg.lapack`.
 
     """
     __props__ = ()
 
-    def output_type(self, inp):
-        return [CudaNdarrayType(broadcastable=[False] * inp.type.ndim)(),
+    def output_type(self):
+        return [CudaNdarrayType(broadcastable=[False, False])(),
                 CudaNdarrayType(broadcastable=[False])(),
                 CudaNdarrayType(broadcastable=[False])(),
                 CudaNdarrayType(broadcastable=[])()]
@@ -44,7 +44,7 @@ class GpuGeqrf(CuSolverOp):
         inp = as_cuda_ndarray_variable(inp)
         assert inp.ndim == 2
 
-        return theano.Apply(self, [inp], self.output_type(inp))
+        return theano.Apply(self, [inp], self.output_type())
 
     def _make_thunk(self, node, storage_map, _, no_recycling=[]):
         inputs = [storage_map[v] for v in node.inputs]
@@ -73,7 +73,89 @@ class GpuGeqrf(CuSolverOp):
         return thunk
 
 
-geqrf = GpuGeqrf()
+gpu_geqrf = GpuGeqrf()
+
+
+class GpuOrmqr(CuSolverOp):
+    """Wrapper of cuSOLVER's implementation of the ormqr LAPACK method.
+
+    Notes
+    -----
+    Wrapping of the methods is done as in `scikits.cuda.cusolver`. The
+    interface is intendend to mimick `scipy.linalg.lapack`.
+
+    """
+    __props__ = ()
+
+    def __init__(self, side, trans, Lwork):
+        self.side = side
+        self.trans = trans
+        self.Lwork = Lwork
+        super(GpuOrmqr, self).__init__()
+
+    def output_type(self):
+        return [CudaNdarrayType(broadcastable=[False, False])(),
+                CudaNdarrayType(broadcastable=[False])(),
+                CudaNdarrayType(broadcastable=[])()]
+
+    def make_node(self, inp1, inp2, inp3):
+        inp1 = as_cuda_ndarray_variable(inp1)
+        inp2 = as_cuda_ndarray_variable(inp2)
+        inp3 = as_cuda_ndarray_variable(inp3)
+
+        assert inp1.ndim == 2
+        assert inp2.ndim == 1
+        assert inp3.ndim == 2
+
+        return theano.Apply(self, [inp1, inp2, inp3], self.output_type())
+
+    def _make_thunk(self, node, storage_map, _, no_recycling=[]):
+        inputs = [storage_map[v] for v in node.inputs]
+        outputs = [storage_map[v] for v in node.outputs]
+
+        def thunk():
+            A = inputs[0][0]
+            TAU = inputs[1][0]
+            C = inputs[2][0]
+            lda, k = A.shape
+            m, n = C.shape
+            ldc = max(1, m)
+
+            # LAPACK routines expect Fortran order
+            A_T = dimshuffle(A, (1, 0)).reshape((lda, k))
+            A_copy = A_T.copy()
+            C_T = dimshuffle(C, (1, 0)).reshape((m, n))
+            C_copy = C_T.copy()
+
+            assert self.side in ('L', 'R')
+            assert self.trans in ('N', 'T')
+            assert ldc >= max(1, m)
+            if self.side == 'L':
+                assert lda >= max(1, m)
+                assert self.Lwork >= max(1, n)
+                assert m >= k >= 0
+            else:
+                assert lda >= max(1, n)
+                assert self.Lwork >= max(1, m)
+                assert n >= k >= 0
+
+            Work = CudaNdarray.zeros((self.Lwork,))
+            devInfo = CudaNdarray.zeros((1,))
+
+            cusolverDnSormqr(handle, _CUBLAS_SIDE_MODE[self.side],
+                             _CUBLAS_OP[self.trans], m, n, k,
+                             A_copy.gpudata, lda, TAU.gpudata, C_copy.gpudata,
+                             ldc, Work.gpudata, self.Lwork, devInfo.gpudata)
+
+            outputs[0][0] = reshape(C_copy, (n, m)).dimshuffle((1, 0))
+            outputs[1][0] = Work
+            outputs[2][0] = devInfo
+
+        return thunk
+
+
+def gpu_ormqr(side, trans, a, tau, c, lwork):
+    return GpuOrmqr(side, trans, lwork)(a, tau, c)
 
 
 if cusolver_available:
@@ -113,3 +195,27 @@ if cusolver_available:
                                                           ctypes.byref(Lwork))
         cusolverCheckStatus(status)
         return Lwork.value
+
+    _libcusolver.cusolverDnSormqr.restype = int
+    _libcusolver.cusolverDnSormqr.argtypes = [ctypes.c_void_p,
+                                              ctypes.c_int,
+                                              ctypes.c_int,
+                                              ctypes.c_int,
+                                              ctypes.c_int,
+                                              ctypes.c_int,
+                                              ctypes.c_void_p,
+                                              ctypes.c_int,
+                                              ctypes.c_void_p,
+                                              ctypes.c_void_p,
+                                              ctypes.c_int,
+                                              ctypes.c_void_p,
+                                              ctypes.c_int,
+                                              ctypes.c_void_p]
+
+    def cusolverDnSormqr(handle, side, trans, m, n, k, A, lda, tau, C, ldc,
+                         work, lwork, devInfo):
+        status = _libcusolver.cusolverDnSormqr(handle, side, trans, m, n, k,
+                                               int(A), lda, int(tau), int(C),
+                                               ldc, int(work), lwork,
+                                               int(devInfo))
+        cusolverCheckStatus(status)
--- theano/sandbox/cuda/tests/test_cusolver.py
+++ theano/sandbox/cuda/tests/test_cusolver.py
@@ -2,12 +2,13 @@ import numpy
 from nose.plugins.skip import SkipTest
 from numpy.testing import assert_allclose
 try:
-    from scipy.linalg.lapack import dgeqrf
+    from scipy.linalg.lapack import dgeqrf, dormqr
 except ImportError:
     raise SkipTest('SciPy not available')
 
 import theano
-from theano.sandbox.cuda.cusolver import geqrf, cusolver_available
+from theano.sandbox.cuda.cusolver import (gpu_geqrf, gpu_ormqr,
+                                          cusolver_available)
 from theano.sandbox.cuda import cuda_available, CudaNdarray
 
 if not cuda_available:
@@ -23,12 +24,11 @@ def test_geqrf():
         A = rng.rand(m, n).astype(theano.config.floatX)
         A_ = theano.shared(CudaNdarray(A), borrow=True)
         A_T = theano.shared(CudaNdarray(A.T), borrow=True)
-        theano.config.optimizer = 'None'
-        dgeqrf_ = theano.function([], geqrf(A_))
-        dgeqrf_T = theano.function([], geqrf(A_T.T))
+        dgeqrf_ = theano.function([], gpu_geqrf(A_))
+        dgeqrf_T = theano.function([], gpu_geqrf(A_T.T))
+        qr, tau, work, info = dgeqrf(A)
         qr_, tau_, work_, info_ = dgeqrf_()
         qr_T, tau_T, work_T, info_T = dgeqrf_T()
-        qr, tau, work, info = dgeqrf(numpy.array(A))
         assert_allclose(qr, qr_.eval(), 1e-3)
         assert_allclose(qr, qr_T.eval(), 1e-3)
         assert_allclose(tau, tau_, 1e-3)
@@ -38,3 +38,39 @@ def test_geqrf():
     yield assert_geqrf_allclose, 10, 10
     yield assert_geqrf_allclose, 20, 10
     yield assert_geqrf_allclose, 10, 20
+
+
+def test_ormqr():
+    rng = numpy.random.RandomState(1)
+
+    def assert_ormqr_allclose(side, trans, a, tau, m, n, lwork):
+        a = a.astype(theano.config.floatX)
+        tau = tau.astype(theano.config.floatX)
+
+        C = rng.rand(m, n).astype(theano.config.floatX)
+        a_ = theano.shared(a)
+        tau_ = theano.shared(tau)
+        C_ = theano.shared(CudaNdarray(C), borrow=True)
+        C_T = theano.shared(CudaNdarray(C.T), borrow=True)
+        dormqr_ = theano.function([], gpu_ormqr(side, trans, a_, tau_,
+                                                C_, lwork))
+        dormqr_T = theano.function([], gpu_ormqr(side, trans, a_, tau_,
+                                                 C_T.T, lwork))
+        cq, work, info = dormqr(side, trans, a, tau, C, lwork)
+        cq_, work_, info_ = dormqr_()
+        cq_T, work_T, info_T = dormqr_T()
+        assert_allclose(cq, cq_.eval(), 1e-3)
+        assert_allclose(cq, cq_T.eval(), 1e-3)
+        assert not info and not numpy.array(info_)
+
+    a, tau, _, _ = dgeqrf(numpy.random.rand(10, 10))
+
+    yield assert_ormqr_allclose, 'L', 'N', a, tau, 10, 10, 10
+    yield assert_ormqr_allclose, 'R', 'N', a, tau, 10, 10, 10
+    yield assert_ormqr_allclose, 'L', 'T', a, tau, 10, 10, 10
+    yield assert_ormqr_allclose, 'R', 'T', a, tau, 10, 10, 10
+
+    a, tau, _, _ = dgeqrf(numpy.random.rand(20, 10))
+
+    yield assert_ormqr_allclose, 'L', 'N', a, tau, 10, 10, 10
+    yield assert_ormqr_allclose, 'R', 'T', a, tau, 10, 10, 10
