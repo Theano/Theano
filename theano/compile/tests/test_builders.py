9b635db7612352c956800e1136fb35527dfb3bf8
ChienliMa <maqianlie@gmail.com>
2015-07-09 20:21:30 +0800

    add other test case

--- theano/compile/tests/test_builders.py
+++ theano/compile/tests/test_builders.py
@@ -7,6 +7,7 @@ from theano.compile import function
 
 from theano import tensor
 from theano import tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
 
 from theano.compile.builders import OpFromGraph
 
@@ -109,15 +110,44 @@ class T_OpFromGraph(unittest.TestCase):
                               fn(xv, yv, zv))
     
     def test_connection_pattern(self):
+        # Basic case 
         x, y, z = T.matrices('xyz')
         out1 = x * y
         out2 = y * z
 
-        op = OpFromGraph([x ,y, z], [out1, out2], mode='FAST_RUN')
-        results = op.connection_pattern(None)
+        op1 = OpFromGraph([x ,y, z], [out1, out2], mode='FAST_RUN')
+        results = op1.connection_pattern(None)
         expect_result = [[True, False],
                          [True, True],
                          [False, True]]
+        assert results == expect_result
+
+        # Graph with ops that don't have a 'full' connection pattern
+        # and with ops that have multiple outputs 
+        m, n, p, q = T.matrices('mnpq')
+        o1, o2 = op1(m, n, p)
+        out1, out2 = op1(o1, q, o2)
+        op2 = OpFromGraph([m, n, p, q], [out1, out2], mode='FAST_RUN')
+
+        results = op2.connection_pattern(None)
+        expect_result = [[True, False],
+                         [True, True],
+                         [False, True],
+                         [True, True]]
+        assert results == expect_result
+
+        # Inner graph where some computation doesn't rely on explicit inputs
+        srng = RandomStreams(seed=234)
+        rv_u = srng.uniform((2,2))
+        x, y = T.matrices('xy')
+        out1 = x + rv_u
+        out2 = y + 3
+        out3 = 3 + rv_u
+        op3 = OpFromGraph([x, y], [out1, out2, out3], mode='FAST_RUN')
+
+        results = op2.connection_pattern(None)
+        expect_result = [[True, False, False],
+                         [False, True, False]]
         assert results == expect_result
 
 
