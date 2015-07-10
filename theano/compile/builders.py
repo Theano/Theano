cdb2f7c2e6a542d1dbdca1b56716f6d54f7c1703
ChienliMa <maqianlie@gmail.com>
2015-07-10 15:15:27 +0800

    delete unused input

--- theano/compile/builders.py
+++ theano/compile/builders.py
@@ -1,11 +1,9 @@
-import numpy
-
 import theano
 from theano import gof
 from theano.compat import izip
 from theano.compile.function_module import orig_function
 from theano.compile import SharedVariable, rebuild_collect_shared
-from theano.gof import ops_with_inner_function, FunctionGraph
+from theano.gof import ops_with_inner_function
 from theano.gof.graph import io_connection_pattern
 
 
