01e7401f773d2c35ef3b5695cafe359177d61e9a
Iban Harlouchet <harlouci@eos3.iro.umontreal.ca>
2015-07-14 15:30:37 -0400

    Added __props__ and removed __eq__, __hash__ to extra_ops.py

--- theano/tensor/extra_ops.py
+++ theano/tensor/extra_ops.py
@@ -13,15 +13,10 @@ tensor = basic
 
 class CumsumOp(theano.Op):
     # See function cumsum for docstring
+    __props__=("axis")
+    
     def __init__(self, axis=None):
         self.axis = axis
-
-    def __eq__(self, other):
-        return (type(self) == type(other) and
-                self.axis == other.axis)
-
-    def __hash__(self):
-        return hash(type(self)) ^ hash(self.axis)
 
     def make_node(self, x):
         x = basic.as_tensor_variable(x)
@@ -135,15 +130,10 @@ def cumsum(x, axis=None):
 
 class CumprodOp(theano.Op):
     # See function cumprod for docstring
+    __props__=("axis")
+    
     def __init__(self, axis=None):
         self.axis = axis
-
-    def __eq__(self, other):
-        return (type(self) == type(other) and
-                self.axis == other.axis)
-
-    def __hash__(self):
-        return hash(type(self)) ^ hash(self.axis)
 
     def make_node(self, x):
         x = basic.as_tensor_variable(x)
@@ -259,6 +249,8 @@ def cumprod(x, axis=None):
 
 class DiffOp(theano.Op):
     # See function diff for docstring
+    __props__=("n", "axis")
+    
     def __init__(self, n=1, axis=-1):
         self.n = n
         self.axis = axis
@@ -266,14 +258,6 @@ class DiffOp(theano.Op):
         # TODO, make an optimization that remove this op in this case.
         if n == 0:
             self.view_map = {0: [0]}
-
-    def __eq__(self, other):
-        return (type(self) == type(other) and
-                self.n == other.n and
-                self.axis == other.axis)
-
-    def __hash__(self):
-        return hash(type(self)) ^ hash(self.n) ^ hash(self.axis)
 
     def make_node(self, x):
         x = basic.as_tensor_variable(x)
@@ -340,6 +324,7 @@ class BinCountOp(theano.Op):
     compatible_type = ('int8', 'int16', 'int32', 'int64',
                        'uint8', 'uint16', 'uint32', 'uint64')
     """Tuple of all compatible dtype for the parameter of this op."""
+    __props__=("minlength")
 
     def __init__(self, minlength=None):
         self.minlength = minlength
@@ -349,13 +334,6 @@ class BinCountOp(theano.Op):
                 raise NotImplementedError(
                     "BinCountOp with minlength attribute"
                     " requires NumPy 1.6 or higher.")
-
-    def __eq__(self, other):
-        return (type(self) == type(other) and
-                self.minlength == other.minlength)
-
-    def __hash__(self):
-        return hash(type(self)) ^ hash(self.minlength)
 
     def make_node(self, x, weights):
         warnings.warn((
@@ -534,16 +512,10 @@ def compress(condition, x, axis=None):
 
 class RepeatOp(theano.Op):
     # See the repeat function for docstring
-
+    __props__=("axis")
+    
     def __init__(self, axis=None):
         self.axis = axis
-
-    def __eq__(self, other):
-        return (type(self) == type(other) and
-                self.axis == other.axis)
-
-    def __hash__(self):
-        return hash(type(self)) ^ hash(self.axis)
 
     def make_node(self, x, repeats):
         x = basic.as_tensor_variable(x)
@@ -713,12 +685,8 @@ def repeat(x, repeats, axis=None):
  
 class Bartlett(gof.Op):
     # See function bartlett for docstring
-    def __eq__(self, other):
-        return type(self) == type(other)
-
-    def __hash__(self):
-        return hash(type(self))
-
+    __props__=()
+    
     def __str__(self):
         return self.__class__.__name__
 
@@ -775,11 +743,7 @@ def bartlett(M):
 
 class FillDiagonal(gof.Op):
     # See function fill_diagonal for docstring
-    def __eq__(self, other):
-        return type(self) == type(other)
-
-    def __hash__(self):
-        return hash(type(self))
+    __props__=()
 
     def __str__(self):
         return self.__class__.__name__
@@ -863,11 +827,7 @@ def fill_diagonal(a, val):
 
 class FillDiagonalOffset(gof.Op):
     # See function fill_diagonal_offset for docstring
-    def __eq__(self, other):
-        return type(self) == type(other)
-
-    def __hash__(self):
-        return hash(type(self))
+    __props__=()
 
     def __str__(self):
         return self.__class__.__name__
