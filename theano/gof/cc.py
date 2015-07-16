0d0b7e7240a0d63558757ead1da16af82025a028
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2015-07-16 14:54:58 -0400

    More general check
    Some clients can be strings other than 'output', for instance 'context'
    
    Conflicts:
    	theano/gof/cc.py

--- theano/gof/cc.py
+++ theano/gof/cc.py
@@ -282,12 +282,10 @@ def get_nothing(r, name, sub):
 
 def get_c_declare(r, name, sub):
     """Wrapper around c_declare that declares py_name"""
-    if any([c != "output" and getattr(c.op, 'check_input',
-           config.check_input) for (c, _) in r.clients]) or (
-            r.owner and getattr(r.owner.op, 'check_input', True)):
-    #if any([getattr(c.op, 'check_input', config.check_input)
-    #        for (c, _) in r.clients
-    #        if not isinstance(c, string_types)]) or (r.owner
+    if ((any([getattr(c.op, 'check_input', config.check_input)
+             for (c, _) in r.clients
+             if not isinstance(c, string_types)]) or
+         (r.owner and getattr(r.owner.op, 'check_input', True)))):
 
         c_declare = r.type.c_declare(name, sub, True)
     else:
