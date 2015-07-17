e5b19522c481859276144ee8c1be84d3162c4f1d
Arnaud Bergeron <abergeron@gmail.com>
2015-07-17 14:12:58 -0400

    Make sure the hash of a cmodule never changes.

--- theano/gof/cmodule.py
+++ theano/gof/cmodule.py
@@ -253,7 +253,10 @@ static struct PyModuleDef moduledef = {{
         self.print_init(sio)
 
         rval = sio.getvalue()
-        self.code_hash = hash_from_code(rval)
+        # Make sure the hash of the code hasn't changed
+        h = hash_from_code(rval)
+        assert self.code_hash is None or self.code_hash == h
+        self.code_hash = h
         rval = re.sub(self.hash_placeholder, self.code_hash, rval)
         # Finalize the Module, so no support code or function
         # can be added
