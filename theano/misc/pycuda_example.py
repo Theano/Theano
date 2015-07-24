8dbb18198fcb1c854fb01a7a67cb39e837e80080
Iban Harlouchet <iban.harlouchet@gmail.com>
2015-07-24 10:41:19 -0400

    Removed 'name' from the props in theano/misc/pycuda_example.py

--- theano/misc/pycuda_example.py
+++ theano/misc/pycuda_example.py
@@ -279,7 +279,7 @@ class PycudaElemwiseSourceModuleOp(GpuOp):
 class PycudaElemwiseSourceModuleMakeThunkOp(Op):
     nin = property(lambda self: self.scalar_op.nin)
     nout = property(lambda self: self.scalar_op.nout)
-    __props__ = ("scalar_op", "inplace_pattern", "name")
+    __props__ = ("scalar_op", "inplace_pattern")
 
     def __init__(self, scalar_op, inplace_pattern=None, name=None):
         if inplace_pattern is None:
