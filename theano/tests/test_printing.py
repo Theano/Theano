677ead94c5e30d6f9a5f4ac422bfc2d8a8922973
Iban Harlouchet <iban.harlouchet@gmail.com>
2015-07-27 10:17:09 -0400

    Added dtype='int16' for debugprint2 and 'int8' for debugprint3

--- theano/tests/test_printing.py
+++ theano/tests/test_printing.py
@@ -344,7 +344,7 @@ def test_scan_debugprint2():
        | |Subtensor{int64} [@J] ''
        |   |Shape [@K] ''
        |   | |Subtensor{int64::} [@L] ''
-       |   |   |ARange [@M] ''
+       |   |   |ARange{dtype='int16'} [@M] ''
        |   |   | |TensorConstant{0} [@N]
        |   |   | |TensorConstant{10000} [@O]
        |   |   | |TensorConstant{1} [@P]
@@ -427,7 +427,7 @@ def test_scan_debugprint3():
        | |Subtensor{int64} [@J] ''
        |   |Shape [@K] ''
        |   | |Subtensor{int64::} [@L] ''
-       |   |   |ARange [@M] ''
+       |   |   |ARange{dtype='int8'} [@M] ''
        |   |   | |TensorConstant{0} [@N]
        |   |   | |TensorConstant{10} [@O]
        |   |   | |TensorConstant{1} [@P]
