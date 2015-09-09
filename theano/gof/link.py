a8b4fe0b9fff8a3999573e9f6e18580d87bbf081
Frederic <nouiz@nouiz.org>
2015-09-09 10:44:34 -0400

    Do not crash when we try to add extra info to existing error

--- theano/gof/link.py
+++ theano/gof/link.py
@@ -302,8 +302,15 @@ def raise_with_op(node, thunk=None, exc_info=None, storage_map=None):
             "HINT: Use the Theano flag 'exception_verbosity=high'"
             " for a debugprint and storage map footprint of this apply node.")
 
-    exc_value = exc_type(str(exc_value) + detailed_err_msg +
-                         '\n' + '\n'.join(hints))
+    try:
+        exc_value = exc_type(str(exc_value) + detailed_err_msg +
+                             '\n' + '\n'.join(hints))
+    except TypeError:
+        print("WARNING: %s error do not allow us to add extra error message" %
+              str(exc_type))
+        # Some exception need extra parameter in inputs. So forget the
+        # extra long error message in that case.
+        pass
     reraise(exc_type, exc_value, exc_trace)
 
 
