0e84e63c97aa746dcf24a609273fcf7381f9c0b3
Arnaud Bergeron <abergeron@gmail.com>
2015-09-11 15:49:02 -0400

    Some flake8 fixes.

--- theano/sandbox/gpuarray/tests/test_neighbours.py
+++ theano/sandbox/gpuarray/tests/test_neighbours.py
@@ -1,9 +1,7 @@
-import unittest
 
 from theano.tensor.nnet.tests import test_neighbours
 # We let that import do the init of the back-end if needed.
-from .test_basic_ops import (mode_with_gpu,
-                             mode_without_gpu)
+from .test_basic_ops import mode_with_gpu
 
 from ..neighbours import GpuImages2Neibs
 
