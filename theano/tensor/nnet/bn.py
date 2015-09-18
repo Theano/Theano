b536fab483b8f9b120fe9d50277550f18a4ac70d
Nicolas Ballas <ballas.n@gmail.com>
2015-09-18 12:20:48 -0400

    update comment

--- theano/tensor/nnet/bn.py
+++ theano/tensor/nnet/bn.py
@@ -33,6 +33,7 @@ def batch_normalization(inputs, gamma, beta, mean, std):
     back-propagation, this implementation lower the memory usage, however,
     it is 5-10% slower than a naive theano implementation, as it redo
     some foward computations for the backprop.
+    Work also on GPU
 
     Parameters
     ----------
