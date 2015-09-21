fcbbca3ae80d65b8554d11080166984d28c82d89
AlOa <orlandi.alberto@gmail.com>
2015-09-21 19:16:17 +0200

    Use of shared variables added to the howto

--- doc/tutorial/shared_library_howto/function.py
+++ doc/tutorial/shared_library_howto/function.py
@@ -4,8 +4,9 @@ import numpy as np
 
 if __name__ == '__main__':
     x = T.matrix('x')
-    y = T.vector('y')
+    W_values = np.asarray([1, 2, 3], dtype=theano.config.floatX)
+    W = theano.shared(value=W_values, name='W', borrow=True)
     linker = theano.gof.CLinker(c_callable=True)
     mode = theano.Mode(linker=linker)
-    f = theano.function([x, y], [2 * x, T.dot(y, x)], mode=mode)
+    f = theano.function([x], [2 * x, T.dot(W, x)], mode=mode)
     print(f.fn.filename)
