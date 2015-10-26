2abad4b785868196ebd5eabbb2fe30614461469a
Arnaud Bergeron <abergeron@gmail.com>
2015-10-26 15:42:15 -0400

    More fixes to doc syntax.

--- theano/sandbox/cuda/var.py
+++ theano/sandbox/cuda/var.py
@@ -126,14 +126,17 @@ class CudaNdarraySharedVariable(_operators, SharedVariable):
 
             * The destination on the GPU must be c_contiguous.
             * The source is on the CPU.
-            * The old value must have the same dtype as the new value (which is
-            a given for now, since only float32 is supported).
+            * The old value must have the same dtype as the new value
+              (which is a given for now, since only float32 is
+              supported).
             * The old and new value must have the same shape.
-            * The old value is being completely replaced by the new value (not
-            partially modified, e.g. by replacing some subtensor of it).
-            * You change the value of the shared variable via set_value, not via
-            the .value accessors. You should not use the .value accessors
-            anyway, since they will soon be deprecated and removed.
+            * The old value is being completely replaced by the new
+              value (not partially modified, e.g. by replacing some
+              subtensor of it).
+            * You change the value of the shared variable via
+              set_value, not via the .value accessors. You should not
+              use the .value accessors anyway, since they will soon be
+              deprecated and removed.
 
         It is also worth mentioning that, for efficient transfer to the GPU,
         Theano will make the new data ``c_contiguous``. This can require an
