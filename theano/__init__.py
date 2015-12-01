8ca002e9128d763ff19f9f7cd156e1c0e99cb5da
joncrall <erotemic@gmail.com>
2015-12-01 14:56:12 -0500

    Added config variable to disable initial testing of nvidia drivers

--- theano/__init__.py
+++ theano/__init__.py
@@ -107,7 +107,8 @@ if config.device.startswith('gpu') or config.init_gpu_device.startswith('gpu'):
     if theano.sandbox.cuda.cuda_available:
         import theano.sandbox.cuda.tests.test_driver
 
-        theano.sandbox.cuda.tests.test_driver.test_nvidia_driver1()
+        if config.enable_initial_driver_test:
+            theano.sandbox.cuda.tests.test_driver.test_nvidia_driver1()
 
 if (config.device.startswith('cuda') or
         config.device.startswith('opencl') or
