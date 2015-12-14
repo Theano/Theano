e2fbb03e763a30da7a43dfa1171b00318560110a
gw0 [http://gw.tnode.com/] <gw.2015@tnode.com>
2015-12-14 16:50:08 +0100

    Remove unused import of pydot.

--- theano/d3viz/__init__.py
+++ theano/d3viz/__init__.py
@@ -1,11 +1,1 @@
 from theano.d3viz.d3viz import d3viz, d3write
-
-has_requirements = False
-try:
-    # pydot-ng is a fork of pydot that is better maintained
-    import pydot_ng as pd
-except ImportError:
-    # fall back on pydot if necessary
-    import pydot as pd
-if pd.find_graphviz():
-    has_requirements = True
