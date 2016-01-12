c37a6bb37bdabde45d0fe242e737143687be15e0
Arnaud Bergeron <abergeron@gmail.com>
2016-01-12 13:34:25 -0500

    Fix test so that it actually crashes on unfixed code.

--- theano/sandbox/gpuarray/tests/test_opt.py
+++ theano/sandbox/gpuarray/tests/test_opt.py
@@ -358,10 +358,14 @@ def test_local_gpu_elemwise():
     utt.assert_allclose(out[1], a_v[::2] * c_v[::2])
 
 
-def test_local_lift_abstractconv_gpu_shape(self):
-    s = tensor.ivector()
-    a = tensor.alloc(0, s[0], s[1], s[2], s[3])
-    b = tensor.alloc(0, s[0], s[1], s[2], s[3])
-    c = tensor.conv.conv2d(a, b)
-    g = theano.grad(c)
-    f = theano.function([s], g, mode=mode_with_gpu)
+def test_local_lift_abstractconv_gpu_shape():
+    prev = theano.config.on_opt_error
+    try:
+        theano.config.on_opt_error = 'raise'
+        s = tensor.ivector()
+        a = tensor.ftensor4()
+        b = tensor.ftensor4()
+        c = tensor.nnet.abstract_conv.AbstractConv2d_gradWeights()(a, b, s)
+        f = theano.function([s, a, b], c, mode=mode_with_gpu)
+    finally:
+        theano.config.on_opt_error = prev
