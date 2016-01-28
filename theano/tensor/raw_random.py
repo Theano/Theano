0d82fe2ee8ca6638ea3a52fbba0ec87db7e0069b
Samira Shabanian <s.shabanian@gmail.com>
2016-01-28 18:38:42 -0500

    change shape dimensions int32 to int64

--- theano/tensor/raw_random.py
+++ theano/tensor/raw_random.py
@@ -520,12 +520,7 @@ def binomial(random_state, size=None, n=1, p=0.5, ndim=None,
     n = tensor.as_tensor_variable(n)
     p = tensor.as_tensor_variable(p)
     ndim, size, bcast = _infer_ndim_bcast(ndim, size, n, p)
-    #if n.dtype == 'int64':
     # THIS WORKS AROUND A NUMPY BUG on 32bit machine
-    # Erase when the following works on a 32bit machine:
-    # numpy.random.binomial(
-    #          n=numpy.asarray([2,3,4], dtype='int64'),
-    #          p=numpy.asarray([.1, .2, .3], dtype='float64'))
     n = tensor.cast(n, 'int64')
     op = RandomFunction('binomial',
                         tensor.TensorType(dtype=dtype,
