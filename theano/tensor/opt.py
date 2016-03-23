ce73e7d5988279a0c06e07bea24edbe6e757e884
sentient07 <vxrram95@gmail.com>
2016-03-24 03:47:52 +0530

    Changed a decorator

--- theano/tensor/opt.py
+++ theano/tensor/opt.py
@@ -605,7 +605,7 @@ def local_dimshuffle_lift(node):
 
 
 @register_canonicalize
-@gof.local_optimizer([T.DimShuffle])
+@gof.local_optimizer([DimShuffle])
 def local_lift_transpose_through_dot(node):
     """
     dot(x,y).T -> dot(y.T, x.T)
