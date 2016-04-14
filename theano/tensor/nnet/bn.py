037fd298f4e91d1a9b6bee439c8c62f8bee55006
Samira Shabanian <s.shabanian@gmail.com>
2016-04-14 17:37:24 -0400

    Disable test values in BNComposite and get_substream_rstates

--- theano/tensor/nnet/bn.py
+++ theano/tensor/nnet/bn.py
@@ -7,6 +7,7 @@ from theano.scalar import add, sub, true_div, mul
 class BNComposite(Composite):
     init_param = ('dtype',)
 
+    @theano.configparser.change_flags(compute_test_value='off')
     def __init__(self, dtype):
         self.dtype = dtype
         x = theano.scalar.Scalar(dtype=dtype).make_variable()
