77ac754f16b86bf7ecc0033dd1310ff8f178f9a8
Frederic Bastien <nouiz@nouiz.org>
2016-04-20 08:48:25 -0400

    Add the reverse version of local_gpu_contiguous

--- theano/sandbox/cuda/opt.py
+++ theano/sandbox/cuda/opt.py
@@ -2281,13 +2281,18 @@ def local_gpu_contiguous_gpu_contiguous(node):
 
 
 @register_opt('fast_compile')
-@local_optimizer([tensor.extra_ops.CpuContiguous])
+@local_optimizer([GpuFromHost, tensor.extra_ops.CpuContiguous])
 def local_gpu_contiguous(node):
     if isinstance(node.op, tensor.extra_ops.CpuContiguous):
         x, = node.inputs
         if x.owner and isinstance(x.owner.op, HostFromGpu):
             gpu_x, = x.owner.inputs
             return [tensor.as_tensor_variable(gpu_contiguous(gpu_x))]
+    if isinstance(node.op, GpuFromHost):
+        x, = node.inputs
+        if x.owner and isinstance(x.owner.op, tensor.extra_ops.CpuContiguous):
+            gpu_x, = x.owner.inputs
+            return [gpu_contiguous(gpu_x)]
     return False
 
 
