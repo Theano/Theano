77996dea5be6332d9346390faa8a38b24edf1c59
Frederic Bastien <nouiz@nouiz.org>
2016-04-26 09:56:27 -0400

    Use the right mode in a CorrMM test. Why it was passing before?

--- theano/tensor/nnet/tests/test_corr.py
+++ theano/tensor/nnet/tests/test_corr.py
@@ -132,7 +132,8 @@ class TestCorr2D(utt.InferShapeTester):
 
         # TEST GRADIENT
         if verify_grad:
-            utt.verify_grad(sym_CorrMM, [orig_image_data, filter_data])
+            utt.verify_grad(sym_CorrMM, [orig_image_data, filter_data],
+                            mode=self.mode)
 
     @attr('slow')
     def test_basic(self):
