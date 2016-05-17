341ac43647e770cbed51c39a55c5372439d0523e
Frederic Bastien <nouiz@nouiz.org>
2016-05-17 12:10:21 -0400

    Better error related to gh-3376

--- theano/__init__.py
+++ theano/__init__.py
@@ -21,6 +21,14 @@ which may be rendered with Sphinx. A rendered version is
 maintained at http://www.deeplearning.net/software/theano/library/
 
 """
+import sys
+
+if sys.platform == 'win32' and sys.version_info[0:2] == (3, 5):
+    raise RuntimeError(
+        "Theano do not support Python 3.5 on Windows. Use Python 2.7 or 3.4.")
+
+
+
 from __future__ import absolute_import, print_function, division
 
 __docformat__ = "restructuredtext en"
