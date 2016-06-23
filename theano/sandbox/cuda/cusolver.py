4e5efda8330f038a68cc7f4b2340dcfab461ec44
Yaroslav Ganin <ganin@skoltech.ru>
2016-06-23 14:44:32 -0400

    Addressed Fred's concerns

--- theano/sandbox/cuda/cusolver.py
+++ theano/sandbox/cuda/cusolver.py
@@ -1,10 +1,11 @@
-from __future__ import absolute_import, print_function, division
-import pkg_resources
+from __future__ import absolute_import, division, print_function
 
+import pkg_resources
 import theano
-from theano.sandbox.cuda.type import CudaNdarrayType
+
 from theano.sandbox.cuda import CudaNdarray, GpuOp
 from theano.sandbox.cuda.basic_ops import as_cuda_ndarray_variable
+from theano.sandbox.cuda.type import CudaNdarrayType
 
 try:
     from theano.sandbox.cuda import cuda_ndarray
@@ -20,9 +21,10 @@ try:
 except (ImportError, OSError, RuntimeError, pkg_resources.DistributionNotFound):
     pass
 
-cusolver_handle = [None]
+cusolver_handle = None
 
-class GpuSolve(GpuOp):
+
+class GpuCusolverSolve(GpuOp):
     """
     CUSOLVER GPU solver OP.
 
@@ -37,10 +39,7 @@ class GpuSolve(GpuOp):
 
     def __init__(self, trans='N'):
         self.trans = trans
-        super(GpuSolve, self).__init__()
-
-    def output_type(self, inp):
-        return CudaNdarrayType(broadcastable=[False] * inp.type.ndim)
+        super(GpuCusolverSolve, self).__init__()
 
     def make_node(self, inp1, inp2):
         inp1 = as_cuda_ndarray_variable(inp1)
@@ -48,7 +47,9 @@ class GpuSolve(GpuOp):
 
         assert inp1.ndim == 2
         assert inp2.ndim == 2
-        return theano.Apply(self, [inp1, inp2], [self.output_type(inp1)()])
+        return theano.Apply(
+            self, [inp1, inp2],
+            [CudaNdarrayType(broadcastable=[False] * inp1.type.ndim)()])
 
     def make_thunk(self,
                    node,
@@ -56,29 +57,31 @@ class GpuSolve(GpuOp):
                    no_recycling=[]):
         if not cusolver_available:
             raise RuntimeError('CUSOLVER is not available and '
-                               'GpuSolve Op can not be constructed.')
+                               'GpuCusolverSolve Op can not be constructed.')
 
         inputs = [storage_map[v] for v in node.inputs]
         outputs = [storage_map[v] for v in node.outputs]
 
         def thunk():
-            # size of the matrices to invert
+            global cusolver_handle
+
+            # Size of the matrices to invert.
             z = outputs[0]
 
-            # Matrix
+            # Matrix.
             A = inputs[0][0]
 
-            # Solution vectors
+            # Solution vectors.
             b = inputs[1][0]
 
             # A is not explicitly converted between C and F order, instead we
-            # switch the "transpose" flag
+            # switch the "transpose" flag.
             if self.trans in ('T', 'C'):
                 trans = 'N'
             else:
                 trans = 'T'
 
-            # Convert b to F-order from c-order.
+            # Convert b to F-order from C-order.
             b_cpy = dimshuffle(b, (1, 0)).reshape((b.shape[0], b.shape[1]))
 
             # This copy forces allocation of a new C-contiguous buffer
@@ -86,67 +89,59 @@ class GpuSolve(GpuOp):
             A_cpy = A.copy()
             b_cpy = b_cpy.copy()
 
-            def cusolver_gpu_solve(A_, b_, trans='T'):
-                A_shape = A_.shape
-                b_shape = b_.shape
+            assert(len(A.shape) == 2)
+            assert(len(b.shape) == 2)
 
-                assert(len(A_shape) == 2)
-                assert(len(b_shape) == 2)
+            if trans in ['T', 'C']:
+                trans = 1
+                l, n = A.shape
+                k, m = b.shape
+                if n != k:
+                    raise ValueError('A and b must be aligned.')
+            elif trans in ['N']:
+                trans = 0
+                n, l = A.shape
+                k, m = b.shape
+                if l != m:
+                    raise ValueError('A and b must be aligned.')
+            else:
+                raise ValueError('Invalid value for trans')
 
-                if trans in ['T', 'C']:
-                    trans = 1
-                    l, n = A_shape
-                    k, m = b_shape
-                    if n != k:
-                        raise ValueError('A and b must be aligned.')
-                elif trans in ['N']:
-                    trans = 0
-                    n, l = A_shape
-                    k, m = b_shape
-                    if l != m:
-                        raise ValueError('A and b must be aligned.')
-                else:
-                    raise ValueError('Invalid value for trans')
+            lda = max(1, n)
+            ldb = max(1, n, l)
 
-                lda = max(1, n)
-                ldb = max(1, n, l)
+            A_ptr = A_cpy.gpudata
+            b_ptr = b_cpy.gpudata
 
-                A_ptr = A_.gpudata
-                b_ptr = b_.gpudata
+            if cusolver_handle is None:
+                cusolver_handle = cusolver.cusolverDnCreate()
 
-                if cusolver_handle[0] is None:
-                    cusolver_handle[0] = cusolver.cusolverDnCreate()
+            workspace_size = cusolver.cusolverDnSgetrf_bufferSize(
+                cusolver_handle, m, n, A_ptr, lda)
 
-                workspace_size = cusolver.cusolverDnSgetrf_bufferSize(
-                        cusolver_handle[0], m, n, A_ptr, lda)
-                
-                if (thunk.workspace is None or 
+            if (thunk.workspace is None or
                     thunk.workspace.size != workspace_size):
-                    thunk.workspace = CudaNdarray.zeros((workspace_size,))
-                
-                if thunk.pivots is None or thunk.pivots.size != min(m, n):
-                    thunk.pivots = CudaNdarray.zeros((min(m, n),))
+                thunk.workspace = CudaNdarray.zeros((workspace_size,))
 
-                if thunk.dev_info is None:
-                    thunk.dev_info = CudaNdarray.zeros((1,))
+            if thunk.pivots is None or thunk.pivots.size != min(m, n):
+                thunk.pivots = CudaNdarray.zeros((min(m, n),))
 
-                workspace_ptr = thunk.workspace.gpudata
-                pivots_ptr = thunk.pivots.gpudata
-                dev_info_ptr = thunk.dev_info.gpudata
+            if thunk.dev_info is None:
+                thunk.dev_info = CudaNdarray.zeros((1,))
 
-                cusolver.cusolverDnSgetrf(
-                        cusolver_handle[0], n, l, A_ptr, lda, workspace_ptr, 
-                        pivots_ptr, dev_info_ptr)
+            workspace_ptr = thunk.workspace.gpudata
+            pivots_ptr = thunk.pivots.gpudata
+            dev_info_ptr = thunk.dev_info.gpudata
 
-                cusolver.cusolverDnSgetrs(
-                        cusolver_handle[0], trans, n, m, A_ptr, lda, 
-                        pivots_ptr, b_ptr, ldb, dev_info_ptr)
+            cusolver.cusolverDnSgetrf(
+                cusolver_handle, n, l, A_ptr, lda, workspace_ptr,
+                pivots_ptr, dev_info_ptr)
 
-                return A_, b_
+            cusolver.cusolverDnSgetrs(
+                cusolver_handle, trans, n, m, A_ptr, lda,
+                pivots_ptr, b_ptr, ldb, dev_info_ptr)
 
-            A_pycuda, b_pycuda = cusolver_gpu_solve(A_cpy, b_cpy, trans)
-
-            # Convert b to F-order from c-order and assign it to output:
+            # Convert b to F-order from C-order and assign it to output.
             b_cpy = b_cpy.reshape(b.shape[::-1])
             b_cpy = dimshuffle(b_cpy, (1, 0))
             z[0] = b_cpy
@@ -161,4 +156,4 @@ class GpuSolve(GpuOp):
 
         return thunk
 
-gpu_solve = GpuSolve()
+gpu_solve = GpuCusolverSolve()
