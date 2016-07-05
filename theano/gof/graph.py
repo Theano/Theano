ddfd7d239a1e656cee850cdbc548da63f349c37d
Francesco Visin <fvisin@gmail.com>
2016-07-05 08:50:43 -0400

    Fix broken source links
    Conflicts:
    	theano/gof/fg.py

--- theano/gof/graph.py
+++ theano/gof/graph.py
@@ -1,9 +1,5 @@
 """
 Node classes (`Apply`, `Variable`) and expression graph algorithms.
-
-To read about what theano graphs are from a user perspective, have a look at
-`graph.html <../doc/graph.html>`__.
-
 """
 from __future__ import print_function
 
@@ -1237,9 +1233,9 @@ def as_string(i, o,
         Input `Variable` s.
     o : list
         Output `Variable` s.
-    leaf_formatter : function
+    leaf_formatter : theano.function
         Takes a `Variable`  and returns a string to describe it.
-    node_formatter : function
+    node_formatter : callable
         Takes an `Op`  and the list of strings corresponding to its arguments
         and returns a string to describe it.
 
