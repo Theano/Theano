b2177eaedd7298f86a9f0710aa2e273419baf579
ruslanagit <ruslana@cim.mcgill.ca>
2016-07-14 16:40:43 -0400

    Use nvcc flags to compile with cuDNN
    Fix for issue #4739

--- theano/sandbox/cuda/__init__.py
+++ theano/sandbox/cuda/__init__.py
@@ -304,7 +304,7 @@ if ((err = cudnnCreate(&_handle)) != CUDNN_STATUS_SUCCESS) {
             if config.nvcc.compiler_bindir:
                 params.extend(['--compiler-bindir',
                                config.nvcc.compiler_bindir])
-            [params.append(flag) for flag in config.nvcc.flags.split(' ') if flag] 
+            params.extend([flag for flag in config.nvcc.flags.split(' ') if flag]) 
             
             # Do not run here the test program. It would run on the
             # default gpu, not the one selected by the user. If mixed
