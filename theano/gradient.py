733ab929911db1550f733525d308fcc9787eace0
slefrancois <simon.lefrancois@umontreal.ca>
2016-07-27 15:50:43 -0400

    test gpu

--- theano/gradient.py
+++ theano/gradient.py
@@ -1,4 +1,4 @@
-"""Driver for gradient calculations. JENKINS"""
+"""Driver for gradient calculations. JENKINS a"""
 from __future__ import absolute_import, print_function, division
 import six.moves.builtins as builtins
 import logging
