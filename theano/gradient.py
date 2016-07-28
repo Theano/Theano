1aea677f3f289b160f9ba67f6d66e9ad13d28217
slefrancois <simon.lefrancois@umontreal.ca>
2016-07-28 13:41:57 -0400

    [ci skip]

--- theano/gradient.py
+++ theano/gradient.py
@@ -1,4 +1,4 @@
-"""Driver for gradient calculations. JENKINS 2n"""
+"""Driver for gradient calculations. JENKINS 2o"""
 from __future__ import absolute_import, print_function, division
 import six.moves.builtins as builtins
 import logging
