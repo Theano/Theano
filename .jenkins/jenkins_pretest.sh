c482713df0be40c3eb2c607b6969098fb9540600
slefrancois <simon.lefrancois@umontreal.ca>
2016-07-29 17:06:07 -0400

    [ci skip]

--- .jenkins/jenkins_pretest.sh
+++ .jenkins/jenkins_pretest.sh
@@ -6,10 +6,10 @@ source ~/.bashrc
 
 # Test flake8
 echo "===== Testing flake8"
-bin/theano-nose theano/tests/test_flake8.py
+bin/theano-nose theano/tests/test_flake8.py || exit 1
 
 # Test documentation
 echo "===== Testing documentation build"
-python doc/scripts/docgen.py --nopdf --check
+python doc/scripts/docgen.py --nopdf --check || exit 1
 echo "===== Testing documentation code snippets"
-python doc/scripts/docgen.py --test --check
+python doc/scripts/docgen.py --test --check || exit 1
