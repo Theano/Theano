ba4e428db1d09f1d5d266995f68e8bfbc5bf65ea
slefrancois <simon.lefrancois@umontreal.ca>
2016-07-29 16:20:09 -0400

    correct linebreaks in echo

--- .jenkins/jenkins_pretest.sh
+++ .jenkins/jenkins_pretest.sh
@@ -5,11 +5,11 @@
 source ~/.bashrc
 
 # Test flake8
-echo "\n===== Testing flake8"
+echo "===== Testing flake8"
 bin/theano-nose theano/tests/test_flake8.py || exit 1
 
 # Test documentation
-echo "\n===== Testing documentation build"
+echo "===== Testing documentation build"
 python doc/scripts/docgen.py --nopdf --check || exit 1
-echo "\n===== Testing documentation code snippets"
+echo "===== Testing documentation code snippets"
 python doc/scripts/docgen.py --test --check || exit 1
