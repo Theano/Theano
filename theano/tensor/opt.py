4826389628ca876c7d1dc37fa8e4ae93a61ead32
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2016-07-31 14:22:04 -0400

    Combine older and newer docstring

--- theano/tensor/opt.py
+++ theano/tensor/opt.py
@@ -4166,10 +4166,15 @@ register_canonicalize(local_reshape_chain(T.Reshape),
 @gof.local_optimizer([T.Reshape])
 def local_useless_reshape(node):
     """
-    Broadcastable dimensions in reshpae are replaced with dimshuffle.
+    Remove two kinds of useless reshape.
+
+    Remove Reshape when both the input and output have a single dimension.
+    Broadcastable dimensions in Reshape are replaced with dimshuffle.
     For example:
+        - reshape(v, (m,)) --> v  # if v.ndim == 1
         - reshape(x, (1, n)) --> dimshuffle{x,0}(reshape(x, (n,))
-        - reshape(x, (1, m, 1, n, 1, 1)) --> dimshuffle{x,0,x,1,x,x}(reshape(x, (m, n)))
+        - reshape(x, (1, m, 1, n, 1, 1))
+          --> dimshuffle{x,0,x,1,x,x}(reshape(x, (m, n)))
 
     """
     op = node.op
