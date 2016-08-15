70aac84f69ded1482e2a6137ae9af1b566776107
Ozan Çağlayan <ozancag@gmail.com>
2016-08-15 17:06:07 +0300

    sandbox/cuda: add old bad names to the end

--- theano/sandbox/cuda/opt.py
+++ theano/sandbox/cuda/opt.py
@@ -1352,7 +1352,8 @@ def cast(x, dtype):
 
 
 @register_opt()
-@local_optimizer([tensor.nnet.CrossentropySoftmaxArgmax1HotWithBias])
+@local_optimizer([tensor.nnet.CrossentropySoftmaxArgmax1HotWithBias],
+                 'local_gpu_crossentorpy_softmax_argmax_1hot_with_bias')
 def local_gpu_crossentropy_softmax_argmax_1hot_with_bias(node):
     if isinstance(node.op, tensor.nnet.CrossentropySoftmaxArgmax1HotWithBias):
         x, b, y = node.inputs
@@ -1381,7 +1382,8 @@ def local_gpu_crossentropy_softmax_argmax_1hot_with_bias(node):
 
 
 @register_opt()
-@local_optimizer([tensor.nnet.CrossentropySoftmax1HotWithBiasDx])
+@local_optimizer([tensor.nnet.CrossentropySoftmax1HotWithBiasDx],
+                 'local_gpu_crossentorpy_softmax_1hot_with_bias_dx')
 def local_gpu_crossentropy_softmax_1hot_with_bias_dx(node):
     if isinstance(node.op, tensor.nnet.CrossentropySoftmax1HotWithBiasDx):
         dnll, sm, yidx = node.inputs
