4de5d0c59e10cfd86d2b035ae961a97d88496652
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2016-08-16 19:48:18 -0400

    Update ellipsis indexing test for GPU
    We have to make the distinction between Op types in the original and
    optimized graph

--- theano/sandbox/cuda/tests/test_basic_ops.py
+++ theano/sandbox/cuda/tests/test_basic_ops.py
@@ -1014,6 +1014,7 @@ class T_subtensor(theano.tensor.tests.test_subtensor.T_subtensor):
     inc_sub = cuda.GpuIncSubtensor
     adv_sub1 = cuda.GpuAdvancedSubtensor1
     adv_incsub1 = cuda.GpuAdvancedIncSubtensor1
+    dimshuffle = cuda.GpuDimShuffle
     mode = mode_with_gpu
     dtype = 'float32'
     type = tcn.CudaNdarrayType
