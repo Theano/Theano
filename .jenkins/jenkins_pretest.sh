aa2b4136a3222a7d22f97a315f2faa8b11ea1cc5
slefrancois <simon.lefrancois@umontreal.ca>
2016-09-13 17:51:34 -0400

    add xunit to jenkins pretest

--- .jenkins/jenkins_pretest.sh
+++ .jenkins/jenkins_pretest.sh
@@ -10,7 +10,7 @@ export PATH=/usr/local/miniconda2/bin:$PATH
 
 # Test flake8
 echo "===== Testing flake8"
-bin/theano-nose theano/tests/test_flake8.py || exit 1
+bin/theano-nose theano/tests/test_flake8.py --with-xunit --xunit-file=theano_pre_tests.xml || exit 1
 
 # Test documentation
 echo "===== Testing documentation build"
