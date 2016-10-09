85551c989bb519976e1523be10f2492d317cad52
sentient07 <vxrram95@gmail.com>
2016-10-10 01:44:15 +0200

    Removed hash_from_dict and a fix for _c_code

--- theano/misc/pycuda_example.py
+++ theano/misc/pycuda_example.py
@@ -183,6 +183,7 @@ class PycudaElemwiseKernelOp(GpuOp):
 class PycudaElemwiseSourceModuleOp(GpuOp):
     nin = property(lambda self: self.scalar_op.nin)
     nout = property(lambda self: self.scalar_op.nout)
+
     __props__ = ("scalar_op", "inplace_pattern")
 
     def __init__(self, scalar_op, inplace_pattern=None, name=None):
