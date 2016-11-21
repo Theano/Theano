633eb5d3cb3cd8f2cabbe056ce468124e7f6fb06
notoraptor <stevenbocco@gmail.com>
2016-11-21 15:04:19 -0500

    test 11

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # Install miniconda to avoid compiling scipy
-if ! test -e $HOME/miniconda2/bin ; then
+if test -e $HOME/miniconda2/bin ; then
+    echo "miniconda already installed."
+else
+    echo "Installing miniconda."
     rm -rf $HOME/miniconda2
     wget -c https://repo.continuum.io/miniconda/Miniconda2-4.1.11-Linux-x86_64.sh -O $HOME/download/miniconda.sh
     chmod +x $HOME/download/miniconda.sh
     $HOME/download/miniconda.sh -b
-else
-    echo "miniconda already installed."
 fi
