6bd1fa897f71226715c54fb03075f6e31f4f2034
notoraptor <stevenbocco@gmail.com>
2016-11-21 14:33:35 -0500

    test 8

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -5,7 +5,7 @@ if ! test -e $HOME/miniconda2/bin ; then
     wget -c https://repo.continuum.io/miniconda/Miniconda2-4.1.11-Linux-x86_64.sh -O $HOME/download/miniconda.sh
     chmod +x $HOME/download/miniconda.sh
     $HOME/download/miniconda.sh -b
-    export PATH=/home/travis/miniconda2/bin:$PATH
 else
     echo "miniconda already installed."
 fi
+export PATH=/home/travis/miniconda2/bin:$PATH
