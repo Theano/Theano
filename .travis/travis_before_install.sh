494783f7c27cbfce59e73e278d41df6032e0334c
notoraptor <stevenbocco@gmail.com>
2016-11-21 14:21:32 -0500

    new test 7

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Install miniconda to avoid compiling scipy
 if ! test -e $HOME/miniconda2/bin ; then
+    rm -rf $HOME/miniconda2
     wget -c https://repo.continuum.io/miniconda/Miniconda2-4.1.11-Linux-x86_64.sh -O $HOME/download/miniconda.sh
     chmod +x $HOME/download/miniconda.sh
     $HOME/download/miniconda.sh -b
