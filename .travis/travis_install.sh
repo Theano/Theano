33a31e4db82fcbd8c44f95c1e1bb1e70fd618363
notoraptor <stevenbocco@gmail.com>
2016-11-22 09:09:41 -0500

    Small correction.

--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -8,6 +8,6 @@ else
 fi
 
 source activate pyenv
-if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then conda install mkl python=2.7 numpy=1.9.1 scipy=0.14.0 nose=1.3.0 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service libgfortran=1; fi
-if [[ $TRAVIS_PYTHON_VERSION == '3.3' ]]; then conda install mkl python=3.3 numpy=1.9.1 scipy=0.14.0 nose=1.3.4 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service; fi
+if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then conda install --yes -q mkl python=2.7 numpy=1.9.1 scipy=0.14.0 nose=1.3.0 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service libgfortran=1; fi
+if [[ $TRAVIS_PYTHON_VERSION == '3.3' ]]; then conda install --yes -q mkl python=3.3 numpy=1.9.1 scipy=0.14.0 nose=1.3.4 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service; fi
 source deactivate
