7defa656af7fcf36bdd99a69c62314c255f813d9
notoraptor <stevenbocco@gmail.com>
2016-11-22 08:58:41 -0500

    Some corrections and code simplification.

--- .travis/travis_before_install.sh
+++ .travis/travis_before_install.sh
@@ -5,6 +5,8 @@ if test -e $HOME/miniconda2/bin ; then
 else
     echo "Installing miniconda."
     rm -rf $HOME/miniconda2
+    mkdir -p $HOME/download
+    if [[ -d $HOME/download/miniconda.sh ]] ; then rm -rf $HOME/download/miniconda.sh ; fi
     wget -c https://repo.continuum.io/miniconda/Miniconda2-4.1.11-Linux-x86_64.sh -O $HOME/download/miniconda.sh
     chmod +x $HOME/download/miniconda.sh
     $HOME/download/miniconda.sh -b
--- .travis/travis_install.sh
+++ .travis/travis_install.sh
@@ -2,12 +2,12 @@
 # In Python 3.3, we test the min version of NumPy and SciPy. In Python 2.7, we test more recent version.
 if test -e $HOME/miniconda2/envs/pyenv ; then
     echo "pyenv already exists."
-    source activate pyenv
-    if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then conda install mkl python=2.7 numpy=1.9.1 scipy=0.14.0 nose=1.3.0 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service libgfortran=1; fi
-    if [[ $TRAVIS_PYTHON_VERSION == '3.3' ]]; then conda install mkl python=3.3 numpy=1.9.1 scipy=0.14.0 nose=1.3.4 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service; fi
-    source deactivate
 else
     echo "Creating pyenv."
-    if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then conda create --yes -q -n pyenv mkl python=2.7 numpy=1.9.1 scipy=0.14.0 nose=1.3.0 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service libgfortran=1; fi
-    if [[ $TRAVIS_PYTHON_VERSION == '3.3' ]]; then conda create --yes -q -n pyenv mkl python=3.3 numpy=1.9.1 scipy=0.14.0 nose=1.3.4 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service; fi
+    conda create --yes -q -n pyenv
 fi
+
+source activate pyenv
+if [[ $TRAVIS_PYTHON_VERSION == '2.7' ]]; then conda install mkl python=2.7 numpy=1.9.1 scipy=0.14.0 nose=1.3.0 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service libgfortran=1; fi
+if [[ $TRAVIS_PYTHON_VERSION == '3.3' ]]; then conda install mkl python=3.3 numpy=1.9.1 scipy=0.14.0 nose=1.3.4 pip flake8=2.3 six=1.9.0 pep8=1.6.2 pyflakes=0.8.1 sphinx mkl-service; fi
+source deactivate
