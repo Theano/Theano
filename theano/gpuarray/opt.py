d0043caf53023c5895f7c33622b28661dbcad220
Frederic Bastien <nouiz@nouiz.org>
2016-12-21 11:31:50 -0500

    Make GpuAdvancedIncSubtensor1_dev20 being introduced for vector_scalar case instead of GpuAdvancedIncSubtensor1

--- theano/gpuarray/opt.py
+++ theano/gpuarray/opt.py
@@ -1036,7 +1036,13 @@ def local_gpua_advanced_incsubtensor(op, context_name, inputs, outputs):
     set_instead_of_inc = op.set_instead_of_inc
 
     compute_capability = int(context.bin_id[-2])
-    if compute_capability < 2 or x.ndim != 2 or y.ndim != 2:
+    if compute_capability >= 2 and x.ndim == 1 and y.ndim == 0:
+        x = x.dimshuffle(0, 'x')
+        y = y.dimshuffle('x', 'x')
+        ret = GpuAdvancedIncSubtensor1_dev20(
+            set_instead_of_inc=set_instead_of_inc)(x, y, ilist).dimshuffle(0)
+        return ret
+    elif compute_capability < 2 or x.ndim != 2 or y.ndim != 2:
         return GpuAdvancedIncSubtensor1(
             set_instead_of_inc=set_instead_of_inc)
     else:
