7ee85c3f688be308b90e7e1cbc89357a56c90eba
Ciyong Chen <ciyong.chen@intel.com>
2017-01-09 12:54:52 +0800

    remove dummy files

--- theano/sandbox/mkl/tests/test_mkl_dummy.py
+++
@@ -1,8 +0,0 @@
-import theano
-from theano.sandbox.mkl.mkl_dummy import dummyOP
-
-x = theano.tensor.matrix('x', dtype='float32')
-f = dummyOP()(x)
-
-print("=====================================================")
-z = theano.function([x], f)
