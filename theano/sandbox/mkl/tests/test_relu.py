72e288c814f451dc53906cd7bfe3bb89fd23e022
Ciyong Chen <ciyong.chen@intel.com>
2017-01-11 15:05:31 +0800

    update relu unit test

--- theano/sandbox/mkl/tests/test_relu.py
+++ theano/sandbox/mkl/tests/test_relu.py
@@ -1,34 +1,121 @@
 from __future__ import absolute_import, print_function, division
 
 import numpy
+import unittest
+from itertools import product
 
-import theano.tensor as tensor
-from theano import config
+import theano.tensor as T
+from theano import function
 from theano.tests import unittest_tools as utt
-from theano.tensor.nnet import relu
+from theano.sandbox.mkl.mkl_relu import Relu
+from theano.sandbox.mkl.basic_ops import (U2IRelu, I2U)
+
+uniq_id = 0
 
 
-def test_relu():
-    x = tensor.ftensor4('x')
-    seed = utt.fetch_seed()
-    rng = numpy.random.RandomState(seed)
-    X = rng.randn(20, 20, 30, 30).astype(config.floatX)
+class TestMKLRelu(unittest.TestCase):
 
-    # test the base case, without custom alpha value
-    y = relu(x).eval({x: X})
-    assert numpy.allclose(y, numpy.maximum(X, 0))
+    def mkl_relu_func(*inputs):
+        global uniq_id
+        uniq_id += 1
 
-    # test for different constant alpha values (also outside of [0, 1])
-    for alpha in 0, 0.3, 1, 2, -0.3, -1, -2:
-        y = relu(x, alpha).eval({x: X})
-        assert numpy.allclose(y, numpy.where(X > 0, X, alpha * X))
+        if len(inputs) == 2:
+            # self, image
+            _, x, = inputs
 
-    def mp(input):
-       return relu(input, alpha)
+            x_internal = U2IRelu(uniq_id=uniq_id)(x)
+            reluOut = Relu(uniq_id=uniq_id)(x_internal)
+            output = I2U(uniq_id=uniq_id)(reluOut)
+        elif len(inputs) == 3:
+            # self, image, slope
+            _, x, slope, = inputs
 
-    imval = rng.rand(2, 3, 3, 4) * 10.0
-    for alpha in (0.0, 0.3, 1.0, -0.3, -1.0):
-        utt.verify_grad(mp, [imval], rng=rng)
+            x_internal = U2IRelu(slope=slope, uniq_id=uniq_id)(x)
+            reluOut = Relu(slope=slope, uniq_id=uniq_id)(x_internal)
+            output = I2U(uniq_id=uniq_id)(reluOut)
+        else:
+            raise ValueError("incorrect inputs list, should be 2 ~ 3 parameters!")
+
+        return output
+
+    def test_relu(self):
+        def ref(input):
+            return numpy.maximum(input, 0)
+
+        seed = utt.fetch_seed()
+        rng = numpy.random.RandomState(seed)
+
+        imgsize_list = ((5, 5), (6, 6), (6, 6), (8, 8))
+        n, c = 4, 2
+
+        image = T.dtensor4('image')
+        for imgsize in imgsize_list:
+            imval = rng.rand(n, c, imgsize[0], imgsize[1])
+
+            output_ref = ref(imval)
+
+            output = self.mkl_relu_func(image)
+
+            f = function([image, ], [output, ])
+            output_val = f(imval)
+
+        utt.assert_allclose(output_val, output_ref)
+
+    def test_relu_slope(self):
+        def ref(input, slope):
+            return numpy.where(input > 0, input, slope * input)
+
+        seed = utt.fetch_seed()
+        rng = numpy.random.RandomState(seed)
+
+        imgsize_list = ((5, 5), (6, 6), (6, 6), (8, 8))
+        slope_list = (0, 0.3, 1, 2, -0.3, -1, -2)
+        n, c = 4, 2
+
+        image = T.dtensor4('image')
+        for imgsize, slope in product(imgsize_list, slope_list):
+            imval = rng.rand(n, c, imgsize[0], imgsize[1])
+
+            output_ref = ref(imval, slope)
+
+            output = self.mkl_relu_func(image, slope)
+
+            f = function([image, ], [output, ])
+            output_val = f(imval)
+
+        utt.assert_allclose(output_val, output_ref)
+
+    def test_relu_grad(self):
+        seed = utt.fetch_seed()
+        rng = numpy.random.RandomState(seed)
+
+        imgsize_list = ((5, 5), (6, 6), (6, 6), (8, 8))
+        n, c = 4, 2
+
+        for imgsize in imgsize_list:
+            imval = rng.rand(n, c, imgsize[0], imgsize[1])
+
+            def mp(input):
+                return self.mkl_relu_func(input)
+
+            utt.verify_grad(mp, [imval], rng=rng)
+
+    def test_relu_slope_grad(self):
+        seed = utt.fetch_seed()
+        rng = numpy.random.RandomState(seed)
+
+        imgsize_list = ((5, 5), (6, 6), (6, 6), (8, 8))
+        slope_list = (0, 0.3, 1, 2, -0.3, -1, -2)
+        n, c = 4, 2
+
+        for imgsize, slope in product(imgsize_list, slope_list):
+            imval = rng.rand(n, c, imgsize[0], imgsize[1])
+
+            def mp(input):
+                return self.mkl_relu_func(input, slope)
+
+            utt.verify_grad(mp, [imval], rng=rng)
+
 
 if __name__ == "__main__":
     test_relu()
