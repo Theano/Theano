e8c3cadb8fab93e34240427eecea31473bb2a3be
jwoolley <jwoolley@nvidia.com>
2017-01-12 16:56:19 -0800

    Add warning message to entrypoint script

--- nvidia_entrypoint.sh
+++ nvidia_entrypoint.sh
@@ -14,8 +14,15 @@ All rights reserved.
 
 Various files include modifications (c) NVIDIA CORPORATION.  All rights reserved.
 NVIDIA modifications are covered by the license terms that apply to the underlying project or file.
-
 EOF
+
+if [[ "$(find /usr -name libcuda.so.1) " == " " || "$(ls /dev/nvidiactl) " == " " ]]; then
+  echo
+  echo "WARNING: NVIDIA Driver not detected.  GPU functionality will not be available."
+  echo "   Please use 'nvidia-docker run' to invoke this container; see /workspace/README.md for details."
+fi
+
+echo
 
 if [[ $# -eq 0 ]]; then
   exec "/bin/bash"
