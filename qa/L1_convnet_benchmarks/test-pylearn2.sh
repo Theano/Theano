86fdcb7007568ae6e5e80888f0922be053cc2fe2
Cliff Woolley <jwoolley@nvidia.com>
2017-02-07 18:32:45 +0000

    Pip install deps for the pylearn2 test so that it works properly

--- qa/L1_convnet_benchmarks/test-pylearn2.sh
+++ qa/L1_convnet_benchmarks/test-pylearn2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-(cd ../third_party/pylearn2 && pip install --no-deps -e .)
-(cd ../third_party/scikits.cuda && pip install --no-deps -e .)
+(cd ../third_party/pylearn2 && pip install -e .)
+(cd ../third_party/scikits.cuda && pip install .)
 
 cd ../third_party/convnet-benchmarks/theano
 SKIP=legacy exec python pylearn2_benchmark.py
