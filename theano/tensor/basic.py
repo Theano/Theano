d506564943beb4812102fe9c4644ac6edfe101ff
Chinnadhurai Sankar <sankarch@leto04.iro.umontreal.ca>
2017-02-09 19:19:06 -0500

    cast numpy.prod(arg) to int when arg is empty

--- theano/tensor/basic.py
+++ theano/tensor/basic.py
@@ -1232,7 +1232,10 @@ class MaxAndArgmax(Op):
         transposed_x = numpy.transpose(x, numpy.concatenate((keep_axes, axes)))
         kept_shape = transposed_x.shape[:len(keep_axes)]
         reduced_shape = transposed_x.shape[len(keep_axes):]
-        new_shape = kept_shape + (numpy.prod(reduced_shape),)
+
+        # Numpy.prod returns 1.0 when arg is empty, so we cast it to int
+        # Otherwise reshape would complain citiing float arg
+        new_shape = kept_shape + (int(numpy.prod(reduced_shape)),)
         reshaped_x = transposed_x.reshape(new_shape)
 
         max_idx[0] = theano._asarray(numpy.argmax(reshaped_x, axis=-1),
