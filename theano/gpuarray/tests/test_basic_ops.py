42ffa21aa20a78cbbea7219b901b9ffc36cec754
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2017-02-18 23:27:32 -0500

    Update param size limit, correct size computation

--- theano/gpuarray/tests/test_basic_ops.py
+++ theano/gpuarray/tests/test_basic_ops.py
@@ -477,14 +477,13 @@ def test_Gpujoin_inplace():
 
 
 def test_many_arg_elemwise():
-    """this test checks whether the + and * elemwise ops can handle extremely large numbers of
-    arguments on gpu
-    i.e., it is a test of the optimization theano/sandbox/cuda/opt.py:local_gpu_huge_add_or_mul """
+    # this test checks whether the + and * elemwise ops can handle
+    # extremely large numbers of arguments on gpu
     rng = np.random.RandomState([1, 2, 3])
 
-    for num_args in [25]:
+    for num_args in [75]:
         for op_to_test in [theano.tensor.add, theano.tensor.mul]:
-            for nb_dim in [2, 3, 4, 5]:
+            for nb_dim in [2, 3, 4, 5, 7]:
                 shapes = [rng.randint(1, 5) for i in range(nb_dim)]
                 args = [np.cast['float32'](rng.randn(*shapes))
                         for arg in range(0, num_args)]
