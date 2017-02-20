00094485913192507363cc75fb8e334f0e546644
Frederic Bastien <nouiz@nouiz.org>
2017-02-20 15:39:37 -0500

    Remove old comments

--- theano/gof/tests/test_graph.py
+++ theano/gof/tests/test_graph.py
@@ -359,8 +359,6 @@ class TestAutoName:
         assert r1 is r2
 
         r3 = tensor.constant(1.6)
-        # The cache still create a new object that we don't return.
-        # This is why we must increase by 2 and not 1.
         assert r3.auto_name == "auto_" + str(autoname_id + 1)
 
     def test_tensorvariable(self):
