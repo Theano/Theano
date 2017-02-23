a9784765f5748ef9219d02bab2d6b5368efa2179
Frederic Bastien <nouiz@nouiz.org>
2017-02-23 08:04:15 -0500

    Remove the old benchmark directory. They use the module system that was removed years ago. Now the benchmark are outside Theano.

--- benchmark/convolution/bench.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python opencv.py $@
-python conv2d.py $@
-python scipy_conv.py $@
-
-echo "WARNING the mode is valid for theano and scipy, but opencv use the mode same! Can opencv do the mode full?"
