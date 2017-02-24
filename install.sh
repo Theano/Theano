c4122e44e3ea43652ecabb1fa94d847153798885
Boris Fomitchev <bfomitchev@nvidia.com>
2017-02-23 20:12:27 -0800

    Added build script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+# PREFIX=${PREFIX:-"${THIS_DIR}/install"}
+PREFIX=${PREFIX:-"/usr/local"}
+MAKE=${MAKE:-"make"}
+SUDO=${SUDO:-""} 
+
+echo "=== Installing requirements..."
+cat requirement-rtd.txt | xargs -n1 ${SUDO} pip install --no-cache-dir || exit 1
+
+echo "=== Builing gpuarray ..."
+cd ${THIS_DIR}/libgpuarray \
+    && cmake -E make_directory build && cd build \
+    && cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    && VERBOSE=1 ${MAKE} \
+    && VERBOSE=1 ${SUDO} ${MAKE} install || exit 1
+
+echo "=== Installing pygpu ..."
+cd ${THIS_DIR}/libgpuarray \
+    python setup.py build_ext \
+    -L ${PREFIX}/lib -I ${MY_PREFIX}/include \
+    && ${SUDO} python setup.py install || exit 1
+
+
+cd ${THIS_DIR}
+
+echo "=== Running setup.py ..."
+python setup.py build_ext \
+    -L ${PREFIX}/lib -I ${MY_PREFIX}/include \
+    && ${SUDO} python setup.py install || exit 1
+
+# Install Theano via PIP
+echo "=== Running setup.py ..."
+${SUDO} pip install -e ${THIS_DIR} || exit 1
+
+echo "=== Finished installing Theano."
+
