28ae0ea0e05e60f7e1023ea10d65cb6ee21bb77e
Boris Fomitchev <bfomitchev@nvidia.com>
2017-02-28 22:37:08 -0800

    Fixed regression test

--- qa/L0_regression/b1874975.py
+++ qa/L0_regression/b1874975.py
@@ -4,8 +4,8 @@ import theano.tensor as T
 import numpy as np
 import sys
 
-d = T.vector('d', dtype=floatX)
-d_np = np.array([1, 1, 1, 1, 0, 1, 1, 1, 1, 1], dtype=floatX)
+d = T.vector('d', dtype=theano.config.floatX)
+d_np = np.array([1, 1, 1, 1, 0, 1, 1, 1, 1, 1], dtype=theano.config.floatX)
 f = theano.function([d], d.sum())
 # 9.0 with gpu, but used to be 5.0 with cuda
 res = f(d_np)
