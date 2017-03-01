506d20cc21ae53bdf8def9f8a3d9689e5d87233b
Boris Fomitchev <bfomitchev@nvidia.com>
2017-02-28 17:11:25 -0800

    Added regression test

--- qa/L0_regression/b1874975.py
+++ qa/L0_regression/b1874975.py
@@ -1,11 +1,14 @@
+# bug 1874975
 import theano
 import theano.tensor as T
 import numpy as np
+import sys
 
 d = T.vector('d', dtype=floatX)
 d_np = np.array([1, 1, 1, 1, 0, 1, 1, 1, 1, 1], dtype=floatX)
 f = theano.function([d], d.sum())
-# 9.0 with gpu, but 5.0 with cuda
-print ("9.0 with gpu, but 5.0 with cuda")
-print(f(d_np))
-
+# 9.0 with gpu, but used to be 5.0 with cuda
+res = f(d_np)
+print ("Result should be 9.0:")
+print(res)
+sys.exit(0 if 9==res else 1) 
