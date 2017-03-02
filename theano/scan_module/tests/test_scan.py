e1180b73553cda71d378e01af6cbf3f3498f5f5c
Arnaud Bergeron <abergeron@gmail.com>
2017-03-02 18:06:07 -0500

    Fix weight initialization in scna test so that training works.

--- theano/scan_module/tests/test_scan.py
+++ theano/scan_module/tests/test_scan.py
@@ -1838,20 +1838,20 @@ class T_Scan(unittest.TestCase):
         n_in = 1
         n_out = 1
 
-        W_hh_v = asarrayX(rng.uniform(size=(n_hid, n_hid), low=-.01, high=.01))
-        h0_v = asarrayX(rng.uniform(size=(2, n_hid), low=-.01, high=.01))
+        W_hh_v = asarrayX(rng.uniform(size=(n_hid, n_hid), low=-1, high=1))
+        h0_v = asarrayX(rng.uniform(size=(2, n_hid), low=-1, high=1))
         b_h_v = asarrayX(rng.uniform(size=(n_hid), low=-.01, high=.01))
-        W_ih_v = asarrayX(rng.uniform(size=(n_in, n_hid), low=-.01, high=.01))
-        W_ho_v = asarrayX(rng.uniform(size=(n_hid, n_out), low=-.01, high=.01))
+        W_ih_v = asarrayX(rng.uniform(size=(n_in, n_hid), low=-1, high=1))
+        W_ho_v = asarrayX(rng.uniform(size=(n_hid, n_out), low=-1, high=1))
         b_o_v = asarrayX(rng.uniform(size=(n_out), low=-.01, high=.01))
 
         # parameters of the rnn
-        b_h = theano.shared(b_h_v)
-        h0 = theano.shared(h0_v)
-        W_ih = theano.shared(W_ih_v)
-        W_hh = theano.shared(W_hh_v)
-        W_ho = theano.shared(W_ho_v)
-        b_o = theano.shared(b_o_v)
+        b_h = theano.shared(b_h_v, name='b_h')
+        h0 = theano.shared(h0_v, name='h0')
+        W_ih = theano.shared(W_ih_v, name='W_ih')
+        W_hh = theano.shared(W_hh_v, name='W_hh')
+        W_ho = theano.shared(W_ho_v, name='W_ho')
+        b_o = theano.shared(b_o_v, name='b_o')
         params = [W_ih, W_hh, b_h, W_ho, b_o, h0]
 
         # first dimension is time
