8612a1f842254dd86f0b105c6170a4a774872784
Boris Fomitchev <bfomitchev@nvidia.com>
2017-03-07 16:18:22 -0800

    Disabling gpu preload, enabling CNMem

--- qa/L1_convnet_benchmarks/test-pylearn2.sh
+++ qa/L1_convnet_benchmarks/test-pylearn2.sh
@@ -3,4 +3,5 @@
 (cd ../third_party/pylearn2 && pip install -e .)
 
 cd ../third_party/convnet-benchmarks/theano
+export THEANO_FLAGS="gpuarray.preallocate=0,lib.cnmem=1"
 SKIP=legacy exec python pylearn2_benchmark.py
