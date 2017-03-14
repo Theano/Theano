7785b0baf395ec584ae193ca5d30df7dac362e2e
Boris Fomitchev <bfomitchev@nvidia.com>
2017-03-14 00:11:09 -0700

    Making tests use 45% memory for each backend

--- qa/L2_self_test_batches/test.sh
+++ qa/L2_self_test_batches/test.sh
@@ -2,6 +2,6 @@
 
 cd /opt/theano
 
-export THEANO_FLAGS="floatX=float32,device=cpu"
+export THEANO_FLAGS="floatX=float32,device=cpu,gpuarray.preallocate=0.45,lib.cnmem=0.45"
 
 exec python -m theano.tests.run_tests_in_batch
