650a5c680b85c0593a912cad00a8faa4f12e53e3
Cliff Woolley <jwoolley@nvidia.com>
2017-03-18 00:12:51 +0000

    Update test.sh [skip ci]

--- qa/L2_self_test_batches/test.sh
+++ qa/L2_self_test_batches/test.sh
@@ -2,6 +2,6 @@
 
 cd /opt/theano
 
-export THEANO_FLAGS="floatX=float32,device=cpu,gpuarray.preallocate=0.45,lib.cnmem=0.45"
+export THEANO_FLAGS="floatX=float32,device=cpu,lib.cnmem=0.45"
 
 exec python -m theano.tests.run_tests_in_batch
