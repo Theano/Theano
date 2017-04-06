0d4091af5943b5ff3daf072403669e6bd3ae4a2d
Arnaud Bergeron <abergeron@gmail.com>
2017-04-06 17:05:45 -0400

    Hack around read-only __doc__ in python 2.

--- theano/tensor/tests/test_sharedvar.py
+++ theano/tensor/tests/test_sharedvar.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import, print_function, division
+import six
+
 import numpy as np
 import unittest
 from functools import update_wrapper
@@ -58,7 +60,13 @@ def makeSharedTester(shared_constructor_,
 
 
     """
+    class m(type):
+        pass
+
     class SharedTester(unittest.TestCase):
+        # This is to allow setting __doc__ with python 2
+        if not six.PY3:
+            __metaclass__ = m
         shared_constructor = staticmethod(shared_constructor_)
         dtype = dtype_
         get_value_borrow_true_alias = get_value_borrow_true_alias_
