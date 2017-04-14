a6e8a353cd74d1e2ad9b00ab6770370773ea856c
amrithasuresh <sureshcbt@gmail.com>
2017-04-14 12:38:57 +0900

    Updated numpy as np

--- theano/scalar/basic.py
+++ theano/scalar/basic.py
@@ -2249,7 +2249,7 @@ class Cast(UnaryScalarOp):
             raise TypeError(o_type)
         super(Cast, self).__init__(specific_out(o_type), name=name)
         self.o_type = o_type
-        self.ctor = getattr(numpy, o_type.dtype)
+        self.ctor = getattr(np, o_type.dtype)
 
     def __str__(self):
         return '%s{%s}' % (self.__class__.__name__, self.o_type.dtype)
