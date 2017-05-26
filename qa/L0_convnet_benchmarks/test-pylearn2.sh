5a78aa271e4eac29947c89bce7c0bab182441f57
Boris Fomitchev <bfomitchev@nvidia.com>
2017-05-26 16:26:36 -0700

    Deleted unsupported tests

--- qa/L0_convnet_benchmarks/test-pylearn2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SUDO=${SUDO:-""} 
-
-(cd ../third_party/pylearn2 && ${SUDO} pip install -e .)
-
-cd ../third_party/convnet-benchmarks/theano
-export THEANO_FLAGS="gpuarray.preallocate=0,lib.cnmem=1"
-SKIP=legacy exec python pylearn2_benchmark.py
