95450282f5d5b1f3a0081b8821e18802952d98eb
affanv14 <affanv14@gmail.com>
2017-06-05 21:50:41 +0530

    change make_node to support num_groups

--- theano/tensor/nnet/corr.py
+++ theano/tensor/nnet/corr.py
@@ -688,8 +688,12 @@ class CorrMM_gradInputs(BaseCorrMM):
             height_width = [as_tensor_variable(shape[0]).astype('int64'),
                             as_tensor_variable(shape[1]).astype('int64')]
 
-        broadcastable = [topgrad.type.broadcastable[0], kern.type.broadcastable[1],
-                         False, False]
+        if self.num_groups > 1:
+            broadcastable = [topgrad.type.broadcastable[0], False,
+                             False, False]
+        else:
+            broadcastable = [topgrad.type.broadcastable[0], kern.type.broadcastable[1],
+                             False, False]
         dtype = kern.type.dtype
         return Apply(self, [kern, topgrad] + height_width,
                      [TensorType(dtype, broadcastable)()])
