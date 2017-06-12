88c9397d60259947e0754799ab2dba17170c9426
Frederic Bastien <nouiz@nouiz.org>
2017-06-12 16:52:08 -0400

    Add c code to abs for uint* and bool

--- theano/gpuarray/tests/test_misc.py
+++ theano/gpuarray/tests/test_misc.py
@@ -1,5 +1,6 @@
 # Test that normaly could be outside gpuarray, to have all gpuarray
 # tests in the same directory, we put them here.
+import numpy as np
 
 import theano
 from theano import tensor
@@ -9,9 +10,12 @@ from .config import mode_with_gpu
 
 
 def test_nan_guard_mode():
-    x = tensor.vector(dtype='int64')
-    y = x + 1
-    mode = NanGuardMode(nan_is_error=True, optimizer=mode_with_gpu.optimizer)
-    f = theano.function([x], y, mode=mode)
-    theano.printing.debugprint(f)
-    f([23, 7])
+    # Also test that abs uint* and bool have c code.
+    for dtype in ['uint8', 'int64', 'bool']:
+        x = tensor.vector(dtype=dtype)
+        y = x + 1
+        mode = NanGuardMode(nan_is_error=True,
+                            optimizer=mode_with_gpu.optimizer)
+        f = theano.function([x], y, mode=mode)
+        d = np.asarray([23, 7]).astype(dtype)
+        assert np.allclose(f(d), d + 1)
