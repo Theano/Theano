edc0d81cdf9351deb2b324a4555e2b6e8c613014
Frederic Bastien <nouiz@nouiz.org>
2017-07-04 11:37:21 -0400

    flake8/syntax

--- theano/tensor/signal/tests/test_pool.py
+++ theano/tensor/signal/tests/test_pool.py
@@ -428,7 +428,7 @@ class TestDownsampleFactorMax(utt.InferShapeTester):
                    (8, 5), (8, 5), (8, 5))
         outputshps = ((4, 10, 4, 7), (4, 10, 5, 8), (4, 10, 2, 3),
                       (4, 10, 3, 4), (4, 10, 2, 3), (4, 10, 2, 3),
-                      (4, 10, 4, 1),]
+                      (4, 10, 4, 1), )
         images = tensor.dtensor4()
         for indx in np.arange(len(maxpoolshps)):
             imvsize = imvsizs[indx]
