f6aa74b210e8f78f302c88f919f638d62614796d
jhelie <jean.helie@gmail.com>
2017-07-05 12:52:57 +0100

    suggest renaming of __neq__ to __ne__

--- theano/gof/opt.py
+++ theano/gof/opt.py
@@ -56,8 +56,8 @@ class Optimizer(object):
         # in subclasses from other bases.
         return id(self) == id(other)
 
-    def __neq__(self, other):
-        # added to override the  __neq__ implementation that may be inherited
+    def __ne__(self, other):
+        # added to override the  __ne__ implementation that may be inherited
         # in subclasses from other bases.
         return id(self) != id(other)
 
