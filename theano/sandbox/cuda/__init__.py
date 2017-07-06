5d82e9e6d3c25e3d5f3f343479aab9a5237a81ed
notoraptor <stevenbocco@gmail.com>
2017-07-06 18:42:01 -0400

    Back to a SkipTest raised for sandbox cuda (explained with a comment).

--- theano/sandbox/cuda/__init__.py
+++ theano/sandbox/cuda/__init__.py
@@ -1,4 +1,8 @@
-raise ImportError(
+from nose.plugins.skip import SkipTest
+# NB: We raise a SkipTest (instead of another type of exception) because we're in a folder,
+# thus nosetests will look for test files into this folder. With a SkipTest raised,
+# the folder will be skipped by nosetests without failing.
+raise SkipTest(
     "You are importing theano.sandbox.cuda. This is the old GPU back-end and "
     "is removed from Theano. Use Theano 0.9 to use it. Even better, "
     "transition to the new GPU back-end! See "
