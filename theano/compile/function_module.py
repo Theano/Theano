1a4fec6e51dda787c8f71de28187bf5d7851a18e
Arnaud Bergeron <abergeron@gmail.com>
2017-07-31 14:33:56 -0400

    De-indent the loop.

--- theano/compile/function_module.py
+++ theano/compile/function_module.py
@@ -847,13 +847,13 @@ class Function(object):
                     if not is_aliased:
                         args_share_memory.append([i])
 
-                # Check for groups of more than one argument that share memory
-                for group in args_share_memory:
-                    if len(group) > 1:
-                        # copy all but the first
-                        for j in group[1:]:
-                            self.input_storage[j].storage[0] = copy.copy(
-                                self.input_storage[j].storage[0])
+            # Check for groups of more than one argument that share memory
+            for group in args_share_memory:
+                if len(group) > 1:
+                    # copy all but the first
+                    for j in group[1:]:
+                        self.input_storage[j].storage[0] = copy.copy(
+                            self.input_storage[j].storage[0])
 
         # Check if inputs are missing, or if inputs were set more than once, or
         # if we tried to provide inputs that are supposed to be implicit.
