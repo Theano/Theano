565138ff9b959895bf7305449c4b55ffdc9f2b4d
Kyle Friedline <kylefriedline@gmail.com>
2017-08-02 17:38:34 -0400

    Update cmodule.py

--- theano/gof/cmodule.py
+++ theano/gof/cmodule.py
@@ -2280,14 +2280,16 @@ class GCC_compiler(Compiler):
             cmd.append('-fvisibility=hidden')
         if lib_filename.find(" ") != -1:
             lib_filename = str("\"" + lib_filename + "\"")
-            cmd.exten(['-o', lib_filename])
+            cmd.extend(['-o', lib_filename])
             lib_filename = lib_filename.replace("\"", "")
         else:
             cmd.extend(['-o', lib_filename])
         if cppfilename.find(" ") != -1:
             cppfilename = str("\"" + cppfilename + "\"")
-        cmd.extend(['-o', lib_filename])
-        cmd.append(cppfilename)
+            cmd.append(cppfilename)
+            cppfilename = cppfilename.replace("\"", "")
+        else:
+            cmd.append(cppfilename)
         cmd.extend(['-l%s' % l for l in libs])
         # print >> sys.stderr, 'COMPILING W CMD', cmd
         _logger.debug('Running cmd: %s', ' '.join(cmd))
