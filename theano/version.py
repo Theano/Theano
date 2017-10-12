093a388bbd39f7572782821fc0b7ebcc3e117dd0
Frederic Bastien <nouiz@nouiz.org>
2017-10-12 10:18:22 -0400

    Use the Fallback version number also when importing Theano.

--- theano/version.py
+++ theano/version.py
@@ -2,23 +2,27 @@ from __future__ import absolute_import, print_function, division
 
 from theano._version import get_versions
 
+FALLBACK_VERSION = "0.10.0beta3+unknown"
+
 info = get_versions()
+if info['error'] is not None:
+    info['version'] = FALLBACK_VERSION
+
 full_version = info['version']
 git_revision = info['full-revisionid']
 del info, get_versions
 
 short_version = full_version.split('+')[0]
 
+
 # This tries to catch a tag like beta2, rc1, ...
 try:
     int(short_version.split('.')[2])
     release = True
-except (ValueError, IndexError):
-    # IndexError can happen with the version:
-    # 0+untagged.2896.g0560a7f for example
+except ValueError:
     release = False
 
-if release:
+if release and info['error'] is None:
     version = short_version
 else:
     version = full_version
