55f013b9e0f258199e312979b9d8a322c6644956
Frederic Bastien <nouiz@nouiz.org>
2017-11-24 09:31:16 -0500

    Re-activate GpuTopK

--- theano/gpuarray/tests/test_sort.py
+++ theano/gpuarray/tests/test_sort.py
@@ -4,13 +4,9 @@ import theano
 import theano.tensor.tests.test_sort
 from .config import mode_with_gpu
 from ..sort import GpuTopKOp
-from nose.plugins.skip import SkipTest
 
 
 class Test_GpuTopK(theano.tensor.tests.test_sort.Test_TopK):
     mode = mode_with_gpu
     dtype = 'float32'
     op_class = GpuTopKOp
-
-    def setUp(self):
-        raise SkipTest("GpuTopK is currently disabled")
