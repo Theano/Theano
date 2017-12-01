4644c5ca378a3d18443ea50ebc1208e72f4f030b
notoraptor <stevenbocco@gmail.com>
2017-12-01 12:24:27 -0500

    Revert "Try to fix cuDNN finding in jenkins tests for Windows"
    This reverts commit 0c4f7f136e9b09d34e5ee52c097f2d848db03954.
    
    Set dnn.base_path earlier for jenkins tests on Windows.

--- .jenkins/jenkins_buildbot_windows.bat
+++ .jenkins/jenkins_buildbot_windows.bat
@@ -14,7 +14,7 @@ set SUITE=--xunit-testsuite-name=
 
 set THEANO_PARAM=theano --with-timer --timer-top-n 10
 
-set THEANO_FLAGS=init_gpu_device=cuda
+set THEANO_FLAGS=init_gpu_device=cuda,dnn.base_path="%CUDNNPATH%"
 
 REM Build libgpuarray
 set GPUARRAY_CONFIG="Release"
@@ -59,5 +59,5 @@ python -c "import theano.gpuarray; theano.gpuarray.use('%DEVICE%')" || exit /b
 REM Fast run and float32
 set FILE=%BUILDBOT_DIR%\theano_python2_fastrun_f32_tests.xml
 set NAME=win_fastrun_f32
-set THEANO_FLAGS=%THEANO_FLAGS%,compiledir=%COMPILEDIR:\=\\%,mode=FAST_RUN,warn.ignore_bug_before=all,on_opt_error=raise,on_shape_error=raise,floatX=float32,dnn.base_path=%CUDNNPATH%,gcc.cxxflags='-I%LIBDIR:\=\\%\\include -L%LIBDIR:\=\\%\\lib'
+set THEANO_FLAGS=%THEANO_FLAGS%,compiledir=%COMPILEDIR:\=\\%,mode=FAST_RUN,warn.ignore_bug_before=all,on_opt_error=raise,on_shape_error=raise,floatX=float32,gcc.cxxflags='-I%LIBDIR:\=\\%\\include -L%LIBDIR:\=\\%\\lib'
 python bin\theano-nose %THEANO_PARAM% %XUNIT%%FILE% %SUITE%%NAME%
