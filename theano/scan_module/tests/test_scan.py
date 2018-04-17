629b4579e9dba738c5b9ec8166eb788058129f2d
Glexin <invetation@gmail.com>
2018-04-17 11:41:58 +0800

    temperary save

--- theano/scan_module/tests/test_scan.py
+++ theano/scan_module/tests/test_scan.py
@@ -5718,3 +5718,16 @@ class TestGradUntil(unittest.TestCase):
         numpy_grad = np.array([0, 0, 0, 5, 6, 10, 4, 5, 0, 0, 0, 0, 0, 0, 0])
         numpy_grad = numpy_grad.astype(theano.config.floatX)
         utt.assert_allclose(theano_gradient, numpy_grad)
+
+def test_dynamic_hidden_shared():
+    old_value = theano.shared(0)
+    print("old_value %s." % old_value.type)
+    def step_f():
+        new_value = old_value + 1
+        return {old_value : new_value}
+
+    n_steps = 3
+    _, updates = theano.scan(step_f, n_steps=n_steps)
+    f = theano.function(inputs=[], outputs=[], updates=updates)
+    f()
+    print(old_value.get_value())
