07cd30ff0692c7104afefdea6f8561d8a076d649
Thomas Wiecki <thomas.wiecki@gmail.com>
2018-09-14 10:13:26 +0200

    Modifications for 1.0.3 release

--- theano/configdefaults.py
+++ theano/configdefaults.py
@@ -752,7 +752,7 @@ AddConfigVar('warn.ignore_bug_before',
               "[warn] flags."),
              EnumStr('0.9', 'None', 'all', '0.3', '0.4', '0.4.1', '0.5', '0.6',
                      '0.7', '0.8', '0.8.1', '0.8.2', '0.9', '0.10', '1.0',
-                     '1.0.1', '1.0.2',
+                     '1.0.1', '1.0.2', '1.0.3',
                      allow_override=False),
              in_c_key=False)
 
--- theano/version.py
+++ theano/version.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function, division
 
 from theano._version import get_versions
 
-FALLBACK_VERSION = "1.0.2+unknown"
+FALLBACK_VERSION = "1.0.3+unknown"
 
 info = get_versions()
 if info['error'] is not None:
