d3b3f92fc38470fe3c93569deb92101bf61310c8
Duc Nguyen <justanhduc@users.noreply.github.com>
2018-12-11 11:25:05 +0900

    changed np cast to theano cast in bilinear_kernel_1D

--- theano/tensor/nnet/abstract_conv.py
+++ theano/tensor/nnet/abstract_conv.py
@@ -1766,7 +1766,7 @@ def bilinear_kernel_1D(ratio, normalize=True):
     kern = T.concatenate([half_kern, half_kern[-2::-1]])
 
     if normalize:
-        kern /= np.cast[theano.config.floatX](ratio)
+        kern /= T.cast(ratio, theano.config.floatX)
     return kern
 
 
