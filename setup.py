bacc028060c757c1c5ae698affe6386bcd295039
Anurag Kumar <mailanu98@gmail.com>
2021-09-08 09:06:56 +0530

    Update setup.py
    Update classifiers

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 """
 NAME                = 'Theano'
 MAINTAINER          = "PyMC devs"
